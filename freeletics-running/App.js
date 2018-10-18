431f08b81ed86c80808e132f228ae7af7b45f540
William Candillon <wcandillon@gmail.com>
2018-10-18 12:28:08 +0200

    🏃🏻‍♂️

---
+++ freeletics-running/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Run distance={200} />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
