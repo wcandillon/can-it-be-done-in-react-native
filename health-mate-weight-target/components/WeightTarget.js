409e451a6cae849cee3e6decacd38cb13c09f8c5
William Candillon <wcandillon@gmail.com>
2018-11-04 15:20:38 +0100

    ⚙️

--- health-mate-weight-target/components/WeightTarget.js
+++ health-mate-weight-target/components/WeightTarget.js
@@ -25,6 +25,8 @@ export default class WeightTarget extends React.PureComponent<WeightTargetProps,
 
     input = React.createRef();
 
+    line = React.createRef();
+
     listener: () => void;
 
     state = {
@@ -65,14 +67,18 @@ export default class WeightTarget extends React.PureComponent<WeightTargetProps,
       });
       const translateY2 = y.interpolate({
         inputRange,
-        outputRange: [height / 2 - 50, -height / 2 + 50],
+        outputRange: [height / 2 - 100, -height / 2 + 100],
       });
       const scale = y.interpolate({
         inputRange: [inputRange[0], inputRange[1] / 2, inputRange[1]],
         outputRange: [0.5, 1, 0.5],
       });
+      const scaleY = y.interpolate({
+        inputRange: [inputRange[0], inputRange[1] / 2, inputRange[1]],
+        outputRange: [height - 150, 50, height - 150],
+      });
       return (
-        <View style={styles.container}>
+        <View style={styles.root}>
           <Animated.ScrollView
             ref={this.scroll}
             style={StyleSheet.absoluteFillObject}
@@ -87,18 +93,21 @@ export default class WeightTarget extends React.PureComponent<WeightTargetProps,
           >
             <Scale from={BMI - 10} to={BMI + 10} />
           </Animated.ScrollView>
-          <View style={styles.cursorContainer} pointerEvents="none">
+          <View style={styles.container} pointerEvents="none">
+            <Animated.View ref={this.line} style={[styles.line, { transform: [{ scaleY }] }]} />
+          </View>
+          <View style={styles.container} pointerEvents="none">
+            <Animated.View style={[styles.oppositeCursor, { transform: [{ translateY: translateY2 }] }]} />
+          </View>
+          <View style={styles.container} pointerEvents="none">
             <Animated.View style={[styles.cursor, { transform: [{ scale }] }]}>
               <TextInput ref={this.input} style={styles.cursorLabel} />
             </Animated.View>
           </View>
-          <View style={styles.cursorContainer} pointerEvents="none">
-            <Animated.View style={[styles.cursor, { transform: [{ translateY }] }]}>
+          <View style={styles.container} pointerEvents="none">
+            <Animated.View style={[styles.mainCursor, { transform: [{ translateY }] }]}>
               <TextInput ref={this.input} style={styles.cursorLabel} />
             </Animated.View>
-          </View>
-          <View style={styles.cursorContainer} pointerEvents="none">
-            <Animated.View style={[styles.oppositeCursor, { transform: [{ translateY: translateY2 }] }]} />
           </View>
         </View>
       );
@@ -106,14 +115,19 @@ export default class WeightTarget extends React.PureComponent<WeightTargetProps,
 }
 
 const styles = StyleSheet.create({
-  container: {
+  root: {
     flex: 1,
     backgroundColor,
   },
-  cursorContainer: {
+  container: {
     ...StyleSheet.absoluteFillObject,
     alignItems: "center",
     justifyContent: "center",
+  },
+  line: {
+    height: 1,
+    width: 1,
+    backgroundColor: "white",
   },
   cursor: {
     borderRadius: 50,
@@ -123,6 +137,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     borderWidth: 2,
     borderColor: "white",
+    backgroundColor,
+  },
+  mainCursor: {
+    borderRadius: 50,
+    height: 100,
+    width: 100,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "white",
   },
   oppositeCursor: {
     height: 50,
