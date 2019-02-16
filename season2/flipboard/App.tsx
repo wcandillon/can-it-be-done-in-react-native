a3851cf3b85230327ca1f7180465a58f80cf85d3
William Candillon <wcandillon@gmail.com>
2019-02-16 20:56:20 +0100

    ⚙️

--- season2/flipboard/App.tsx
+++ season2/flipboard/App.tsx
@@ -1,21 +1,62 @@
+/* eslint-disable global-require */
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions } from "react-native";
+import { ImageManipulator, Asset, AppLoading } from "expo";
 
-import Stories from "./components/Stories";
+import Stories, { Story } from "./components/Stories";
 
-export default class App extends React.Component {
+const { width, height } = Dimensions.get("window");
+const screens = [
+  require("./assets/stories/story1.png"),
+  require("./assets/stories/story2.png"),
+  require("./assets/stories/story3.png"),
+  require("./assets/stories/story4.png"),
+];
+
+interface IAppProps {}
+interface IAppState {
+  stories: Story[];
+}
+
+export default class App extends React.Component<IAppProps, IAppState> {
+  state = {
+    stories: [],
+  };
+
+  async componentDidMount() {
+    const edits = screens.map(async (screen) => {
+      const image = Asset.fromModule(screen);
+      await image.downloadAsync();
+      const { localUri } = image;
+      const crop1 = {
+        originX: 0,
+        originY: 0,
+        width,
+        height: height / 2,
+      };
+      const crop2 = {
+        originX: 0,
+        originY: height / 2,
+        width,
+        height: height / 2,
+      };
+      const { uri: top } = await ImageManipulator.manipulateAsync(localUri, [crop1]);
+      const { uri: bottom } = await ImageManipulator.manipulateAsync(localUri, [crop2]);
+      return { top, bottom };
+    });
+    const stories = await Promise.all(edits);
+    this.setState({ stories });
+  }
+
   render() {
+    const { stories } = this.state;
+    if (stories.length === 0) {
+      return (
+        <AppLoading />
+      );
+    }
     return (
-      <Stories />
+      <Stories {...{ stories }} />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- season2/flipboard/components/Stories.tsx
+++ season2/flipboard/components/Stories.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
-import { View, StyleSheet, Dimensions } from "react-native";
+import {
+  View, StyleSheet, Dimensions, Image,
+} from "react-native";
 import { DangerZone } from "expo";
 
 const {
@@ -9,8 +11,16 @@ const {
   Value, Extrapolate, event, interpolate, concat,
 } = Animated;
 const { height } = Dimensions.get("window");
+const perspective = 1000;
 
-interface StoriesProps {}
+export interface Story {
+  top: string;
+  bottom: string;
+}
+
+interface StoriesProps {
+  stories: Story[];
+}
 
 export default class Stories extends React.PureComponent<StoriesProps> {
   y = new Value(0);
@@ -27,29 +37,32 @@ export default class Stories extends React.PureComponent<StoriesProps> {
 
   render() {
     const { y, onScroll } = this;
-    const translateY = interpolate(y, {
-      inputRange: [0, height],
-      outputRange: [0, height / 2],
-      extrapolate: Extrapolate.CLAMP,
-    });
+    const { stories } = this.props;
+    const story = stories[0];
+    const { top } = story;
     const rotateX = concat(interpolate(y, {
       inputRange: [0, height],
-      outputRange: [0, 180],
+      outputRange: [0, -180],
       extrapolate: Extrapolate.CLAMP,
     }), "deg");
     return (
       <View style={styles.container}>
         <View style={styles.story}>
-          <Animated.View style={[styles.topHalf, { transform: [{ rotateX }] }]} />
+          <Animated.View
+            style={[styles.topHalf, {
+              transform: [{ perspective }, { translateY: height / 4 }, { rotateX }, { translateY: -height / 4 }],
+            }]}
+          >
+            <Image source={{ uri: top }} style={styles.image} />
+          </Animated.View>
           <View style={styles.bottomHalf} />
         </View>
         <Animated.ScrollView
-          style={{ ...StyleSheet.absoluteFillObject }}
+          style={styles.scrollView}
           showsVerticalScrollIndicator={false}
           scrollEventThrottle={1}
           bounces={false}
           contentContainerStyle={{ height: height * 2 }}
-
           decelerationRate="fast"
           {...{ onScroll }}
         />
@@ -65,13 +78,21 @@ const styles = StyleSheet.create({
   story: {
     flex: 1,
   },
+  scrollView: {
+    ...StyleSheet.absoluteFillObject,
+  },
   bottomHalf: {
     flex: 1,
-    backgroundColor: "blue",
+    backgroundColor: "transparent",
   },
   topHalf: {
     flex: 1,
-    backgroundColor: "red",
     // backfaceVisibility: "hidden",
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    width: null,
+    height: null,
+    resizeMode: "cover",
   },
 });
