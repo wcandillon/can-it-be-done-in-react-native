6a7339ec4a2465825ea3c8595cdff309bf60cda3
William Candillon <wcandillon@gmail.com>
2019-02-21 23:00:05 +0100

    🆕 (#9)

---
+++ bonuses/flip-card/App.tsx
@@ -0,0 +1,73 @@
+import React from "react";
+import {
+  View, StyleSheet, Dimensions, ImageSourcePropType,
+} from "react-native";
+import { Asset, AppLoading, DangerZone } from "expo";
+
+import Card from "./components/Card";
+
+const { Animated } = DangerZone;
+const { Value, call } = Animated;
+const { width } = Dimensions.get("window");
+
+interface AppState {
+  card?: {
+    front: ImageSourcePropType;
+    back: ImageSourcePropType;
+  }
+}
+
+export default class App extends React.Component<{}, AppState> {
+  x = new Value(0);
+
+  state: AppState = {
+    card: null,
+  };
+
+  async componentDidMount() {
+    const front = require("./assets/front.png");
+    const back = require("./assets/back.png");
+    await Promise.all([
+      Asset.loadAsync(front),
+      Asset.loadAsync(back),
+    ]);
+    this.setState({ card: { front, back } });
+  }
+
+  render() {
+    const { x } = this;
+    const { card } = this.state;
+    if (!card) {
+      return (<AppLoading />);
+    }
+    return (
+      <View style={styles.container}>
+        <Card front={card.front} back={card.back} {...{ x }} />
+        <Animated.ScrollView
+          style={StyleSheet.absoluteFillObject}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{ width: width * 2 }}
+          scrollEventThrottle={16}
+          onScroll={Animated.event(
+            [
+              {
+                nativeEvent: {
+                  contentOffset: { x },
+                },
+              },
+            ],
+          )}
+          horizontal
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
---
+++ bonuses/flip-card/components/Card.tsx
@@ -0,0 +1,79 @@
+import * as React from "react";
+import {
+  View, Image, StyleSheet, ImageSourcePropType, Dimensions, Platform, Text,
+} from "react-native";
+import { DangerZone } from "expo";
+
+const { Animated } = DangerZone;
+const {
+  Value, concat, interpolate, cond, and, greaterOrEq, lessThan,
+} = Animated;
+const { width } = Dimensions.get("window");
+const perspective = Platform.OS === "ios" ? 1000 : undefined;
+
+interface CardProps {
+  front: ImageSourcePropType;
+  back: ImageSourcePropType;
+  x: Value
+}
+
+export default class Card extends React.PureComponent<CardProps> {
+  render() {
+    const { front, back, x } = this.props;
+    const rotateYAsDeg = interpolate(x, {
+      inputRange: [0, width],
+      outputRange: [0, 180],
+    });
+    const rotateY = concat(rotateYAsDeg, "deg");
+    const opacity = cond(
+      and(greaterOrEq(rotateYAsDeg, -90), lessThan(rotateYAsDeg, 90)),
+      1,
+      0,
+    );
+    const backOpacity = Platform.OS === "android" ? cond(opacity, 0, 1) : 1;
+    /*
+          */
+    return (
+      <View style={styles.container}>
+        <Animated.View style={{
+          ...StyleSheet.absoluteFillObject,
+          justifyContent: "center",
+          alignItems: "center",
+          opacity: backOpacity,
+          transform: [
+            { perspective },
+            { rotateY: "180deg" },
+            { rotateY },
+          ],
+        }}
+        >
+          <Image source={back} style={styles.image} />
+          <Text style={{ color: "white", fontSize: 72 }}>Hello</Text>
+        </Animated.View>
+        <Animated.View style={{
+          ...StyleSheet.absoluteFillObject,
+          opacity,
+          transform: [
+            { perspective },
+            { rotateY },
+          ],
+        }}
+        >
+          <Image source={front} style={styles.image} />
+        </Animated.View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    width: 306,
+    height: 434,
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    width: undefined,
+    height: undefined,
+  },
+});
