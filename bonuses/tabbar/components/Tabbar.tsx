f36145608bcef6b5c80dfd8cab9202756828ec64
William Candillon <wcandillon@gmail.com>
2019-03-10 14:17:33 +0100

    ⚙️

--- bonuses/tabbar/components/Tabbar.tsx
+++ bonuses/tabbar/components/Tabbar.tsx
@@ -1,13 +1,32 @@
 import * as React from "react";
-import { View, SafeAreaView, StyleSheet } from "react-native";
+import {
+  SafeAreaView, StyleSheet, Dimensions,
+} from "react-native";
+import { path } from "d3-path";
+
+import { Svg } from "expo";
+
+const { width } = Dimensions.get("window");
+const height = 64;
+const { Path } = Svg;
+
+const d = path();
+d.moveTo(0, 0);
+d.lineTo(width, 0);
+d.lineTo(width, height);
+d.lineTo(0, height);
+d.lineTo(0, 0);
 
 interface TabbarProps {}
 
+// eslint-disable-next-line react/prefer-stateless-function
 export default class Tabbar extends React.PureComponent<TabbarProps> {
   render() {
     return (
       <SafeAreaView style={styles.container}>
-        <View style={styles.tabbar} />
+        <Svg {...{ width, height }}>
+          <Path d={d.toString()} fill="blue" />
+        </Svg>
       </SafeAreaView>
     );
   }
@@ -20,8 +39,5 @@ const styles = StyleSheet.create({
     left: 0,
     right: 0,
     backgroundColor: "white",
-  },
-  tabbar: {
-    height: 64,
   },
 });
