e6a70384ca5ff44dbdc1d4c7ad00a660ac42d68e
William Candillon <wcandillon@gmail.com>
2019-03-11 21:31:13 +0100

    ⚙️

--- bonuses/tabbar/components/Tabbar.tsx
+++ bonuses/tabbar/components/Tabbar.tsx
@@ -31,23 +31,38 @@ const tabs = [
 const tabWidth = width / tabs.length;
 const backgroundColor = "white";
 
-const getPath = (index: number): string => shape.line()
-  .x(d => d.x)
-  .y(d => d.y)
-  .curve(shape.curveBasis)([
+const getPath = (index: number): string => {
+  const start = index * tabWidth;
+  const end = start + tabWidth;
+  const left = shape.line().x(d => d.x).y(d => d.y)([
     { x: 0, y: 0 },
-    { x: 0 + 10, y: 10 },
-    { x: 0 + 10 + 10, y: height },
-    { x: 0 + tabWidth - 20, y: height },
-    { x: 0 + tabWidth - 10, y: 10 },
-    { x: tabWidth, y: 0 },
+    { x: start, y: 0 },
   ]);
+  // .curve(shape.curveMonotoneX)
+  const tab = shape.line().x(d => d.x).y(d => d.y).curve(shape.curveBasis)([
+    { x: start, y: 0 },
+    { x: start + 10, y: 10 },
+    { x: start + 15, y: height },
+    { x: end - 15, y: height },
+    { x: end - 10, y: 10 },
+    { x: end, y: 0 },
+  ]);
+  const right = shape.line().x(d => d.x).y(d => d.y)([
+    { x: end, y: 0 },
+    { x: width, y: 0 },
+    { x: width, y: height },
+    { x: 0, y: height },
+    { x: 0, y: 0 },
+  ]);
+  console.log({ left, tab, right });
+  return `${left} ${tab} ${right}`;
+};
 interface TabbarProps {}
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Tabbar extends React.PureComponent<TabbarProps> {
   render() {
-    const d = `${getPath(0)}L375,0L375,64L0,64`;
+    const d = getPath(1);
     console.log({ d });
     return (
       <>
