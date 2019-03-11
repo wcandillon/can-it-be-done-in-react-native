cc35d905056cb887ede005877ec663b470a8f361
William Candillon <wcandillon@gmail.com>
2019-03-11 20:49:25 +0100

    ⚙️

--- bonuses/tabbar/components/Tabbar.tsx
+++ bonuses/tabbar/components/Tabbar.tsx
@@ -2,30 +2,54 @@ import * as React from "react";
 import {
   SafeAreaView, StyleSheet, Dimensions,
 } from "react-native";
-import { path } from "d3-path";
+// import { path } from "d3-path";
+import * as shape from "d3-shape";
+
 
 import { Svg } from "expo";
 
 const { width } = Dimensions.get("window");
 const height = 64;
 const { Path } = Svg;
+const tabs = [
+  {
+    name: "grid",
+  },
+  {
+    name: "list",
+  },
+  {
+    name: "repeat",
+  },
+  {
+    name: "map",
+  },
+  {
+    name: "user",
+  },
+];
+const tabWidth = width / tabs.length;
 
-const d = path();
-d.moveTo(0, 0);
-d.lineTo(width, 0);
-d.lineTo(width, height);
-d.lineTo(0, height);
-d.lineTo(0, 0);
-
+const getPath = (index: number): string => shape.line()
+  .x(d => d.x)
+  .y(d => d.y)
+  .curve(shape.curveMonotoneX)([
+    { x: 0, y: 0 },
+    { x: width, y: 0 },
+    { x: width, y: height },
+    { x: 0, y: height },
+  ]);
 interface TabbarProps {}
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Tabbar extends React.PureComponent<TabbarProps> {
   render() {
+    const d = getPath(0);
+    console.log({ d });
     return (
       <SafeAreaView style={styles.container}>
         <Svg {...{ width, height }}>
-          <Path d={d.toString()} fill="blue" />
+          <Path fill="blue" {...{ d }} />
         </Svg>
       </SafeAreaView>
     );
