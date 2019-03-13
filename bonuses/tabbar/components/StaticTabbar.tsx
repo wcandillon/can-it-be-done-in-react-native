961e30c8095d9e3fad7df7be43c86efed44fbb2c
William Candillon <wcandillon@gmail.com>
2019-03-13 12:26:08 +0100

    ⚙️

--- bonuses/tabbar/components/StaticTabbar.tsx
+++ bonuses/tabbar/components/StaticTabbar.tsx
@@ -18,8 +18,9 @@ interface StaticTabbarProps {
 export default class StaticTabbar extends React.PureComponent<StaticTabbarProps> {
   onPress = (index: number) => {
     const { value, tabs } = this.props;
+    const tabWidth = width / tabs.length;
     Animated.timing(value, {
-      toValue: (width / tabs.length) * index,
+      toValue: tabWidth * index,
       duration: 300,
       useNativeDriver: true,
     }).start();
@@ -27,17 +28,49 @@ export default class StaticTabbar extends React.PureComponent<StaticTabbarProps>
 
   render() {
     const { onPress } = this;
-    const { tabs } = this.props;
+    const { tabs, value } = this.props;
     return (
       <View style={styles.container}>
         {
-          tabs.map((tab, key) => (
-            <TouchableWithoutFeedback onPress={() => onPress(key)} {...{ key }}>
-              <View style={styles.tab}>
-                <Icon name={tab.name} color="black" size={25} />
-              </View>
-            </TouchableWithoutFeedback>
-          ))
+          tabs.map((tab, key) => {
+            const tabWidth = width / tabs.length;
+            const cursor = tabWidth * key;
+            const opacity = value.interpolate({
+              inputRange: [cursor - tabWidth, cursor, cursor + tabWidth],
+              outputRange: [1, 0, 1],
+              extrapolate: "clamp",
+            });
+            const translateY = value.interpolate({
+              inputRange: [cursor - tabWidth, cursor, cursor + tabWidth],
+              outputRange: [64, 0, 64],
+              extrapolate: "clamp",
+            });
+            return (
+              <React.Fragment {...{ key }}>
+                <TouchableWithoutFeedback onPress={() => onPress(key)}>
+                  <Animated.View style={[styles.tab, { opacity }]}>
+                    <Icon name={tab.name} color="black" size={25} />
+                  </Animated.View>
+                </TouchableWithoutFeedback>
+                <Animated.View
+                  style={{
+                    position: "absolute",
+                    top: -8,
+                    left: tabWidth * key,
+                    width: tabWidth,
+                    height: 64,
+                    justifyContent: "center",
+                    alignItems: "center",
+                    transform: [{ translateY }],
+                  }}
+                >
+                  <View style={styles.activeIcon}>
+                    <Icon name={tab.name} color="black" size={25} />
+                  </View>
+                </Animated.View>
+              </React.Fragment>
+            );
+          })
         }
       </View>
     );
@@ -53,5 +86,13 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     height: 64,
+  },
+  activeIcon: {
+    backgroundColor: "white",
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    justifyContent: "center",
+    alignItems: "center",
   },
 });
