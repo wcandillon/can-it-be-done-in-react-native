b1a4d4936904d4e1c189b8e5ccc36d02649abc08
William Candillon <wcandillon@gmail.com>
2019-03-16 17:59:26 +0100

    ⚙️

--- season2/google-chrome-refresh/components/Header.tsx
+++ season2/google-chrome-refresh/components/Header.tsx
@@ -1,12 +1,15 @@
 import * as React from "react";
-import { View, SafeAreaView, StyleSheet } from "react-native";
+import { SafeAreaView, StyleSheet, Dimensions } from "react-native";
 import { Feather as Icon } from "@expo/vector-icons";
 import { DangerZone } from "expo";
 
 const { Animated } = DangerZone;
 const {
-  Value, interpolate, add, multiply, divide,
+  Value, Extrapolate, interpolate, divide,
 } = Animated;
+const height = 64;
+const iconSize = 32;
+const { width } = Dimensions.get("window");
 
 interface HeaderProps {
   x: Value,
@@ -17,12 +20,39 @@ export default class Header extends React.PureComponent<HeaderProps> {
   render() {
     const { x, y } = this.props;
     const translateY = divide(y, -2);
+    const translation = (width - (iconSize * 3)) / 4;
+    const opacity = interpolate(y, {
+      inputRange: [height, height * 2],
+      outputRange: [0, 1],
+      extrapolate: Extrapolate.CLAMP,
+    });
+    const opacityCenter = interpolate(y, {
+      inputRange: [0, height],
+      outputRange: [0, 1],
+      extrapolate: Extrapolate.CLAMP,
+    });
+    const translateXLeft = interpolate(y, {
+      inputRange: [height, height * 2],
+      outputRange: [translation, 0],
+      extrapolate: Extrapolate.CLAMP,
+    });
+    const translateXRight = interpolate(y, {
+      inputRange: [height, height * 2],
+      outputRange: [-translation, 0],
+      extrapolate: Extrapolate.CLAMP,
+    });
     return (
       <SafeAreaView style={styles.container}>
         <Animated.View style={[styles.content, { transform: [{ translateY }] }]}>
-          <Icon name="plus" size={32} />
-          <Icon name="refresh-ccw" size={32} />
-          <Icon name="x" size={32} />
+          <Animated.View style={{ opacity, transform: [{ translateX: translateXLeft }] }}>
+            <Icon name="plus" size={iconSize} />
+          </Animated.View>
+          <Animated.View style={{ opacity: opacityCenter }}>
+            <Icon name="refresh-ccw" size={iconSize} />
+          </Animated.View>
+          <Animated.View style={{ opacity, transform: [{ translateX: translateXRight }] }}>
+            <Icon name="x" size={iconSize} />
+          </Animated.View>
         </Animated.View>
       </SafeAreaView>
     );
@@ -34,7 +64,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#f0f1f2",
   },
   content: {
-    height: 64,
+    height,
     flexDirection: "row",
     justifyContent: "space-evenly",
     alignItems: "center",
