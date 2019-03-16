ac81f038a275cc7f1c24191815da49dc2c1f986f
William Candillon <wcandillon@gmail.com>
2019-03-16 17:25:59 +0100

    ⚙️

--- season2/google-chrome-refresh/App.tsx
+++ season2/google-chrome-refresh/App.tsx
@@ -1,9 +1,30 @@
 import React from "react";
+import { AppLoading, Asset } from "expo";
 
 import BrowserTab from "./components/BrowserTab";
 
-export default class App extends React.Component {
+interface AppProps {}
+interface AppState {
+  ready: boolean;
+}
+
+export default class App extends React.Component<AppProps, AppState> {
+  state = {
+    ready: false,
+  };
+
+  async componentDidMount() {
+    await Asset.loadAsync(require("./assets/image.jpg"));
+    this.setState({ ready: true });
+  }
+
   render() {
+    const { ready } = this.state;
+    if (!ready) {
+      return (
+        <AppLoading />
+      );
+    }
     return (
       <BrowserTab />
     );
