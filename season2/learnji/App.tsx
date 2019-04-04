15a1cc547f8150395241c5ef007fadd333942c79
William Candillon <wcandillon@gmail.com>
2019-04-04 18:26:44 +0200

    2️⃣

--- season2/learnji/App.tsx
+++ season2/learnji/App.tsx
@@ -1,21 +1,76 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet, View, Dimensions,
+} from "react-native";
+import { DangerZone } from "expo";
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
-  }
-}
+import Emojis, { EMOJI_WIDTH, EMOJIS_OFFSET } from "./components/Emojis";
+
+const { Animated } = DangerZone;
+const { Value, event } = Animated;
+const emojis = require("./assets/emoji-db.json");
+
+const { width, height } = Dimensions.get("window");
+const horizontalPanHeight = EMOJI_WIDTH;
+const verticalPanHeight = height / 2 - horizontalPanHeight / 2;
+const numberOfEmojis = Object.keys(emojis).length;
+const onScroll = (contentOffset: { x?: typeof Value, y?: typeof Value }) => event(
+  [
+    {
+      nativeEvent: {
+        contentOffset,
+      },
+    },
+  ],
+  { useNativeDriver: true },
+);
+
+
+export default () => {
+  const x = new Value(0);
+  const y = new Value(0);
+  return (
+    <View style={styles.container}>
+      <Emojis {...{ emojis, x, y }} />
+      <Animated.ScrollView
+        style={styles.verticalPan}
+        contentContainerStyle={styles.verticalPanContent}
+        showsVerticalScrollIndicator={false}
+        onScroll={onScroll({ y })}
+        scrollEventThrottle={1}
+        vertical
+      />
+      <Animated.ScrollView
+        style={styles.horizontalPan}
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ width: EMOJI_WIDTH * numberOfEmojis }}
+        onScroll={onScroll({ x })}
+        scrollEventThrottle={1}
+        snapToInterval={EMOJI_WIDTH}
+        decelerationRate="fast"
+        horizontal
+      />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  verticalPan: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    height: verticalPanHeight,
+  },
+  verticalPanContent: {
+    width: verticalPanHeight * 2 + EMOJIS_OFFSET * 2,
+  },
+  horizontalPan: {
+    position: "absolute",
+    top: verticalPanHeight,
+    left: 0,
+    height: horizontalPanHeight,
   },
 });
---
+++ season2/learnji/components/Emojis.tsx
@@ -0,0 +1,88 @@
+import * as React from "react";
+import {
+  View, Text, StyleSheet, Dimensions,
+} from "react-native";
+import { DangerZone } from "expo";
+
+const { Animated } = DangerZone;
+const { Value, event, interpolate } = Animated;
+const { width } = Dimensions.get("window");
+
+const padding = 8;
+const fontSize = 92;
+export const EMOJI_WIDTH = fontSize + padding * 2;
+export const EMOJIS_OFFSET = (width - EMOJI_WIDTH) / 2;
+
+interface Emoji {
+  "de": string;
+  "it": string;
+  "fr": string;
+  "es": string;
+  "en": string;
+  "pt": string;
+  "zh_Hant": string;
+  "ko": string;
+  "ja": string;
+}
+
+interface Emojis {
+  [emoji: string]: Emoji
+}
+
+interface EmojisProps {
+  emojis: Emojis;
+  x: Value;
+  y: Value;
+}
+
+export default ({
+  emojis, x, y,
+}: EmojisProps) => (
+  <View style={styles.container}>
+    <Animated.View style={styles.emojis}>
+      {
+        Object.keys(emojis).map((emoji, index) => {
+          const translateX = interpolate(x, {
+            inputRange: [0, EMOJI_WIDTH],
+            outputRange: [EMOJIS_OFFSET, EMOJIS_OFFSET + (-1 * EMOJI_WIDTH)],
+          });
+          const scale = interpolate(x, {
+            inputRange: [EMOJI_WIDTH * (index - 1), EMOJI_WIDTH * index, EMOJI_WIDTH * (index + 1)],
+            outputRange: [1, 1.3, 1],
+          });
+          return (
+            <Animated.View
+              key={emoji}
+              style={[styles.emoji, {
+                transform: [
+                  { translateX },
+                  { scale },
+                ],
+              }]}
+            >
+              <Text
+                style={{ fontSize }}
+              >
+                {emoji}
+              </Text>
+            </Animated.View>
+          );
+        })
+      }
+    </Animated.View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  emojis: {
+    flexDirection: "row",
+  },
+  emoji: {
+    width: EMOJI_WIDTH,
+    padding,
+  },
+});
