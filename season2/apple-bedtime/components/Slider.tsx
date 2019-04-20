a6410ee483e033d7c194c7aeff72443d96aa833f
William Candillon <wcandillon@gmail.com>
2019-04-20 21:28:32 +0200

    ⚙️

--- season2/apple-bedtime/components/Slider.tsx
+++ season2/apple-bedtime/components/Slider.tsx
@@ -1,7 +1,10 @@
 import * as React from "react";
 import { Dimensions } from "react-native";
-import { Svg } from "expo";
+import { Svg, DangerZone } from "expo";
 import SVGPath from "art/modes/svg/path";
+
+const { Animated } = DangerZone;
+const { Value } = Animated;
 
 const { width } = Dimensions.get("window");
 const size = width - 32;
@@ -10,6 +13,7 @@ const radius = size / 2 - padding;
 const {
   Defs, LinearGradient, Stop, Path,
 } = Svg;
+const AnimatedPath = Animated.createAnimatedComponent(Path);
 
 const d = SVGPath()
   .moveTo(padding, radius + padding)
@@ -22,21 +26,23 @@ const start = "#f7cd46";
 const end = "#ef9837";
 // interface SliderProps {}
 
-export default () => (
-  <Svg width={size} height={size}>
-    <Defs>
-      <LinearGradient id="grad" x1="0" y1="0" x2="100%" y2="0">
-        <Stop offset="0" stopColor={start} stopOpacity="1" />
-        <Stop offset="1" stopColor={end} stopOpacity="1" />
-      </LinearGradient>
-    </Defs>
-    <Path
-      strokeWidth={padding * 2}
-      stroke="url(#grad)"
-      fill="none"
-      strokeDasharray={Math.PI * radius}
-      strokeDashoffset={radius}
-      {...{ d }}
-    />
-  </Svg>
-);
+export default () => {
+  const strokeDasharray = new Value(Math.PI * radius);
+  const strokeDashoffset = new Value(radius);
+  return (
+    <Svg width={size} height={size}>
+      <Defs>
+        <LinearGradient id="grad" x1="0" y1="0" x2="100%" y2="0">
+          <Stop offset="0" stopColor={start} stopOpacity="1" />
+          <Stop offset="1" stopColor={end} stopOpacity="1" />
+        </LinearGradient>
+      </Defs>
+      <AnimatedPath
+        strokeWidth={padding * 2}
+        stroke="url(#grad)"
+        fill="none"
+        {...{ d, strokeDasharray, strokeDashoffset }}
+      />
+    </Svg>
+  );
+};
