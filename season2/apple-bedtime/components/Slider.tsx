9f92d20abd4d4049b03fa0a3f523a24273200dee
William Candillon <wcandillon@gmail.com>
2019-04-20 21:15:28 +0200

    ⚙️

--- season2/apple-bedtime/components/Slider.tsx
+++ season2/apple-bedtime/components/Slider.tsx
@@ -5,26 +5,27 @@ import SVGPath from "art/modes/svg/path";
 
 const { width } = Dimensions.get("window");
 const size = width - 32;
-const radius = size / 2;
+const padding = 25;
+const radius = size / 2 - padding;
 const {
   Defs, LinearGradient, Stop, Path,
 } = Svg;
-const padding = 25;
+
 const d = SVGPath()
-  .moveTo(0, radius)
-  .arcTo(radius, 0, radius)
-  .arcTo(radius * 2, radius, radius)
-  .arcTo(radius, radius * 2, radius)
-  .arcTo(0, radius, radius)
+  .moveTo(padding, radius + padding)
+  .arcTo(radius + padding, padding, radius)
+  .arcTo(radius * 2 + padding, radius + padding, radius)
+  .arcTo(radius + padding, radius * 2 + padding, radius)
+  .arcTo(padding, radius + padding, radius)
   .toSVG();
-const start = "#f5cb44";
+const start = "#f7cd46";
 const end = "#ef9837";
 // interface SliderProps {}
 
 export default () => (
   <Svg width={size} height={size}>
     <Defs>
-      <LinearGradient id="grad" x1="0" y1="0" x2={2 * radius} y2="0">
+      <LinearGradient id="grad" x1="0" y1="0" x2="100%" y2="0">
         <Stop offset="0" stopColor={start} stopOpacity="1" />
         <Stop offset="1" stopColor={end} stopOpacity="1" />
       </LinearGradient>
