42afc53a9ea941735247b3bfc135bbdf206b00a6
William Candillon <wcandillon@gmail.com>
2019-04-23 21:18:49 +0200

    ⚙️

--- season2/apple-bedtime/components/Slider.tsx
+++ season2/apple-bedtime/components/Slider.tsx
@@ -1,12 +1,13 @@
 import * as React from "react";
 import { Dimensions, View, StyleSheet } from "react-native";
-import { Svg, DangerZone } from "expo";
+import { Svg, DangerZone, Contacts } from "expo";
 
 import Cursor from "./Cursor";
+import { toDeg } from "./Math";
 
 const { Animated } = DangerZone;
 const {
-  Value, debug, block, multiply, sub,
+  Value, debug, block, multiply, sub, concat, round, cond, greaterThan,
 } = Animated;
 
 const { width } = Dimensions.get("window");
@@ -21,27 +22,37 @@ const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 export default () => {
   const start = new Value(0);
   const end = new Value(0);
-  const strokeDasharray = multiply(sub(end, start), radius);
-  const strokeDashoffset = multiply(sub(0, start), radius);
+  const circumference = radius * 2 * Math.PI;
+  const delta = cond(greaterThan(end, start), sub(end, start), sub(start, end));
+  const strokeDashoffset = multiply(delta, radius);
   return (
     <View style={styles.container}>
-      <Svg style={StyleSheet.absoluteFill}>
-        <Defs>
-          <LinearGradient id="grad" x1="0" y1="0" x2="100%" y2="0">
-            <Stop offset="0" stopColor="#f7cd46" />
-            <Stop offset="1" stopColor="#ef9837" />
-          </LinearGradient>
-        </Defs>
-        <AnimatedCircle
-          strokeWidth={padding * 2}
-          stroke="url(#grad)"
-          fill="none"
-          cx={size / 2}
-          cy={size / 2}
-          r={radius}
-          {...{ strokeDasharray, strokeDashoffset }}
-        />
-      </Svg>
+      <Animated.View style={{
+        ...StyleSheet.absoluteFillObject,
+        transform: [
+          { rotateZ: concat(sub(Math.PI * 2, cond(greaterThan(end, start), start, end)), "rad") },
+        ],
+      }}
+      >
+        <Svg style={StyleSheet.absoluteFill}>
+          <Defs>
+            <LinearGradient id="grad" x1="0" y1="0" x2="100%" y2="0">
+              <Stop offset="0" stopColor="#f7cd46" />
+              <Stop offset="1" stopColor="#ef9837" />
+            </LinearGradient>
+          </Defs>
+          <AnimatedCircle
+            strokeWidth={padding * 2}
+            stroke="url(#grad)"
+            fill="none"
+            cx={size / 2}
+            cy={size / 2}
+            r={radius}
+            strokeDasharray={`${circumference}, ${circumference}`}
+            {...{ strokeDashoffset }}
+          />
+        </Svg>
+      </Animated.View>
       <Cursor α={start} {...{ radius }} />
       <Cursor α={end} {...{ radius }} />
     </View>
