70c344b0c052b4bd2dac998918369d5693fdcd97
William Candillon <wcandillon@gmail.com>
2019-04-30 21:48:20 +0200

    ⚙️

--- season2/apple-appoftheday/App.tsx
+++ season2/apple-appoftheday/App.tsx
@@ -5,7 +5,23 @@ import {
 
 import App, { Apps } from "./components/App";
 
-const apps: Apps = [];
+const apps: Apps = [
+  {
+    id: "yoga",
+    title: "Namaste",
+    subtitle: "Best Yoga apps for the summer",
+    source: require("./assets/images/yoga.jpg"),
+    content: "",
+  },
+
+  {
+    id: "fitness",
+    title: "Get Fit",
+    subtitle: "Wear it while you work out",
+    source: require("./assets/images/fitness.jpg"),
+    content: "",
+  },
+];
 
 export default () => (
   <SafeAreaView style={styles.container}>
--- season2/apple-appoftheday/components/App.tsx
+++ season2/apple-appoftheday/components/App.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
-import { View, ImageSourcePropType } from "react-native";
+import {
+  View, ImageSourcePropType, StyleSheet, Dimensions, Image, Text,
+} from "react-native";
 
 export interface App {
   id: string;
   title: string;
   subtitle: string;
-  background: ImageSourcePropType;
+  source: ImageSourcePropType;
   content: string;
 }
 
@@ -15,10 +17,50 @@ interface AppProps {
   app: App;
 }
 
-export default class App extends React.PureComponent<AppProps> {
+const { width, height } = Dimensions.get("window");
+
+export default class extends React.PureComponent<AppProps> {
   render() {
+    const { app: { source, title, subtitle } } = this.props;
     return (
-      <View />
+      <View style={styles.container}>
+        <Image style={styles.image} {...{ source }} />
+        <View style={styles.content}>
+          <Text style={styles.title}>{title}</Text>
+          <Text style={styles.subtitle}>{subtitle}</Text>
+        </View>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: width - 32,
+    height: height / 2,
+    alignSelf: "center",
+    borderRadius: 8,
+    marginBottom: 16,
+  },
+  content: {
+    ...StyleSheet.absoluteFillObject,
+    padding: 16,
+    justifyContent: "space-between",
+  },
+  title: {
+    color: "white",
+    fontSize: 34,
+    lineHeight: 41,
+    fontWeight: "bold",
+  },
+  subtitle: {
+    color: "white",
+    fontSize: 18,
+  },
+  image: {
+    borderRadius: 8,
+    ...StyleSheet.absoluteFillObject,
+    width: undefined,
+    height: undefined,
+  },
+});
