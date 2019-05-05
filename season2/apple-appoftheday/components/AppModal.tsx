a2e4c5f29dc00e16b4b11252f54392c0c46bb941
William Candillon <wcandillon@gmail.com>
2019-05-05 13:47:08 +0200

    ⚙️

--- season2/apple-appoftheday/components/AppModal.tsx
+++ season2/apple-appoftheday/components/AppModal.tsx
@@ -1,60 +1,53 @@
 import * as React from "react";
 import {
-  View, StyleSheet,
+  View, StyleSheet, Dimensions,
 } from "react-native";
 import { DangerZone } from "expo";
+import { runSpring, binaryInterpolation } from "react-native-redash";
 
 import AppThumbnail from "./AppThumbnail";
 import { App, Position } from "./Model";
 
+const { width: wWidth } = Dimensions.get("window");
 const { Animated } = DangerZone;
-const { Value } = Animated;
+const {
+  Value, Clock, block, set,
+} = Animated;
 
 export interface AppModalProps {
   app: App;
   position: Position;
 }
 
-export default class AppModal extends React.PureComponent<AppModalProps> {
-  constructor(props: AppModalProps) {
-    super(props);
-    const {
-      position: {
-        x, y, width, height,
-      },
-    } = props;
-    this.x = new Value(x);
-    this.y = new Value(y);
-    this.width = new Value(width);
-    this.height = new Value(height);
-  }
-
-  x: typeof Value;
-
-  y: typeof Value;
-
-  width: typeof Value;
-
-  height: typeof Value;
-
-  render() {
-    const {
-      x, y, width, height,
-    } = this;
-    const { app } = this.props;
-    return (
-      <View style={StyleSheet.absoluteFill}>
-        <Animated.View
-          style={{
-            width,
-            height,
-            top: y,
-            left: x,
-          }}
-        >
-          <AppThumbnail {...{ app }} />
-        </Animated.View>
-      </View>
-    );
-  }
-}
+export default ({ app, position } : AppModalProps) => {
+  const clock = new Clock();
+  const spring = new Value(0);
+  const x = new Value(position.x);
+  const y = new Value(position.y);
+  const width = new Value(position.width);
+  const height = new Value(position.height);
+  return (
+    <View style={StyleSheet.absoluteFill}>
+      <Animated.Code>
+        {
+          () => block([
+            set(spring, runSpring(clock, 0, 1)),
+            set(x, binaryInterpolation(spring, x, 0)),
+            set(y, binaryInterpolation(spring, y, 0)),
+            set(width, binaryInterpolation(spring, width, wWidth)),
+          ])
+        }
+      </Animated.Code>
+      <Animated.View
+        style={{
+          width,
+          height,
+          top: y,
+          left: x,
+        }}
+      >
+        <AppThumbnail {...{ app }} />
+      </Animated.View>
+    </View>
+  );
+};
