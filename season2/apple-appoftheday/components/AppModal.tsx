b9046dbdfe52a9d9d703b43437fd3933ef69d299
William Candillon <wcandillon@gmail.com>
2019-05-05 15:17:35 +0200

    ⚙️

--- season2/apple-appoftheday/components/AppModal.tsx
+++ season2/apple-appoftheday/components/AppModal.tsx
@@ -3,19 +3,20 @@ import {
   View, StyleSheet, Dimensions,
 } from "react-native";
 import { DangerZone } from "expo";
-import { runSpring, binaryInterpolation, runTiming } from "react-native-redash";
+import { binaryInterpolation, runTiming } from "react-native-redash";
 
 import AppThumbnail from "./AppThumbnail";
 import { App, Position } from "./Model";
+import Content from "./Content";
 
 const { width: wWidth } = Dimensions.get("window");
 const { Animated, Easing } = DangerZone;
 const {
-  Value, Clock, block, set,
+  Value, Clock, block, set, cond, greaterThan,
 } = Animated;
 const timingConfig = {
   toValue: 1,
-  duration: 5000,
+  duration: 300,
   easing: Easing.inOut(Easing.ease),
 };
 export interface AppModalProps {
@@ -41,9 +42,9 @@ export default ({ app, position } : AppModalProps) => {
             set(driver, runTiming(clock, 0, timingConfig)),
             set(x, binaryInterpolation(driver, position.x, 0)),
             set(y, binaryInterpolation(driver, position.y, 0)),
+            set(width, binaryInterpolation(driver, position.width, wWidth)),
             set(contentX, binaryInterpolation(driver, position.x, 0)),
             set(contentY, binaryInterpolation(driver, position.y, position.height)),
-            set(width, binaryInterpolation(driver, width, wWidth)),
           ])
         }
       </Animated.Code>
@@ -58,7 +59,15 @@ export default ({ app, position } : AppModalProps) => {
           backgroundColor: "white",
         }}
       >
-
+      </Animated.View>
+      <Animated.View
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          marginTop: position.height,
+          opacity: cond(greaterThan(driver, 0.5), 1, 0),
+        }}
+      >
+        <Content />
       </Animated.View>
       <Animated.View
         style={{
---
+++ season2/apple-appoftheday/components/Content.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { Text, StyleSheet } from "react-native";
+import { DangerZone } from "expo";
+
+const { Animated } = DangerZone;
+const { Value } = Animated;
+
+interface ContentProps {
+  opacity: typeof Value;
+}
+
+export default () => (
+  <Animated.View style={styles.container}>
+    <Text style={styles.paragraph}>
+      <Text style={{ fontWeight: "bold" }}>Lorem ipsum</Text>
+      {" "}
+      dolor sit amet, consectetur adipiscing elit.
+      Proin nec dolor sed enim consequat consequat.
+    </Text>
+    <Text style={styles.paragraph}>
+      Phasellus porta risus id leo consequat fermentum.
+      Cras sed justo ac odio malesuada malesuada.
+    </Text>
+  </Animated.View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  paragraph: {
+    fontSize: 24,
+    marginBottom: 16,
+  },
+});
