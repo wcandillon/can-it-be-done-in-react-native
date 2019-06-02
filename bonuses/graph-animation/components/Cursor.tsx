409ebee0033fba8f52149037a4e203b571bdf310
William Candillon <wcandillon@gmail.com>
2019-06-02 13:04:21 +0200

    ⚙️

---
+++ bonuses/graph-animation/components/Cursor.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import { View, StyleSheet, Dimensions } from "react-native";
+import Animated from "react-native-reanimated";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+import { decay, getY, clamp } from "react-native-redash";
+
+const { Value, event, sub } = Animated;
+const TOUCH_SIZE = 200;
+const { width } = Dimensions.get("window");
+
+interface CursorProps {
+  d: string;
+  r: number;
+  borderWidth: number;
+  borderColor: string;
+}
+
+export default ({ d, r, borderWidth, borderColor }: CursorProps) => {
+  const radius = r + borderWidth / 2;
+  const translationX = new Value(0);
+  const velocityX = new Value(0);
+  const state = new Value(State.UNDETERMINED);
+  const onGestureEvent = event([
+    {
+      nativeEvent: {
+        translationX,
+        velocityX,
+        state
+      }
+    }
+  ]);
+  const x = clamp(decay(translationX, state, velocityX), 0, width);
+  const y = getY(d, x);
+  const translateX: any = sub(x, TOUCH_SIZE / 2);
+  const translateY: any = sub(y, TOUCH_SIZE / 2);
+  return (
+    <View style={StyleSheet.absoluteFill}>
+      <PanGestureHandler
+        onHandlerStateChange={onGestureEvent}
+        {...{ onGestureEvent }}
+      >
+        <Animated.View
+          style={{
+            transform: [{ translateX }, { translateY }],
+            width: TOUCH_SIZE,
+            height: TOUCH_SIZE,
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <View
+            style={{
+              width: radius * 2,
+              height: radius * 2,
+              borderRadius: radius,
+              borderColor,
+              borderWidth
+            }}
+          />
+        </Animated.View>
+      </PanGestureHandler>
+    </View>
+  );
+};
--- bonuses/graph-animation/components/Graph.tsx
+++ bonuses/graph-animation/components/Graph.tsx
@@ -4,11 +4,7 @@ import Svg, { Path, Defs, LinearGradient, Stop } from "react-native-svg";
 import { scaleTime, scaleLinear } from "d3-scale";
 import * as shape from "d3-shape";
 
-const φ = (1 + Math.sqrt(5)) / 2;
-const { width, height: wHeight } = Dimensions.get("window");
-const height = (1 - 1 / φ) * wHeight;
-const strokeWidth = 5;
-const padding = strokeWidth / 2;
+import Cursor from "./Cursor";
 
 interface DataPoint {
   date: number;
@@ -19,6 +15,13 @@ interface GraphProps {
   data: DataPoint[];
 }
 
+const φ = (1 + Math.sqrt(5)) / 2;
+const { width, height: wHeight } = Dimensions.get("window");
+const height = (1 - 1 / φ) * wHeight;
+const strokeWidth = 4;
+const padding = strokeWidth / 2;
+const CURSOR_RADIUS = 8;
+const STROKE_WIDTH = CURSOR_RADIUS / 2;
 const getDomain = (domain: number[]) => [
   Math.min(...domain),
   Math.max(...domain)
@@ -52,6 +55,12 @@ export default ({ data }: GraphProps) => {
         />
         <Path fill="transparent" stroke="#3977e3" {...{ d, strokeWidth }} />
       </Svg>
+      <Cursor
+        r={CURSOR_RADIUS}
+        borderWidth={STROKE_WIDTH}
+        borderColor="#3977e3"
+        {...{ d }}
+      />
     </View>
   );
 };
