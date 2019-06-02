bc30fb7cca1ccfbad4c0d6e805deb0ff79b66605
William Candillon <wcandillon@gmail.com>
2019-06-02 12:40:24 +0200

    🎬

--- bonuses/graph-animation/App.tsx
+++ bonuses/graph-animation/App.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+import Graph from "./components/Graph";
+
+const data = [
+  { date: new Date(2018, 9, 1).getTime(), value: 0 },
+  { date: new Date(2018, 9, 16).getTime(), value: 0 },
+  { date: new Date(2018, 9, 17).getTime(), value: 200 },
+  { date: new Date(2018, 10, 1).getTime(), value: 200 },
+  { date: new Date(2018, 10, 2).getTime(), value: 300 },
+  { date: new Date(2018, 10, 5).getTime(), value: 300 }
+];
+
+export default () => (
+  <View style={styles.container}>
+    <Graph {...{ data }} />
+  </View>
+);
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
