19b02660f27d336a74a0587b3e0d2b202d2b0dfe
William Candillon <wcandillon@gmail.com>
2019-06-05 13:06:54 +0200

    💄

--- season2/spotify-header/components/Content.tsx
+++ season2/spotify-header/components/Content.tsx
@@ -1,29 +1,31 @@
 import * as React from "react";
 import {
-  StyleSheet, View, Text, ScrollView, Platform,
+  StyleSheet, View,
 } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 import Animated from "react-native-reanimated";
 import { onScroll } from "react-native-redash";
 
-import { Album, MAX_HEADER_HEIGHT, HEADER_DELTA } from "./Model";
+import {
+  Album, MAX_HEADER_HEIGHT, MIN_HEADER_HEIGHT,
+} from "./Model";
 import Track from "./Track";
-import ShufflePlay from "./ShufflePlay";
+import ShufflePlay, { BUTTON_HEIGHT } from "./ShufflePlay";
+import Header from "./Header";
 
 interface ContentProps {
   album: Album;
   y: Animated.Value<number>;
-  btnOpacity: Animated.Value<number>;
 }
 
 const {
-  interpolate, Extrapolate, cond, eq,
+  interpolate, Extrapolate,
 } = Animated;
 
-export default ({ album: { artist, tracks }, y, btnOpacity }: ContentProps) => {
+export default ({ album: { artist, tracks }, y }: ContentProps) => {
   const height = interpolate(y, {
-    inputRange: [-MAX_HEADER_HEIGHT, 0],
-    outputRange: [0, MAX_HEADER_HEIGHT],
+    inputRange: [-MAX_HEADER_HEIGHT, -32],
+    outputRange: [0, MAX_HEADER_HEIGHT + 32],
     extrapolate: Extrapolate.CLAMP,
   });
   const opacity = interpolate(y, {
@@ -37,6 +39,7 @@ export default ({ album: { artist, tracks }, y, btnOpacity }: ContentProps) => {
       style={styles.container}
       showsVerticalScrollIndicator={false}
       scrollEventThrottle={1}
+      stickyHeaderIndices={[1]}
     >
       <View style={styles.header}>
         <Animated.View
@@ -53,14 +56,11 @@ export default ({ album: { artist, tracks }, y, btnOpacity }: ContentProps) => {
           <Animated.Text style={[styles.artist, { opacity }]}>{artist}</Animated.Text>
         </View>
       </View>
+      <View style={{ zIndex: 1, marginTop: -BUTTON_HEIGHT }}>
+        <Header {...{ y, artist }} />
+        <ShufflePlay />
+      </View>
       <View style={styles.tracks}>
-        {
-          Platform.OS === "ios" && (
-            <Animated.View style={{ opacity: cond(eq(btnOpacity, 1), 0, 1) }}>
-              <ShufflePlay />
-            </Animated.View>
-          )
-        }
         {
           tracks.map((track, key) => (
             <Track
@@ -77,9 +77,10 @@ export default ({ album: { artist, tracks }, y, btnOpacity }: ContentProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: MIN_HEADER_HEIGHT - BUTTON_HEIGHT / 2,
   },
   header: {
-    height: MAX_HEADER_HEIGHT,
+    height: MAX_HEADER_HEIGHT - BUTTON_HEIGHT,
   },
   gradient: {
     position: "absolute",
