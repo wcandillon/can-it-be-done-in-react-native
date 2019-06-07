92b01c3b40059993ec9b162b7a3b9fa126cb890f
William Candillon <wcandillon@gmail.com>
2019-06-07 20:44:16 +0200

    ⚙️

--- season2/monzo-card-selection/components/CardSelection2.tsx
+++ season2/monzo-card-selection/components/CardSelection2.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { View, Text, StyleSheet, SafeAreaView } from "react-native";
 import { RectButton } from "react-native-gesture-handler";
 import Animated from "react-native-reanimated";
-import { runSpring } from "react-native-redash";
+import { runSpring, bInterpolate } from "react-native-redash";
 
 import Card, { Card as CardModel, CARD_WIDTH } from "./Card";
 import CheckIcon from "./CheckIcon";
@@ -22,6 +22,7 @@ const {
   multiply,
   cond,
   eq,
+  clockRunning,
   onChange,
   debug
 } = Animated;
@@ -32,20 +33,25 @@ export default ({ cards }: CardSelectionProps) => {
   const selectedCard = new Value(INITIAL_INDEX);
   const cardZIndexes = cards.map((_, index) => new Value(index));
   const cardRotates = cards.map(() => new Value(0));
+  const spring = new Value(0);
   const cardRotatesClock = new Clock();
   const translationX = new Value(CARD_WIDTH);
-  const translationXClock = new Clock();
-  const selectCard = (index: number) => {
-    selectedCard.setValue(index);
-  };
+  const selectCard = (index: number) => selectedCard.setValue(index);
   useCode(
     block([
-      cond(
-        eq(selectedCard, INITIAL_INDEX),
-        set(cardRotates[0], runSpring(cardRotatesClock, 0, -15)),
-        set(translationX, runSpring(translationXClock, translationX, 0))
-      ),
-      set(cardRotates[2], multiply(cardRotates[0], -1))
+      set(cardRotates[2], multiply(cardRotates[0], -1)),
+      cond(eq(selectedCard, INITIAL_INDEX), [
+        set(spring, runSpring(cardRotatesClock, 0, 1)),
+        set(cardRotates[0], bInterpolate(spring, 0, -15)),
+        set(cardRotates[2], bInterpolate(spring, 0, 15))
+      ]),
+      cond(eq(selectedCard, 0), [
+        set(spring, runSpring(cardRotatesClock, 0, 1)),
+        set(cardRotates[0], bInterpolate(spring, 0, -7.5)),
+        set(cardRotates[1], bInterpolate(spring, 0, 7.5)),
+        set(cardRotates[2], bInterpolate(spring, 15, 0)),
+        set(translationX, bInterpolate(spring, translationX, 0))
+      ])
     ]),
     [cards]
   );
@@ -65,8 +71,7 @@ export default ({ cards }: CardSelectionProps) => {
                 transform: [
                   { translateX: multiply(translationX, -1) },
                   { rotateZ },
-                  { translateX: translationX },
-                  { translateY: 0 }
+                  { translateX: translationX }
                 ]
               }}
             >
