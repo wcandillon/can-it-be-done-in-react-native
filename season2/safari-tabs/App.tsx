027c03c31603921f2ff0abd008e3f3c810a8b0fc
William Candillon <wcandillon@gmail.com>
2019-06-21 21:56:48 +0200

    ⚙️

--- season2/safari-tabs/App.tsx
+++ season2/safari-tabs/App.tsx
@@ -6,7 +6,10 @@ import Tabs, { ITabs } from "./components/Tabs";
 const tabs: ITabs = [
   { id: "screen1", screen: require("./assets/screens/screen1.jpeg") },
   { id: "screen2", screen: require("./assets/screens/screen2.jpeg") },
-  { id: "screen3", screen: require("./assets/screens/screen3.jpeg") }
+  { id: "screen3", screen: require("./assets/screens/screen3.jpeg") },
+
+  { id: "screen4", screen: require("./assets/screens/screen4.jpg") },
+  { id: "screen5", screen: require("./assets/screens/screen5.jpg") }
 ];
 
 export default () => (
--- season2/safari-tabs/components/Tabs.tsx
+++ season2/safari-tabs/components/Tabs.tsx
@@ -1,10 +1,13 @@
 import * as React from "react";
-import { StyleSheet, StatusBar } from "react-native";
+import { StyleSheet, StatusBar, Dimensions, View } from "react-native";
 import Animated from "react-native-reanimated";
 
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+import { decay, clamp } from "react-native-redash";
 import Tab, { ITab, OVERVIEW } from "./Tab";
 
-const { Value } = Animated;
+const { height } = Dimensions.get("window");
+const { Value, event, cond, eq, interpolate } = Animated;
 
 export type ITabs = ITab[];
 
@@ -15,14 +18,37 @@ interface TabsProps {
 export default ({ tabs }: TabsProps) => {
   const transition = new Value(0);
   const selectedTab = new Value(OVERVIEW);
+  const translationY = new Value(0);
+  const velocityY = new Value(0);
+  const state = new Value(State.UNDETERMINED);
+  const onGestureEvent = event([
+    {
+      nativeEvent: {
+        translationY,
+        velocityY,
+        state
+      }
+    }
+  ]);
+  const y = clamp(decay(translationY, state, velocityY), -150 * tabs.length, 0);
+  const translateY = interpolate(transition, {
+    inputRange: [0, 1],
+    outputRange: [0, y]
+  });
   return (
     <>
       <StatusBar hidden />
-      <Animated.View style={styles.container}>
-        {tabs.map((tab, index) => (
-          <Tab key={tab.id} {...{ tab, transition, selectedTab, index }} />
-        ))}
-      </Animated.View>
+      <View style={styles.background} />
+      <PanGestureHandler
+        onHandlerStateChange={onGestureEvent}
+        {...{ onGestureEvent }}
+      >
+        <Animated.View style={{ flex: 1, transform: [{ translateY }] }}>
+          {tabs.map((tab, index) => (
+            <Tab key={tab.id} {...{ tab, transition, selectedTab, index }} />
+          ))}
+        </Animated.View>
+      </PanGestureHandler>
     </>
   );
 };
@@ -30,6 +56,10 @@ export default ({ tabs }: TabsProps) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "black"
+  },
+  background: {
+    ...StyleSheet.absoluteFillObject,
     backgroundColor: "black"
   }
 });
