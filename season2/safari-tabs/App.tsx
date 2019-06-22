9dfc5da69ae77c82ee8a531102a57922bf2d22e4
William Candillon <wcandillon@gmail.com>
2019-06-22 13:32:59 +0200

    ⚙️

--- season2/safari-tabs/App.tsx
+++ season2/safari-tabs/App.tsx
@@ -4,10 +4,9 @@ import LoadAssets from "./components/LoadAssets";
 import Tabs, { ITabs } from "./components/Tabs";
 
 const tabs: ITabs = [
-  { id: "screen1", screen: require("./assets/screens/screen1.jpeg") },
-  { id: "screen2", screen: require("./assets/screens/screen2.jpeg") },
-  { id: "screen3", screen: require("./assets/screens/screen3.jpeg") },
-
+  { id: "screen1", screen: require("./assets/screens/screen1.jpg") },
+  { id: "screen2", screen: require("./assets/screens/screen2.jpg") },
+  { id: "screen3", screen: require("./assets/screens/screen3.jpg") },
   { id: "screen4", screen: require("./assets/screens/screen4.jpg") },
   { id: "screen5", screen: require("./assets/screens/screen5.jpg") }
 ];
