3a802b453ee4faf778ef84684883d31bd1b015e1
William Candillon <wcandillon@gmail.com>
2019-06-23 10:50:32 +0200

    ⚙️

--- season2/safari-tabs/components/Tab.tsx
+++ season2/safari-tabs/components/Tab.tsx
@@ -1,7 +1,7 @@
 import React, { useMemo } from "react";
 import { Dimensions, StyleSheet, TouchableWithoutFeedback } from "react-native";
 import Animated, { Easing } from "react-native-reanimated";
-import { translateZ, runTiming } from "react-native-redash";
+import { translateZ, runTiming, bInterpolate } from "react-native-redash";
 
 import Content from "./Content";
 import { ITabs } from "./Tabs";
@@ -49,14 +49,9 @@ export default ({
   const margin = selectedTab === OVERVIEW ? 16 : 0;
   const position = index > selectedTab ? height : 0;
   const top = selectedTab === OVERVIEW ? index * 150 : position;
-  const rotateX = useMemo(
-    () =>
-      interpolate(transition, {
-        inputRange: [0, 1],
-        outputRange: [0, -Math.PI / 6]
-      }),
-    [transition]
-  );
+  const rotateX = useMemo(() => bInterpolate(transition, 0, -Math.PI / 6), [
+    transition
+  ]);
   const z = useMemo(() => multiply(H, sin(abs(rotateX))), [H, rotateX]);
   return (
     <TouchableWithoutFeedback {...{ onPress }}>
