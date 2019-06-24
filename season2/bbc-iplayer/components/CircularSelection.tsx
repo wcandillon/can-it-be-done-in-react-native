c10fc3a3175026a92a205fc8b00ea1742078d4f7
William Candillon <wcandillon@gmail.com>
2019-06-24 18:36:00 +0200

    ⚙️

--- season2/bbc-iplayer/components/CircularSelection.tsx
+++ season2/bbc-iplayer/components/CircularSelection.tsx
@@ -1,28 +1,30 @@
 import * as React from 'react';
-import { View } from 'react-native';
+import { View, Dimensions } from 'react-native';
 import Svg, {Path} from "react-native-svg";
 
 import {Channel} from "./Model";
 
-const ICON_SIZE = 40;
+const {width, height} = Dimensions.get("window");
+// const φ = (1 + Math.sqrt(5)) / 2;
+const D = (width * 1.4);
+const R = D / 2;
+const circle = (r: number, cx: number, cy: number) => `M ${cx - R}, ${cy}
+a ${r},${r} 0 1,0 ${r * 2},0
+a ${r},${r} 0 1,0 ${-r * 2},0`;
 
 interface CircularSelectionProps {
-  channels: Channel[]
+  channels: Channel[];
 }
 
 export default ({channels}: CircularSelectionProps) => {
-  const circumference = channels.length * ICON_SIZE;
-  const r = circumference / (2 * Math.PI);
+  const l = Math.sin(Math.PI / channels.length);
+  const r = (R * l) / (1 - l);
   const cx = r;
   const cy = r;
-  const d = `M ${cx - r}, ${cy}
-  a ${r},${r} 0 1,0 ${r * 2},0
-  a ${r},${r} 0 1,0 ${-r * 2},0`;
+  const d = circle(R, width / 2, height / 2);
   return (
-    <View>
-      <Svg style={{ flex: 1 }}>
+      <Svg width={width} height={width}>
         <Path {...{d}} />
       </Svg>
-    </View>
   );
 }
