da5cc32dbc6f48e73ee8665a03147713fef18ec0
William Candillon <wcandillon@gmail.com>
2019-06-24 21:16:04 +0200

    ⚙️

--- season2/bbc-iplayer/components/CircularSelection.tsx
+++ season2/bbc-iplayer/components/CircularSelection.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, View, StyleSheet } from "react-native";
 import Svg, { Path, Defs, LinearGradient, Stop } from "react-native-svg";
 
 import { Channel } from "./Model";
@@ -10,6 +10,12 @@ const R = D / 2;
 const circle = (r: number, cx: number, cy: number) => `M ${cx - r}, ${cy}
 a ${r},${r} 0 1,0 ${r * 2},0
 a ${r},${r} 0 1,0 ${-r * 2},0`;
+const styles = StyleSheet.create({
+  container: {
+    width,
+    height: width / 1.4
+  }
+});
 
 interface CircularSelectionProps {
   channels: Channel[];
@@ -21,14 +27,21 @@ export default ({ channels }: CircularSelectionProps) => {
   const R1 = R + 2 * r;
   const d = circle(R1, R1 - (R1 - width / 2), R1);
   return (
-    <Svg width={width} height={width / 1.5}>
-      <Defs>
-        <LinearGradient id="bg" x1="0%" y1="0%" x2="0%" y2="50%">
-          <Stop offset="0" stopColor="#353637" />
-          <Stop offset="1" stopColor="#1c1d1e" />
-        </LinearGradient>
-      </Defs>
-      <Path fill="#3498db" {...{ d }} />
-    </Svg>
+    <View style={styles.container}>
+      <Svg style={StyleSheet.absoluteFill}>
+        <Defs>
+          <LinearGradient id="bg" x1="0%" y1="0%" x2="0%" y2="50%">
+            <Stop offset="0" stopColor="#353637" />
+            <Stop offset="1" stopColor="#1c1d1e" />
+          </LinearGradient>
+        </Defs>
+        <Path fill="#3498db" {...{ d }} />
+      </Svg>
+      <View style={StyleSheet.absoluteFill}>
+        {channels.map((channel, index) => (
+          <View key={index} />
+        ))}
+      </View>
+    </View>
   );
 };
