8f50422386080a9a218d8526aa72e6b551b14f90
William Candillon <wcandillon@gmail.com>
2019-06-27 16:43:27 +0200

    ⚙️

--- season2/bbc-iplayer/components/PanGesture.tsx
+++ season2/bbc-iplayer/components/PanGesture.tsx
@@ -1,10 +1,83 @@
 import * as React from "react";
 import { StyleSheet } from "react-native";
 import Animated from "react-native-reanimated";
-import { onGestureEvent, decay } from "react-native-redash";
+import { onGestureEvent, runSpring, snapPoint } from "react-native-redash";
 import { State, PanGestureHandler } from "react-native-gesture-handler";
 
-const { Value, useCode, set, modulo, divide, diff, sub } = Animated;
+const {
+  Clock,
+  Value,
+  useCode,
+  set,
+  modulo,
+  divide,
+  diff,
+  sub,
+  add,
+  block,
+  cond,
+  eq,
+  stopClock
+} = Animated;
+
+export const spring = (
+  translation: Animated.Value<number>,
+  velocity: Animated.Value<number>,
+  state: Animated.Value<State>,
+  ratio: number
+) => {
+  const springedValue = new Value(0);
+  const offset = new Value(0);
+  const clock = new Clock();
+  const rerunSpring = new Value(0);
+  // http://chenglou.github.io/react-motion/demos/demo5-spring-parameters-chooser/
+  const springConfig = {
+    toValue: new Value(0),
+    damping: 15,
+    mass: 1,
+    stiffness: 150,
+    overshootClamping: false,
+    restSpeedThreshold: 0.001,
+    restDisplacementThreshold: 0.001
+  };
+  return block([
+    cond(
+      eq(state, State.END),
+      [
+        set(
+          springedValue,
+          runSpring(
+            clock,
+            add(translation, offset),
+            snapPoint(add(translation, offset), velocity, [
+              sub(
+                add(translation, offset),
+                modulo(add(translation, offset), ratio)
+              ),
+              add(
+                sub(
+                  add(translation, offset),
+                  modulo(add(translation, offset), ratio)
+                ),
+                ratio
+              )
+            ]),
+            springConfig
+          )
+        )
+      ],
+      [
+        stopClock(clock),
+        cond(eq(state, State.BEGAN), [
+          set(rerunSpring, 0),
+          set(offset, sub(springedValue, translation))
+        ]),
+        set(springedValue, add(translation, offset))
+      ]
+    ),
+    springedValue
+  ]);
+};
 
 interface PanGestureProps {
   index: Animated.Value<number>;
@@ -21,7 +94,7 @@ export default ({ index, ratio, length }: PanGestureProps) => {
     velocityX,
     state
   });
-  const translateX = decay(translationX, state, velocityX);
+  const translateX = spring(translationX, velocityX, state, ratio);
   const increment = divide(diff(translateX), ratio);
   useCode(set(index, modulo(sub(index, increment), length)), []);
   return (
