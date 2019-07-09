a3d2ad3c6af64a253191a5e11d12c48ce2b6c6b8
William Candillon <wcandillon@gmail.com>
2019-07-09 12:55:36 +0200

    🧮5min circular progress (#28)

--- bonuses/circular-progress/components/CircularProgress2.tsx
+++ bonuses/circular-progress/components/CircularProgress2.tsx
@@ -1,68 +1,60 @@
 import * as React from "react";
 import { Dimensions, StyleSheet } from "react-native";
-import { DangerZone, Svg } from "expo";
-
-import { drawArc } from "./SVGHelpers";
-
-const { Animated } = DangerZone;
-const {
-  Value, interpolate, multiply,
-} = Animated;
-const {
+import Svg, {
   Defs, LinearGradient, Stop, Path,
-} = Svg;
+} from "react-native-svg";
+import Animated from "react-native-reanimated";
 
-const { PI } = Math;
+const { interpolate, multiply } = Animated;
 const { width } = Dimensions.get("window");
 const size = width - 32;
 const strokeWidth = 50;
-const radius = (size - strokeWidth) / 2;
 const AnimatedPath = Animated.createAnimatedComponent(Path);
-
+const { PI, cos, sin } = Math;
+const r = (size - strokeWidth) / 2;
+const cx = size / 2;
+const cy = size / 2;
 const A = PI + PI * 0.4;
-const startAngle = PI + (A - PI) / 2;
-const endAngle = PI * 2 - (A - PI) / 2;
-const d = drawArc({
-  x: radius,
-  y: radius,
-  radius,
-  startAngle,
-  endAngle,
-  strokeWidth,
-});
-
-type Value = typeof Value;
+const startAngle = PI + PI * 0.2;
+const endAngle = 2 * PI - PI * 0.2;
+// A rx ry x-axis-rotation large-arc-flag sweep-flag x y
+const x1 = cx - r * cos(startAngle);
+const y1 = -r * sin(startAngle) + cy;
+const x2 = cx - r * cos(endAngle);
+const y2 = -r * sin(endAngle) + cy;
+const d = `M ${x1} ${y1} A ${r} ${r} 0 1 0 ${x2} ${y2}`;
 
 interface CircularPogressProps {
-  progress: Value;
+  progress: Animated.Value<number>;
 }
 
 export default ({ progress }: CircularPogressProps) => {
-  const length = radius * A;
+  const circumference = r * A;
   const α = interpolate(progress, {
     inputRange: [0, 1],
     outputRange: [0, A],
   });
-  const strokeDashoffset = multiply(α, radius);
+  const strokeDashoffset = multiply(α, r);
   return (
-    <Svg width={size} height={size} style={styles.container}>
+    <Svg width={size} height={size}>
       <Defs>
         <LinearGradient id="grad" x1="0" y1="0" x2="100%" y2="0">
           <Stop offset="0" stopColor="#f7cd46" />
           <Stop offset="1" stopColor="#ef9837" />
         </LinearGradient>
       </Defs>
+      <Path
+        stroke="white"
+        fill="none"
+        strokeDasharray={`${circumference}, ${circumference}`}
+        {...{ d, strokeWidth }}
+      />
       <AnimatedPath
         stroke="url(#grad)"
         fill="none"
-        strokeDasharray={`${length}, ${length}`}
-        {...{ strokeDashoffset, strokeWidth, d }}
+        strokeDasharray={`${circumference}, ${circumference}`}
+        {...{ d, strokeDashoffset, strokeWidth }}
       />
     </Svg>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-  },
-});
