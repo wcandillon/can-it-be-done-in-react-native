e63a90944eed99e845995f20bc62b696537f70ef
William Candillon <wcandillon@gmail.com>
2019-08-30 15:47:26 +0200

    ⚙️

--- season2/spotify-player/App.tsx
+++ season2/spotify-player/App.tsx
@@ -1,19 +1,6 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import BottomTab from "./components/BottomTab";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+  return <BottomTab />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ season2/spotify-player/components/BottomTab.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Dimensions, StyleSheet } from "react-native";
+import Animated from "react-native-reanimated";
+import { PanGestureHandler, State } from "react-native-gesture-handler";
+import { clamp, onGestureEvent, withSpring } from "react-native-redash";
+
+const { height } = Dimensions.get("window");
+const SNAP_TOP = 0;
+const SNAP_BOTTOM = height - 64;
+const config = {
+  damping: 15,
+  mass: 1,
+  stiffness: 150,
+  overshootClamping: false,
+  restSpeedThreshold: 0.1,
+  restDisplacementThreshold: 0.1
+};
+const { Value, cond, eq } = Animated;
+const withClamp = (
+  value: Animated.Node<number>,
+  state: Animated.Value<State>,
+  min: Animated.Adaptable<number>,
+  max: Animated.Adaptable<number>
+) => cond(eq(state, State.ACTIVE), clamp(value, min, max), value);
+
+const styles = StyleSheet.create({
+  playerSheet: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "cyan"
+  }
+});
+
+export default () => {
+  const translationY = new Value(0);
+  const velocityY = new Value(0);
+  const state = new Value(State.UNDETERMINED);
+  const gestureHandler = onGestureEvent({
+    state,
+    translationY,
+    velocityY
+  });
+  const translateY = withClamp(
+    withSpring({
+      value: translationY,
+      velocity: velocityY,
+      state,
+      snapPoints: [SNAP_TOP, SNAP_BOTTOM],
+      config
+    }),
+    state,
+    SNAP_TOP,
+    SNAP_BOTTOM
+  );
+  return (
+    <PanGestureHandler {...gestureHandler}>
+      <Animated.View
+        style={[styles.playerSheet, { transform: [{ translateY }] }]}
+      />
+    </PanGestureHandler>
+  );
+};
