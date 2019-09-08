1b5eb5abc4cdd137f8183687662945de11d18a68
William Candillon <wcandillon@gmail.com>
2019-09-08 14:49:59 +0200

    ⚙️

--- season3/src/Things/ScrollView.tsx
+++ season3/src/Things/ScrollView.tsx
@@ -2,15 +2,88 @@ import React, { ReactNode, useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { PanGestureHandler, State } from "react-native-gesture-handler";
 import Animated from "react-native-reanimated";
-import { onGestureEvent, withDecay } from "react-native-redash";
 import { useMemoOne } from "use-memo-one";
+import { panGestureHandlerWithY } from "../components/AnimationHelpers";
 
-const { Value, diffClamp } = Animated;
+const {
+  Value,
+  Clock,
+  eq,
+  stopClock,
+  startClock,
+  not,
+  decay: reDecay,
+  clockRunning,
+  set,
+  neq,
+  diffClamp,
+  multiply,
+  add,
+  and,
+  greaterOrEq,
+  lessOrEq,
+  cond,
+  divide,
+  diff,
+  sub,
+  pow,
+  block,
+  debug,
+  exp
+} = Animated;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1
   }
 });
+
+interface WithScrollParams {
+  value: Animated.Adaptable<number>;
+  velocity: Animated.Adaptable<number>;
+  state: Animated.Value<State>;
+  offset?: Animated.Value<number>;
+  deceleration?: number;
+  // lowerBound: number;
+  // upperBound: number;
+}
+
+export const withScroll = (config: WithScrollParams) => {
+  const { value, velocity, state, offset, deceleration } = {
+    offset: new Value(0),
+    deceleration: 0.998,
+    ...config
+  };
+  const clock = new Clock();
+  const decayState = {
+    finished: new Value(0),
+    velocity: new Value(0),
+    position: new Value(0),
+    time: new Value(0)
+  };
+
+  const isDecayInterrupted = and(eq(state, State.BEGAN), clockRunning(clock));
+  const finishDecay = [set(offset, decayState.position), stopClock(clock)];
+
+  return block([
+    cond(isDecayInterrupted, finishDecay),
+    cond(neq(state, State.END), [
+      set(decayState.finished, 0),
+      set(decayState.position, add(offset, value))
+    ]),
+    cond(eq(state, State.END), [
+      cond(and(not(clockRunning(clock)), not(decayState.finished)), [
+        set(decayState.velocity, velocity),
+        set(decayState.time, 0),
+        startClock(clock)
+      ]),
+      reDecay(clock, decayState, { deceleration }),
+      cond(decayState.finished, finishDecay)
+    ]),
+    decayState.position
+  ]);
+};
+
 interface ScrollViewProps {
   children: ReactNode;
 }
@@ -18,31 +91,21 @@ interface ScrollViewProps {
 export default ({ children }: ScrollViewProps) => {
   const [containerHeight, setContainerHeight] = useState(0);
   const [contentHeight, setContentHeight] = useState(0);
-  const { translationY, velocityY, state } = useMemoOne(
-    () => ({
-      translationY: new Value(0),
-      velocityY: new Value(0),
-      state: new Value(State.UNDETERMINED)
-    }),
-    []
-  );
-  const { gestureHandler, decay } = useMemoOne(
-    () => ({
-      gestureHandler: onGestureEvent({
-        translationY,
-        velocityY,
-        state
-      }),
-      decay: withDecay({
-        value: translationY,
-        velocity: velocityY,
-        state
-      })
-    }),
+  const { gestureHandler, translationY, velocityY, state } = useMemoOne(
+    () => panGestureHandlerWithY(),
     []
   );
   const lowerBound = -1 * (contentHeight - containerHeight);
-  const translateY = diffClamp(decay, lowerBound, 0);
+  const upperBound = 0;
+  const translateY = diffClamp(
+    withScroll({
+      value: translationY,
+      velocity: velocityY,
+      state
+    }),
+    lowerBound,
+    upperBound
+  );
   return (
     <View
       style={styles.container}
---
+++ season3/src/components/AnimationHelpers.ts
@@ -0,0 +1,23 @@
+import Animated from "react-native-reanimated";
+import { State } from "react-native-gesture-handler";
+import { onGestureEvent } from "react-native-redash";
+
+const { Value } = Animated;
+
+// eslint-disable-next-line import/prefer-default-export
+export const panGestureHandlerWithY = () => {
+  const translationY = new Value(0);
+  const velocityY = new Value(0);
+  const state = new Value(State.UNDETERMINED);
+  const gestureHandler = onGestureEvent({
+    translationY,
+    velocityY,
+    state
+  });
+  return {
+    translationY,
+    state,
+    velocityY,
+    gestureHandler
+  };
+};
