33761964bbc139b5b0d124fe877fe06bea7fc370
William Candillon <wcandillon@gmail.com>
2019-09-28 10:51:14 +0200

    ⚙️

--- season3/src/LiquidSwipe/LiquidSwipe.tsx
+++ season3/src/LiquidSwipe/LiquidSwipe.tsx
@@ -3,7 +3,7 @@ import { Dimensions, StyleSheet, View } from "react-native";
 import Animated from "react-native-reanimated";
 
 import { PanGestureHandler, State } from "react-native-gesture-handler";
-import { onGestureEvent, withOffset } from "react-native-redash";
+import { onGestureEvent, withOffset, withSpring } from "react-native-redash";
 import Weave from "./Weave";
 import {
   initialWaveCenter,
@@ -18,41 +18,43 @@ const { Value, interpolate, useCode, debug } = Animated;
 export default () => {
   const translationX = new Value(0);
   const translationY = new Value(0);
+  const velocityX = new Value(0);
+  const velocityY = new Value(0);
   const state = new Value(State.UNDETERMINED);
   const gestureHandler = onGestureEvent({
     translationX,
     translationY,
     state
   });
-  const offsetY = new Value(initialWaveCenter);
-  const translateX = withOffset(translationX, state);
-  const translateY = withOffset(translationY, state, offsetY);
+  const translateX = withSpring({
+    value: translationX,
+    velocity: velocityX,
+    state,
+    snapPoints: [-width, 0]
+  });
+  const translateY = withSpring({
+    value: translationY,
+    velocity: velocityY,
+    state,
+    snapPoints: [initialWaveCenter],
+    offset: new Value(initialWaveCenter)
+  });
   const progress = interpolate(translateX, {
     inputRange: [-width, 0],
-    outputRange: [0.4, 0]
+    outputRange: [1, 0]
   });
   const horRadius = waveHorRadius(progress);
   const vertRadius = waveVertRadius(progress);
-  useCode(debug("progress", progress), []);
+  const sWidth = sideWidth(progress);
   return (
-    <View style={{ flex: 1 }}>
-      <PanGestureHandler {...gestureHandler}>
-        <Animated.View>
-          <Weave
-            centerY={translateY}
-            sideWidth={sideWidth(progress)}
-            {...{ horRadius, vertRadius }}
-          />
-        </Animated.View>
-      </PanGestureHandler>
-      <Animated.View
-        pointerEvents="none"
-        style={{
-          ...StyleSheet.absoluteFillObject,
-          transform: [{ translateX }],
-          backgroundColor: "rgba(100, 200, 300, 0.3)"
-        }}
-      />
-    </View>
+    <PanGestureHandler {...gestureHandler}>
+      <Animated.View>
+        <Weave
+          centerY={translateY}
+          sideWidth={sWidth}
+          {...{ horRadius, vertRadius }}
+        />
+      </Animated.View>
+    </PanGestureHandler>
   );
 };
