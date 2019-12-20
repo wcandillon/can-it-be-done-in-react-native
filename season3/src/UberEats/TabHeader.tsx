4e7d95d9625a7b023c9825fd6a10ea988d5f90cd
William Candillon <wcandillon@gmail.com>
2019-12-20 15:13:05 +0100

    🛠

--- season3/src/UberEats/TabHeader.tsx
+++ season3/src/UberEats/TabHeader.tsx
@@ -1,6 +1,16 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
-import Animated, { Value, interpolate } from "react-native-reanimated";
+import Animated, {
+  Value,
+  and,
+  block,
+  cond,
+  greaterOrEq,
+  interpolate,
+  lessOrEq,
+  set,
+  useCode
+} from "react-native-reanimated";
 import MaskedView from "@react-native-community/masked-view";
 
 import { withTransition } from "react-native-redash";
@@ -20,7 +30,7 @@ interface TabHeaderProps {
   y: Animated.Node<number>;
 }
 
-export default ({ transition }: TabHeaderProps) => {
+export default ({ transition, y }: TabHeaderProps) => {
   const index = new Value<number>(0);
   const [measurements, setMeasurements] = useState<number[]>(
     new Array(tabs.length).fill(0)
@@ -50,6 +60,23 @@ export default ({ transition }: TabHeaderProps) => {
     transform: [{ translateX }]
   };
   const maskElement = <Animated.View {...{ style }} />;
+  useCode(
+    () =>
+      block(
+        tabs.map((tab, i) =>
+          cond(
+            i === tabs.length - 1
+              ? greaterOrEq(y, tab.anchor)
+              : and(
+                  greaterOrEq(y, tab.anchor),
+                  lessOrEq(y, tabs[i + 1].anchor)
+                ),
+            set(index, i)
+          )
+        )
+      ),
+    [index, y]
+  );
   return (
     <Animated.View style={[styles.container, { opacity }]}>
       <Tabs
