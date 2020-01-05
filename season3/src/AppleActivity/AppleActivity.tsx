479fb5ce235271bd128ff686f4059b3d0198bcdd
William Candillon <wcandillon@gmail.com>
2020-01-05 23:32:43 +0100

    🔧

--- season3/src/AppleActivity/AppleActivity.tsx
+++ season3/src/AppleActivity/AppleActivity.tsx
@@ -9,12 +9,11 @@ const { PI } = Math;
 const TAU = 2 * PI;
 
 const S = width * PixelRatio.get();
-const STROKE_WIDTH = 60 * PixelRatio.get();
+const STROKE_WIDTH = 40 * PixelRatio.get();
 const SAMPLING = 75;
 const SAMPLES = new Array(SAMPLING).fill(0).map((_, i) => i);
 const DELTA = TAU / SAMPLING;
-let FROM: any;
-let TO: any;
+
 let START: number;
 const DURATION = 5000;
 
@@ -40,8 +39,8 @@ const drawRing = (
 ) => {
   const progress = absoluteProgress / (TAU / total);
   const alpha = Math.min(progress * TAU, TAU);
-  const CX = size / 2;
-  const CY = size / 2;
+  const CX = S / 2;
+  const CY = S / 2;
   const { x: cx, y: cy } = polar2Canvas(
     {
       alpha,
@@ -54,7 +53,6 @@ const drawRing = (
   if (rot > 0) {
     p.rotate(-rot);
   }
-  p.background(0, 0, 1);
   // 1. center black circle
   p.fill(0, 0, 1);
   p.noStroke();
@@ -62,14 +60,14 @@ const drawRing = (
   // 2. end linecap
   p.translate(cx, cy);
   p.rotate(-alpha);
-  p.fill(p.lerpColor(FROM, TO, alpha / TAU));
+  p.fill(p.lerpColor(startColor, endColor, alpha / TAU));
   p.ellipse(0, 0, STROKE_WIDTH, STROKE_WIDTH);
   p.fill(0, 0, 1);
   p.arc(-3, -3, STROKE_WIDTH, STROKE_WIDTH, PI, TAU);
   p.rotate(alpha);
   p.translate(-cx, -cy);
   // 3. start linecap
-  p.fill(FROM);
+  p.fill(startColor);
   p.ellipse(size / 2 - STROKE_WIDTH / 2, 0, STROKE_WIDTH, STROKE_WIDTH);
 
   SAMPLES.forEach(i => {
@@ -98,8 +96,8 @@ const drawRing = (
     p.noStroke();
     p.fill(
       currentProgress > progress
-        ? p.color(10, 20, 30)
-        : p.lerpColor(FROM, TO, i / SAMPLING)
+        ? backgroundColor
+        : p.lerpColor(startColor, endColor, i / SAMPLING)
     );
     p.beginShape(8);
     p.vertex(0, 0);
@@ -117,21 +115,36 @@ export default () => {
   const sketch = (p: any) => {
     p.setup = () => {
       START = new Date().getTime();
-      FROM = p.color(0, 217, 253);
-      TO = p.color(0, 255, 169);
     };
 
     p.draw = () => {
       const NOW = new Date().getTime();
       const absoluteProgress =
         NOW - START > DURATION ? 1 : (NOW - START) / DURATION;
+      p.background(0, 0, 1);
       drawRing(p, {
         progress: absoluteProgress,
-        size: S,
+        size: S - STROKE_WIDTH * 4,
         startColor: p.color(0, 217, 253),
         endColor: p.color(0, 255, 169),
         backgroundColor: p.color(0, 72, 77),
-        total: TAU * 2.75
+        total: TAU
+      });
+      drawRing(p, {
+        progress: absoluteProgress,
+        size: S - STROKE_WIDTH * 2,
+        startColor: p.color(153, 255, 0),
+        endColor: p.color(216, 255, 1),
+        backgroundColor: p.color(47, 78, 0),
+        total: TAU
+      });
+      drawRing(p, {
+        progress: absoluteProgress,
+        size: S,
+        startColor: p.color(249, 18, 78),
+        endColor: p.color(249, 56, 133),
+        backgroundColor: p.color(50, 1, 14),
+        total: TAU
       });
     };
   };
@@ -144,7 +157,14 @@ export default () => {
         backgroundColor: "rgb(0, 0, 1)"
       }}
     >
-      <ProcessingView style={{ width, height: width }} {...{ sketch }} />
+      <ProcessingView
+        style={{
+          width,
+          height: width,
+          transform: [{ rotate: "270deg" }]
+        }}
+        {...{ sketch }}
+      />
     </View>
   );
 };
