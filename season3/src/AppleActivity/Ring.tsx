439470fe2909a8c4edb321496518e4e067f71095
William Candillon <wcandillon@gmail.com>
2020-01-09 13:30:26 +0100

    :construction:

--- season3/src/AppleActivity/Ring.tsx
+++ season3/src/AppleActivity/Ring.tsx
@@ -1,5 +1,9 @@
 import React from "react";
-import Animated, { min } from "react-native-reanimated";
+import Animated, {
+  Extrapolate,
+  interpolate,
+  min
+} from "react-native-reanimated";
 import { View } from "react-native";
 import { Ring, TAU } from "./Constants";
 import CircularProgress from "./CircularProgress";
@@ -15,11 +19,18 @@ export default ({ ring, theta }: RingProps) => {
     <AngularGradient size={ring.size} colors={[ring.end, ring.start]} />
   );
   const bg = <View style={{ backgroundColor: ring.bg, flex: 1 }} />;
+  const rotate = interpolate(theta, {
+    inputRange: [TAU, 2 * TAU],
+    outputRange: [0, 2 * TAU],
+    extrapolateLeft: Extrapolate.CLAMP
+  });
   return (
-    <CircularProgress
-      theta={min(theta, TAU)}
-      radius={ring.size / 2}
-      {...{ fg, bg }}
-    />
+    <Animated.View style={{ transform: [{ rotate }] }}>
+      <CircularProgress
+        theta={min(theta, TAU)}
+        radius={ring.size / 2}
+        {...{ fg, bg }}
+      />
+    </Animated.View>
   );
 };
