7f87242387ce308383829506a881e4da24c7d1f9
William Candillon <wcandillon@gmail.com>
2020-01-27 15:06:11 +0100

    🔧

--- season3/src/CoinbasePro/CoinbasePro.tsx
+++ season3/src/CoinbasePro/CoinbasePro.tsx
@@ -1,9 +1,19 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-
 import { PanGestureHandler, State } from "react-native-gesture-handler";
-import Animated, { add, cond, eq, modulo, sub } from "react-native-reanimated";
+import Animated, {
+  add,
+  call,
+  eq,
+  modulo,
+  onChange,
+  or,
+  sub,
+  useCode
+} from "react-native-reanimated";
 import { onGestureEvent, useValues } from "react-native-redash";
+import * as Haptics from "expo-haptics";
+
 import data from "./data.json";
 import Chart, { size } from "./Chart";
 import Header from "./Header";
@@ -27,12 +37,20 @@ export default () => {
   const caliber = size / candles.length;
   const translateX = add(sub(x, modulo(x, caliber)), caliber / 2);
   const opacity = eq(state, State.ACTIVE);
+  useCode(
+    () =>
+      onChange(
+        translateX,
+        call([], () => Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light))
+      ),
+    [translateX]
+  );
   return (
     <View style={styles.container}>
       <Header />
       <View>
         <Chart {...{ candles }} />
-        <PanGestureHandler {...gestureHandler}>
+        <PanGestureHandler minDist={0} {...gestureHandler}>
           <Animated.View style={StyleSheet.absoluteFill}>
             <Animated.View
               style={{
