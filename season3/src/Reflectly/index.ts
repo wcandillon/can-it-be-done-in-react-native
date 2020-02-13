7fe5320306b648791f17e3a3630e9ff55cdd513e
William Candillon <wcandillon@gmail.com>
2020-02-13 21:40:21 +0100

    :wrench:

--- season3/src/Reflectly/index.ts
+++ season3/src/Reflectly/index.ts
@@ -1,4 +1,5 @@
 import { createSharedElementStackNavigator } from "react-navigation-shared-element";
+
 import Home from "./Home";
 import Note from "./Note";
 
@@ -7,5 +8,13 @@ export default createSharedElementStackNavigator(
     Home,
     Note
   },
-  {}
+  {
+    mode: "modal",
+    headerMode: "none",
+    defaultNavigationOptions: {
+      cardStyleInterpolator: () => {
+        return { cardStyle: { opacity: 0 } };
+      }
+    }
+  }
 );
