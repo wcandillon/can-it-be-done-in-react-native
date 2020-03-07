fb2646d47e2cb1d9cd6ed6c094e59009631622b3
William Candillon <wcandillon@gmail.com>
2020-03-07 09:08:24 +0100

    :wrench:

--- the-10-min/src/Menu/Menu.tsx
+++ the-10-min/src/Menu/Menu.tsx
@@ -1,12 +1,19 @@
 import React, { useState } from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet, View, processColor } from "react-native";
 
+import {
+  bInterpolate,
+  bInterpolateColor,
+  interpolateColor,
+  useTransition
+} from "react-native-redash";
+import Animated from "react-native-reanimated";
 import Screen from "./Screen";
-
-interface ComponentNameProps {}
 
 const { width } = Dimensions.get("window");
 const perspective = { perspective: 1000 };
+const white = processColor("white");
+const grey = processColor("rgba(100, 100, 100, 0.5)");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -16,22 +23,34 @@ const styles = StyleSheet.create({
 
 export default () => {
   const [open, setOpen] = useState(false);
+  const transition = useTransition(open);
+  const rotateY = bInterpolate(transition, 0, -Math.PI / 4);
+  const scale = bInterpolate(transition, 1, 0.9);
+  const opacity = bInterpolate(transition, 0, 0.5);
   return (
     <View style={styles.container}>
-      <View
+      <Animated.View
         style={{
           flex: 1,
           transform: [
             perspective,
             { translateX: width / 2 },
-            { rotateY: open ? "-45deg" : "0deg" },
+            { rotateY },
             { translateX: -width / 2 },
-            { scale: open ? 0.95 : 1 }
+            { scale }
           ]
         }}
       >
         <Screen onPress={() => setOpen(prev => !prev)} />
-      </View>
+        <Animated.View
+          pointerEvents="none"
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            backgroundColor: "black",
+            opacity
+          }}
+        />
+      </Animated.View>
     </View>
   );
 };
