83c558a90f58f7545b3d04414842191662c6010b
William Candillon <wcandillon@gmail.com>
2020-03-12 08:30:16 +0100

    :lipstick:

--- the-10-min/src/Menu/Constants.ts
+++ the-10-min/src/Menu/Constants.ts
@@ -1,5 +1,2 @@
-import { Platform } from "react-native";
-
 export const alpha = Math.PI / 4;
-export const perspective =
-  Platform.OS === "ios" ? { perspective: 1000 } : undefined;
+export const perspective = { perspective: 1000 };
