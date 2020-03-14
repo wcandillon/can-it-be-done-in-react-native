c514dfde8ae3c14b7d69357111a15cd849e17f82
Neil Hannah <ne.hannah@gmail.com>
2020-03-14 19:59:43 +0100

    update segment on tab bar

--- the-10-min/src/Tabbar/icons/Constants.ts
+++ the-10-min/src/Tabbar/icons/Constants.ts
@@ -1,4 +1,4 @@
-import { Dimensions } from "react-native";
+import { Dimensions, PixelRatio } from "react-native";
 
 const { width } = Dimensions.get("window");
 
@@ -10,7 +10,7 @@ const numberOfIcons = 5;
 const horizontalPadding = 48;
 export const DURATION = 450;
 export const PADDING = 16;
-export const SEGMENT = width / numberOfIcons;
+export const SEGMENT = PixelRatio.roundToNearestPixel(width / numberOfIcons);
 export const ICON_SIZE = SEGMENT - horizontalPadding;
 
 export const Colors = {
