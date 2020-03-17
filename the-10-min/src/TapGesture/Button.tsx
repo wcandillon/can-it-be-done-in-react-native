a086bffa1904206c48d4702031b8a31fdb9b0506
William Candillon <wcandillon@gmail.com>
2020-03-17 09:40:28 +0100

    :wrench:

--- the-10-min/src/TapGesture/Button.tsx
+++ the-10-min/src/TapGesture/Button.tsx
@@ -6,15 +6,19 @@ import { bInterpolate } from "react-native-redash";
 import { StyleGuide } from "../components";
 
 const SIZE = 100;
+const ICON_SIZE = 72;
+const PADDING = (SIZE - ICON_SIZE) / 2;
 
 const styles = StyleSheet.create({
   container: {
     width: SIZE,
     height: SIZE,
     borderRadius: SIZE / 2,
-    backgroundColor: "white",
-    justifyContent: "center",
-    alignItems: "center"
+    backgroundColor: "white"
+  },
+  content: {
+    marginTop: PADDING,
+    marginLeft: PADDING
   }
 });
 
@@ -28,22 +32,26 @@ export default ({ progress }: ButtonProps) => {
   const height = bInterpolate(progress, 0, SIZE);
   return (
     <View style={styles.container}>
-      <Icon
-        name="fingerprint"
-        color={StyleGuide.palette.background}
-        size={72}
-      />
-      <Animated.View
-        style={{
-          ...StyleSheet.absoluteFillObject,
-          justifyContent: "center",
-          alignItems: "center",
-          overflow: "hidden",
-          height
-        }}
-      >
-        <Icon name="fingerprint" color={StyleGuide.palette.primary} size={72} />
-      </Animated.View>
+      <View style={styles.content}>
+        <Icon
+          name="fingerprint"
+          color={StyleGuide.palette.background}
+          size={72}
+        />
+        <Animated.View
+          style={{
+            ...StyleSheet.absoluteFillObject,
+            overflow: "hidden",
+            height
+          }}
+        >
+          <Icon
+            name="fingerprint"
+            color={StyleGuide.palette.primary}
+            size={ICON_SIZE}
+          />
+        </Animated.View>
+      </View>
     </View>
   );
 };
--- the-10-min/src/TapGesture/TapGesture.tsx
+++ the-10-min/src/TapGesture/TapGesture.tsx
@@ -23,10 +23,8 @@ import Animated, {
   useCode
 } from "react-native-reanimated";
 
-import { onGestureEvent } from "react-native-redash";
+import { onGestureEvent, timing, withTransition } from "react-native-redash";
 import Button from "./Button";
-
-const DURATION = 5000;
 
 const styles = StyleSheet.create({
   container: {
@@ -38,28 +36,10 @@ const styles = StyleSheet.create({
 });
 
 export default () => {
-  const time = new Value(0);
-  const clock = new Clock();
-  const progress = new Value(0);
   const state = new Value(State.UNDETERMINED);
+  const isActive = eq(state, State.BEGAN);
+  const progress = withTransition(isActive, { duration: 2000 });
   const gestureHandler = onGestureEvent({ state });
-  useCode(
-    () =>
-      block([
-        cond(not(clockRunning(clock)), set(time, clock)),
-        cond(eq(state, State.BEGAN), startClock(clock)),
-        cond(eq(state, State.END), stopClock(clock)),
-        set(
-          progress,
-          interpolate(clock, {
-            inputRange: [time, add(time, DURATION)],
-            outputRange: [0, 1],
-            extrapolate: Extrapolate.CLAMP
-          })
-        )
-      ]),
-    [clock, progress, state, time]
-  );
   return (
     <View style={styles.container}>
       <TapGestureHandler {...gestureHandler}>
