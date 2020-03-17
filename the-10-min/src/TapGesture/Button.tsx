44294011f245e26e152a46a7b56ebf4c296d31d7
William Candillon <wcandillon@gmail.com>
2020-03-17 09:23:18 +0100

    :wrench:

--- the-10-min/src/TapGesture/Button.tsx
+++ the-10-min/src/TapGesture/Button.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { StyleSheet, View } from "react-native";
 import Animated from "react-native-reanimated";
 import { MaterialCommunityIcons as Icon } from "@expo/vector-icons";
+import { bInterpolate } from "react-native-redash";
 import { StyleGuide } from "../components";
 
 const SIZE = 100;
@@ -24,9 +25,25 @@ interface ButtonProps {
 //  check-circle
 //
 export default ({ progress }: ButtonProps) => {
+  const height = bInterpolate(progress, 0, SIZE);
   return (
     <View style={styles.container}>
-      <Icon name="fingerprint" color={StyleGuide.palette.primary} size={72} />
+      <Icon
+        name="fingerprint"
+        color={StyleGuide.palette.background}
+        size={72}
+      />
+      <Animated.View
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          justifyContent: "center",
+          alignItems: "center",
+          overflow: "hidden",
+          height
+        }}
+      >
+        <Icon name="fingerprint" color={StyleGuide.palette.primary} size={72} />
+      </Animated.View>
     </View>
   );
 };
--- the-10-min/src/TapGesture/TapGesture.tsx
+++ the-10-min/src/TapGesture/TapGesture.tsx
@@ -1,9 +1,32 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
-import { TapGestureHandler } from "react-native-gesture-handler";
-import Animated, { Value } from "react-native-reanimated";
+import {
+  LongPressGestureHandler,
+  State,
+  TapGestureHandler
+} from "react-native-gesture-handler";
+import Animated, {
+  Clock,
+  Extrapolate,
+  Value,
+  add,
+  block,
+  clockRunning,
+  cond,
+  debug,
+  eq,
+  interpolate,
+  not,
+  set,
+  startClock,
+  stopClock,
+  useCode
+} from "react-native-reanimated";
 
+import { onGestureEvent } from "react-native-redash";
 import Button from "./Button";
+
+const DURATION = 5000;
 
 const styles = StyleSheet.create({
   container: {
@@ -15,10 +38,31 @@ const styles = StyleSheet.create({
 });
 
 export default () => {
+  const time = new Value(0);
+  const clock = new Clock();
   const progress = new Value(0);
+  const state = new Value(State.UNDETERMINED);
+  const gestureHandler = onGestureEvent({ state });
+  useCode(
+    () =>
+      block([
+        cond(not(clockRunning(clock)), set(time, clock)),
+        cond(eq(state, State.BEGAN), startClock(clock)),
+        cond(eq(state, State.END), stopClock(clock)),
+        set(
+          progress,
+          interpolate(clock, {
+            inputRange: [time, add(time, DURATION)],
+            outputRange: [0, 1],
+            extrapolate: Extrapolate.CLAMP
+          })
+        )
+      ]),
+    [clock, progress, state, time]
+  );
   return (
     <View style={styles.container}>
-      <TapGestureHandler>
+      <TapGestureHandler {...gestureHandler}>
         <Animated.View>
           <Button {...{ progress }} />
         </Animated.View>
