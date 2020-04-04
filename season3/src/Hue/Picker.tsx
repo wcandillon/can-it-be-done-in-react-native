f2c04ac5b64a347e6b0fd83a70d3c77d4d2c5f65
William Candillon <wcandillon@gmail.com>
2020-04-04 17:51:28 +0200

    :wrench:

--- season3/src/Hue/Picker.tsx
+++ season3/src/Hue/Picker.tsx
@@ -10,6 +10,7 @@ import Animated, {
   debug,
   diffClamp,
   divide,
+  floor,
   interpolate,
   lessThan,
   modulo,
@@ -20,6 +21,7 @@ import Animated, {
 } from "react-native-reanimated";
 import {
   Vector,
+  bInterpolate,
   canvas2Polar,
   clamp,
   onGestureEvent,
@@ -85,6 +87,45 @@ function colorHSV(
   ]) as Animated.Node<number>;
 }
 
+const fract = (x: Animated.Node<number>) => sub(x, floor(x));
+const mix = (
+  x: Animated.Adaptable<number>,
+  y: Animated.Adaptable<number>,
+  a: Animated.Adaptable<number>
+) => bInterpolate(a, x, y);
+
+const hsv2rgb = (
+  h: Animated.Adaptable<number>,
+  s: Animated.Adaptable<number>,
+  v: Animated.Adaptable<number>
+) => {
+  // vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+  const K = {
+    x: 1,
+    y: 2 / 3,
+    z: 1 / 3,
+    w: 3
+  };
+  // vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+  const p = {
+    x: abs(sub(multiply(fract(add(h, K.x)), 6), K.w)),
+    y: abs(sub(multiply(fract(add(h, K.y)), 6), K.w)),
+    z: abs(sub(multiply(fract(add(h, K.z)), 6), K.w))
+  };
+  //   return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+  const rgb = {
+    x: multiply(v, mix(K.x, clamp(sub(p.x, K.x), 0, 1), s)),
+    y: multiply(v, mix(K.x, clamp(sub(p.y, K.x), 0, 1), s)),
+    z: multiply(v, mix(K.x, clamp(sub(p.z, K.x), 0, 1), s))
+  };
+  const result = {
+    x: round(multiply(rgb.x, 255)),
+    y: round(multiply(rgb.y, 255)),
+    z: round(multiply(rgb.z, 255))
+  };
+  return color(result.x, result.y, result.z);
+};
+
 const { width } = Dimensions.get("window");
 export const PICKER_SIZE = 40;
 export const CANVAS_SIZE = width - PICKER_SIZE * 2;
@@ -117,10 +158,24 @@ export default () => {
   };
   // a * 0.5 / PI + 0.5;
   const angle = modulo(v.theta, 2 * Math.PI);
-  const backgroundColor = colorHSV(
-    toDeg(angle),
+  const backgroundColor = hsv2rgb(
+    interpolate(angle, {
+      inputRange: [0, 2 * Math.PI],
+      outputRange: [0, 1]
+    }),
     divide(v.radius, CANVAS_SIZE / 2),
     1
+  );
+  useCode(
+    () =>
+      debug(
+        "angle",
+        interpolate(angle, {
+          inputRange: [0, 2 * Math.PI],
+          outputRange: [0, 1]
+        })
+      ),
+    [angle]
   );
   return (
     <View style={StyleSheet.absoluteFill}>
