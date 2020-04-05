c87ed08b84f0509b009c5af19bd905ccd74fc869
William Candillon <wcandillon@gmail.com>
2020-04-05 21:50:16 +0200

    :wrench:

--- season3/src/Hue/Header.tsx
+++ season3/src/Hue/Header.tsx
@@ -5,27 +5,14 @@ import Animated, {
   color,
   cond,
   divide,
-  greaterThan,
   lessOrEq,
-  multiply,
   pow
 } from "react-native-reanimated";
 import { Feather as Icon } from "@expo/vector-icons";
-import { hsv2color, hsv2rgb } from "react-native-redash";
+import { colorForBackground, hsv2rgb } from "react-native-redash";
 
 import Slider from "./Slider";
 
-const l = (c1: Animated.Node<number>) => {
-  const c = divide(c1, 255);
-  return cond(
-    lessOrEq(c, 0.03928),
-    divide(c, 12.92),
-    pow(divide(add(c, 0.055), 1.055), 2.4)
-  );
-};
-
-const black = color(0, 0, 0);
-const white = color(255, 255, 255);
 const BUTTON_SIZE = 35;
 const styles = StyleSheet.create({
   container: {
@@ -72,9 +59,6 @@ interface HeaderProps {
 export default ({ h, s, v, backgroundColor }: HeaderProps) => {
   const { r, g, b } = hsv2rgb(h, s, 1);
   const bg2 = color(r, g, b);
-  const L = add(multiply(0.299, r), multiply(0.587, g), multiply(0.114, b));
-  // https://stackoverflow.com/questions/3942878/how-to-decide-font-color-in-white-or-black-depending-on-background-color
-  const textColor = cond(greaterThan(L, 186), black, white);
   return (
     <View>
       <Animated.View style={{ backgroundColor: bg2 }}>
@@ -82,7 +66,9 @@ export default ({ h, s, v, backgroundColor }: HeaderProps) => {
           <View style={styles.container}>
             <View style={styles.side}>
               <Button name="arrow-left" />
-              <Animated.Text style={[styles.name, { color: textColor }]}>
+              <Animated.Text
+                style={[styles.name, { color: colorForBackground(r, g, b) }]}
+              >
                 Living Room
               </Animated.Text>
             </View>
