3524444f06576df16fbeed3ffb1dcc4b459a6e5a
William Candillon <wcandillon@gmail.com>
2020-05-02 16:16:31 +0200

    :wrench:

--- the-10-min/src/ThreeD/Face.tsx
+++ the-10-min/src/ThreeD/Face.tsx
@@ -27,11 +27,11 @@ export type Vec3 = readonly [
 
 export type Matrix3 = readonly [Vec3, Vec3, Vec3];
 
-interface PCProps {
+interface PointProps {
   point: Vector;
 }
 
-const PC = ({ point }: PCProps) => (
+const PointComp = ({ point }: PointProps) => (
   <Animated.View
     style={[
       {
@@ -41,7 +41,7 @@ const PC = ({ point }: PCProps) => (
         width: 10,
         borderRadius: 5,
         opacity: 0.5,
-        transform: translate(vec.multiply(point, SIZE)),
+        transform: translate(point),
       },
     ]}
   />
@@ -56,19 +56,19 @@ const Face = ({ points, theta, backgroundColor, label }: FaceProps) => {
 
   const p1V = matrixVecMul(m, [points[0].x, points[0].y, points[0].z, 1]);
   const z1 = divide(1, sub(DISTANCE, p1V[2]));
-  const p1 = vec.create(multiply(p1V[0], z1), multiply(p1V[1], z1));
+  const p1 = vec.create(multiply(p1V[0], z1, SIZE), multiply(p1V[1], z1, SIZE));
 
   const p2V = matrixVecMul(m, [points[1].x, points[1].y, points[1].z, 1]);
   const z2 = divide(1, sub(DISTANCE, p2V[2]));
-  const p2 = vec.create(multiply(p2V[0], z2), multiply(p2V[1], z2));
+  const p2 = vec.create(multiply(p2V[0], z2, SIZE), multiply(p2V[1], z2, SIZE));
 
   const p3V = matrixVecMul(m, [points[2].x, points[2].y, points[2].z, 1]);
   const z3 = divide(1, sub(DISTANCE, p3V[2]));
-  const p3 = vec.create(multiply(p3V[0], z3), multiply(p3V[1], z3));
+  const p3 = vec.create(multiply(p3V[0], z3, SIZE), multiply(p3V[1], z3, SIZE));
 
   const p4V = matrixVecMul(m, [points[3].x, points[3].y, points[3].z, 1]);
   const z4 = divide(1, sub(DISTANCE, p4V[2]));
-  const p4 = vec.create(multiply(p4V[0], z4), multiply(p4V[1], z4));
+  const p4 = vec.create(multiply(p4V[0], z4, SIZE), multiply(p4V[1], z4, SIZE));
   /*
   const vec4 = matrixVecMul(m, [x, y, z, 1]);
   const perspective = divide(1, sub(DISTANCE, vec4[2]));
@@ -81,19 +81,19 @@ const Face = ({ points, theta, backgroundColor, label }: FaceProps) => {
     {
       p1: {
         o: vec.multiply(points[0], SIZE),
-        p: vec.multiply(p1, SIZE),
+        p: p1,
       },
       p2: {
         o: vec.multiply(points[1], SIZE),
-        p: vec.multiply(p2, SIZE),
+        p: p2,
       },
       p3: {
         o: vec.multiply(points[2], SIZE),
-        p: vec.multiply(p3, SIZE),
+        p: p3,
       },
       p4: {
         o: vec.multiply(points[3], SIZE),
-        p: vec.multiply(p4, SIZE),
+        p: p4,
       },
     },
     SIZE
@@ -147,10 +147,10 @@ const Face = ({ points, theta, backgroundColor, label }: FaceProps) => {
       >
         <Text style={{ color: "white", fontSize: 16 }}>{label}</Text>
       </Animated.View>
-      <PC point={p1} />
-      <PC point={p2} />
-      <PC point={p3} />
-      <PC point={p4} />
+      <PointComp point={p1} />
+      <PointComp point={p2} />
+      <PointComp point={p3} />
+      <PointComp point={p4} />
     </>
   );
 };
