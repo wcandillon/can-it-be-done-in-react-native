3e2000880ad3c1b86d65addf90b869b962fc4165
William Candillon <wcandillon@gmail.com>
2020-05-11 20:27:57 +0200

    :wrench:

--- season3/src/WhatsApp/ImageViewer.tsx
+++ season3/src/WhatsApp/ImageViewer.tsx
@@ -1,144 +1,32 @@
-import React, { ReactNode } from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
-import Animated, {
-  Value,
-  and,
-  block,
-  cond,
-  debug,
-  eq,
-  event,
-  max,
-  multiply,
-  neq,
-  not,
-  onChange,
-  or,
-  set,
-  sub,
-  useCode,
-} from "react-native-reanimated";
-import {
-  PanGestureHandler,
-  PinchGestureHandler,
-  State,
-} from "react-native-gesture-handler";
-import {
-  Vector,
-  onGestureEvent,
-  pinchActive,
-  pinchBegan,
-  vec,
-} from "react-native-redash";
+import React from "react";
+import { Dimensions, Image, StyleSheet, View } from "react-native";
+import { vec } from "react-native-redash";
 
 const { width, height } = Dimensions.get("window");
 export const CANVAS = vec.create(width, height);
-const CENTER = vec.divide(CANVAS, 2);
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: "black",
+    width: CANVAS.x,
+    height: CANVAS.y,
+    overflow: "hidden",
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    width: undefined,
+    height: undefined,
   },
 });
 
-interface PinchGestureProps {
-  pan: Vector<Animated.Value<number>>;
-  panState: Animated.Value<number>;
-  panGestureHandler: {
-    onHandlerStateChange: (...args: unknown[]) => void;
-    onGestureEvent: (...args: unknown[]) => void;
-  };
-  scale: Animated.Value<number>;
-  translate: Vector<Animated.Value<number>>;
-  children: ReactNode;
-  index: Animated.Node<number>;
+interface ImageViewProps {
+  source: number;
 }
 
-const PinchGesture = ({
-  children,
-  scale,
-  translate,
-  index,
-  panGestureHandler,
-  pan,
-  panState,
-}: PinchGestureProps) => {
-  const origin = vec.createValue(0);
-  const focal = vec.createValue(0);
-  const gestureScale = new Value(1);
-  const numberOfPointers = new Value(0);
-  const pinchState = new Value(State.END);
-  const pinchGestureHandler = onGestureEvent({
-    numberOfPointers,
-    scale: gestureScale,
-    state: pinchState,
-    focalX: focal.x,
-    focalY: focal.y,
-  });
-
-  const scaleOffset = new Value(1);
-  const minVec = vec.min(vec.multiply(-0.5, CANVAS, sub(scale, 1)), 0);
-  const maxVec = vec.max(vec.minus(minVec), 0);
-  const offset = vec.createValue(0);
-  const translation = vec.createValue(0);
-  const adjustedFocal = vec.sub(focal, vec.add(CENTER, offset));
-  useCode(
-    () =>
-      block([
-        onChange(index, [
-          debug("onChange(index)", index),
-          set(scaleOffset, 1),
-          vec.set(offset, 0),
-          set(scale, 1),
-          vec.set(translate, 0),
-        ]),
-        cond(
-          eq(panState, State.ACTIVE),
-          vec.set(translation, vec.clamp(pan, minVec, maxVec))
-        ),
-        cond(pinchBegan(pinchState), vec.set(origin, adjustedFocal)),
-        cond(pinchActive(pinchState, numberOfPointers), [
-          vec.set(
-            translation,
-            vec.add(
-              vec.sub(adjustedFocal, origin),
-              origin,
-              vec.multiply(-1, gestureScale, origin)
-            )
-          ),
-        ]),
-        cond(
-          and(
-            or(eq(pinchState, State.END), eq(panState, State.END)),
-            or(neq(pinchState, State.ACTIVE), neq(panState, State.ACTIVE))
-          ),
-          [
-            vec.set(offset, vec.add(offset, translation)),
-            set(scaleOffset, scale),
-            set(gestureScale, 1),
-            vec.set(translation, 0),
-            vec.set(focal, 0),
-          ]
-        ),
-        set(scale, multiply(gestureScale, scaleOffset)),
-        vec.set(translate, vec.add(offset, translation)),
-      ]),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  );
+const ImageViewer = ({ source }: ImageViewProps) => {
   return (
     <View style={styles.container}>
-      <PinchGestureHandler {...pinchGestureHandler}>
-        <Animated.View style={StyleSheet.absoluteFillObject}>
-          <PanGestureHandler {...panGestureHandler}>
-            <Animated.View style={StyleSheet.absoluteFill}>
-              {children}
-            </Animated.View>
-          </PanGestureHandler>
-        </Animated.View>
-      </PinchGestureHandler>
+      <Image style={styles.image} {...{ source }} />
     </View>
   );
 };
 
-export default PinchGesture;
+export default ImageViewer;
--- season3/src/WhatsApp/WhatsApp.tsx
+++ season3/src/WhatsApp/WhatsApp.tsx
@@ -1,50 +1,8 @@
 import React from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
-import Animated, {
-  add,
-  and,
-  clockRunning,
-  cond,
-  debug,
-  diff,
-  divide,
-  eq,
-  floor,
-  greaterThan,
-  lessThan,
-  max,
-  min,
-  multiply,
-  neq,
-  not,
-  onChange,
-  round,
-  set,
-  sub,
-  useCode,
-} from "react-native-reanimated";
-import {
-  addTo,
-  clamp,
-  dec,
-  inc,
-  minus,
-  snapPoint,
-  subTo,
-  timing,
-  useClock,
-  usePanGestureHandler,
-  useValue,
-  useValues,
-  useVector,
-  useVectors,
-  vec,
-  withTransition,
-} from "react-native-redash";
-import { State } from "react-native-gesture-handler";
+import { StyleSheet, View } from "react-native";
+import Animated from "react-native-reanimated";
 import ImageViewer, { CANVAS } from "./ImageViewer";
 
-const { x: width, y: height } = CANVAS;
 export const assets = [
   require("./assets/3.jpg"),
   require("./assets/2.jpg"),
@@ -53,101 +11,28 @@ export const assets = [
   require("./assets/1.jpg"),
 ];
 
+const { x: width, y: height } = CANVAS;
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
     backgroundColor: "black",
   },
-  picture: {
-    width,
+  pictures: {
+    flexDirection: "row",
     height,
-    overflow: "hidden",
-  },
-  image: {
-    ...StyleSheet.absoluteFillObject,
-    width: undefined,
-    height: undefined,
+    width: width * assets.length,
   },
 });
 
 const WhatsApp = () => {
-  const clock = useClock();
-  const index = useValue(0);
-  const translateX = useValue(0);
-  const scale = useValue(1);
-  const translate = useVector(0);
-  const {
-    gestureHandler,
-    translation,
-    velocity,
-    state,
-  } = usePanGestureHandler();
-  const offset1 = multiply(index, -width);
-  const minX = min(multiply(-0.5, width, sub(scale, 1)), 0);
-  const maxX = max(minus(minX), 0);
-  const { x } = translation;
-  const left = sub(x, clamp(x, minX, maxX));
-  const snapTo = snapPoint(
-    translateX,
-    velocity.x,
-    assets.map((_, i) => -width * i)
-  );
-  const newIndex = floor(divide(translateX, -width));
-  useCode(
-    () => [
-      cond(eq(state, State.ACTIVE), [set(translateX, add(offset1, left))]),
-      cond(eq(state, State.END), [
-        set(translateX, timing({ clock, from: translateX, to: snapTo })),
-        cond(and(not(clockRunning(clock)), neq(index, newIndex)), [
-          set(index, newIndex),
-          set(state, State.UNDETERMINED),
-        ]),
-      ]),
-    ],
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
-  );
   return (
-    <ImageViewer
-      pan={translation}
-      panState={state}
-      panGestureHandler={gestureHandler}
-      {...{ scale, translate, index }}
-    >
-      <Animated.View style={styles.container}>
-        <Animated.View
-          style={{
-            height,
-            width: width * assets.length,
-            flexDirection: "row",
-            transform: [{ translateX }],
-          }}
-        >
-          {assets.map((asset, i) => {
-            const active = eq(i, index);
-            return (
-              <View key={asset} style={styles.picture}>
-                <Animated.Image
-                  source={asset}
-                  style={[
-                    styles.image,
-                    {
-                      transform: [
-                        {
-                          translateX: cond(active, translate.x),
-                        },
-                        { translateY: cond(active, translate.y, 0) },
-                        { scale: cond(active, scale, 1) },
-                      ],
-                    },
-                  ]}
-                />
-              </View>
-            );
-          })}
-        </Animated.View>
-      </Animated.View>
-    </ImageViewer>
+    <Animated.View style={styles.container}>
+      <View style={styles.pictures}>
+        {assets.map((source) => (
+          <ImageViewer key={source} {...{ source }} />
+        ))}
+      </View>
+    </Animated.View>
   );
 };
 
