81e5eb234ee16887fc40b22967be6be6a8bff7d2
William Candillon <wcandillon@gmail.com>
2020-05-12 14:17:19 +0200

    :wrench:

--- the-10-min/src/Swiper/Swiper.tsx
+++ the-10-min/src/Swiper/Swiper.tsx
@@ -1,6 +1,24 @@
 import React from "react";
-import { View } from "react-native";
+import { Dimensions, Image, StyleSheet, View } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 
+const { width, height } = Dimensions.get("window");
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: "black",
+  },
+  picture: {
+    width,
+    height,
+    overflow: "hidden",
+  },
+  image: {
+    ...StyleSheet.absoluteFillObject,
+    width: undefined,
+    height: undefined,
+  },
+});
 export const assets = [
   require("./assets/3.jpg"),
   require("./assets/2.jpg"),
@@ -10,7 +28,17 @@ export const assets = [
 ];
 
 const Swiper = () => {
-  return <View />;
+  return (
+    <View style={styles.container}>
+      <ScrollView snapToInterval={width} decelerationRate="fast" horizontal>
+        {assets.map((source) => (
+          <View key={source} style={styles.picture}>
+            <Image style={styles.image} {...{ source }} />
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
 };
 
 export default Swiper;
