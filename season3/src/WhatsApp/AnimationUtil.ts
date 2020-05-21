2db9af103b36ad1c0b5b34ec0ffb9da42101e296
William Candillon <wcandillon@gmail.com>
2020-05-21 08:33:28 +0200

    :wrench:

--- season3/src/WhatsApp/AnimationUtil.ts
+++ season3/src/WhatsApp/AnimationUtil.ts
@@ -1,46 +1,83 @@
 import Animated, {
   Clock,
   Value,
-  add,
   and,
   block,
   clockRunning,
   cond,
   diff,
-  divide,
   eq,
-  floor,
+  lessThan,
   multiply,
   neq,
   not,
   or,
+  proc,
   decay as reDecay,
   set,
   startClock,
   stopClock,
-  sub,
   useCode,
 } from "react-native-reanimated";
 import {
   Vector,
-  clamp,
   panGestureHandler,
   pinchActive,
   pinchBegan,
   pinchGestureHandler,
-  snapPoint,
-  timing,
-  useClock,
   useValue,
-  useVector,
   vec,
 } from "react-native-redash";
-import { Dimensions } from "react-native";
+import { Dimensions, Platform } from "react-native";
 import { State } from "react-native-gesture-handler";
 
 const { width, height } = Dimensions.get("window");
 export const CANVAS = vec.create(width, height);
 const CENTER = vec.divide(CANVAS, 2);
+const pinchEnd = proc(
+  (state: Animated.Node<State>, numberOfPointers: Animated.Node<number>) =>
+    Platform.OS === "android"
+      ? or(eq(state, State.END), lessThan(numberOfPointers, 2))
+      : eq(state, State.END)
+);
+
+const decay = (
+  position: Animated.Adaptable<number>,
+  velocity: Animated.Adaptable<number>,
+  clock: Animated.Clock
+) => {
+  const state = {
+    finished: new Value(0),
+    position: new Value(0),
+    time: new Value(0),
+    velocity: new Value(0),
+  };
+  const config = { deceleration: 0.998 };
+  return block([
+    cond(not(clockRunning(clock)), [
+      set(state.finished, 0),
+      set(state.position, position),
+      set(state.velocity, velocity),
+      set(state.time, 0),
+      startClock(clock),
+    ]),
+    reDecay(clock, state, config),
+    state.position,
+  ]);
+};
+
+const decayVector = (
+  position: Vector,
+  velocity: Vector,
+  clock: Vector<Clock>
+) => {
+  const x = decay(position.x, velocity.x, clock.x);
+  const y = decay(position.y, velocity.y, clock.y);
+  return {
+    x,
+    y,
+  };
+};
 
 interface UsePinchParams {
   pan: ReturnType<typeof panGestureHandler>;
@@ -59,6 +96,8 @@ export const usePinch = ({
   minVec,
   maxVec,
 }: UsePinchParams) => {
+  const shouldDecay = useValue(0);
+  const clock = vec.create(new Clock(), new Clock());
   const offset = vec.createValue(0, 0);
   const scaleOffset = new Value(1);
   const origin = vec.createValue(0, 0);
@@ -84,7 +123,10 @@ export const usePinch = ({
       ]),
       cond(
         and(
-          or(eq(pinch.state, State.UNDETERMINED), eq(pinch.state, State.END)),
+          or(
+            eq(pinch.state, State.UNDETERMINED),
+            pinchEnd(pinch.state, pinch.numberOfPointers)
+          ),
           or(eq(pan.state, State.UNDETERMINED), eq(pan.state, State.END))
         ),
         [
@@ -95,6 +137,25 @@ export const usePinch = ({
           vec.set(pinch.focal, 0),
         ]
       ),
+      cond(or(eq(pan.state, State.ACTIVE), eq(pinch.state, State.ACTIVE)), [
+        stopClock(clock.x),
+        stopClock(clock.y),
+        set(shouldDecay, 0),
+      ]),
+      cond(
+        and(
+          neq(diff(pan.state), 0),
+          eq(pan.state, State.END),
+          neq(pinch.state, State.ACTIVE)
+        ),
+        set(shouldDecay, 1)
+      ),
+      cond(shouldDecay, [
+        vec.set(
+          offset,
+          vec.clamp(decayVector(offset, pan.velocity, clock), minVec, maxVec)
+        ),
+      ]),
       set(scale, multiply(pinch.scale, scaleOffset)),
       vec.set(translate, vec.add(translation, offset)),
     ],
