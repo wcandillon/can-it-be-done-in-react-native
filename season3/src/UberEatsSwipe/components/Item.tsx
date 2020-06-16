4e37435a38ba935fa3f3f77a4c99ae70857b53cf
William Candillon <wcandillon@gmail.com>
2020-06-16 10:25:33 +0200

    :wrench:

--- season3/src/UberEatsSwipe/components/Item.tsx
+++ season3/src/UberEatsSwipe/components/Item.tsx
@@ -2,18 +2,12 @@ import React from "react";
 import { Dimensions, StyleSheet, View } from "react-native";
 import Animated from "react-native-reanimated";
 import { PanGestureHandler } from "react-native-gesture-handler";
-import { clamp, usePanGestureHandler, withSpring } from "react-native-redash";
+import { clamp, snapPoint, usePanGestureHandler } from "react-native-redash";
+import { withTiming } from "./AnimatedHelpers";
 import ItemLayout, { ItemModel } from "./ItemLayout";
 
 const { width } = Dimensions.get("window");
-const config = {
-  stiffness: 1000,
-  damping: 500,
-  mass: 3,
-  overshootClamping: true,
-  restDisplacementThreshold: 0.01,
-  restSpeedThreshold: 0.01,
-};
+const snapPoints = [-width, 0];
 const styles = StyleSheet.create({
   background: {
     ...StyleSheet.absoluteFillObject,
@@ -32,14 +26,12 @@ const Item = ({ item }: ItemProps) => {
     velocity,
     state,
   } = usePanGestureHandler();
-  const value = clamp(translation.x, -width, 0);
-  const translateX = withSpring({
-    value,
-    velocity: velocity.x,
-    snapPoints: [0, -width],
+  const from = clamp(translation.x, -width, 0);
+  const to = snapPoint(from, velocity.x, snapPoints);
+  const translateX = withTiming({
+    from,
+    to,
     state,
-    config,
-    onSnap: () => alert("bruh"),
   });
   return (
     <Animated.View>
