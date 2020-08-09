4fd7e4709ab496c04ae0121cad278e422c41efa7
William Candillon <wcandillon@gmail.com>
2020-08-09 17:54:57 +0200

    :wrench:

--- reanimated-2/src/MaskedView/MaskedView.tsx
+++ reanimated-2/src/MaskedView/MaskedView.tsx
@@ -1,48 +1,78 @@
-import React from "react";
-import { View, StyleSheet, Dimensions, Image } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Image, Text, Dimensions } from "react-native";
 import RNMaskedView from "@react-native-community/masked-view";
 import Animated from "react-native-reanimated";
 
-const { width } = Dimensions.get("window");
+import { phones, SIZE } from "./Phones";
+import { RectButton } from "react-native-gesture-handler";
 
+const { width } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
   picture: {
-    width,
-    height: width,
+    width: SIZE,
+    height: SIZE,
   },
   background: {
     ...StyleSheet.absoluteFillObject,
     width: undefined,
     height: undefined,
   },
+  title: {
+    textAlign: "center",
+  },
+  colors: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  button: {
+    width: width / 4,
+    height: width / 4,
+    alignSelf: "center",
+    justifyContent: "center",
+  },
+  dot: { width: 20, height: 20, borderRadius: 10 },
 });
 
 const MaskedView = () => {
+  const [phone, setPhone] = useState(phones[0]);
   return (
     <View style={styles.container}>
-      <RNMaskedView
-        style={styles.picture}
-        maskElement={
-          <Animated.View
-            style={{
-              ...StyleSheet.absoluteFillObject,
-              backgroundColor: "black",
-              width: undefined,
-              height: undefined,
-              transform: [{ scale: 2 }],
-              borderRadius: width / 2,
-            }}
-          />
-        }
-      >
-        <Image
-          source={require("./assets/blue.png")}
-          style={styles.background}
-        />
-      </RNMaskedView>
+      <View style={styles.picture}>
+        <Image source={require("./assets/red.png")} style={styles.background} />
+        <RNMaskedView
+          style={StyleSheet.absoluteFill}
+          maskElement={
+            <Animated.View
+              style={{
+                ...StyleSheet.absoluteFillObject,
+                backgroundColor: "black",
+                width: undefined,
+                height: undefined,
+                transform: [{ scale: 0.5 }, { translateY: SIZE / 2 }],
+                borderRadius: SIZE / 2,
+              }}
+            />
+          }
+        >
+          <Image source={phone.picture} style={styles.background} />
+        </RNMaskedView>
+      </View>
+      <Text style={styles.title}>{phone.name}</Text>
+      <View style={styles.colors}>
+        {phones.map((p) => (
+          <RectButton
+            key={p.id}
+            onPress={() => setPhone(p)}
+            style={styles.button}
+          >
+            <View style={[styles.dot, { backgroundColor: p.color }]} />
+          </RectButton>
+        ))}
+      </View>
     </View>
   );
 };
