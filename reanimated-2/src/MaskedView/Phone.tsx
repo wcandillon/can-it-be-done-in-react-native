e3fc9e1fcc4cac0fe5b0a0ccb5e2944014b881f9
William Candillon <wcandillon@gmail.com>
2020-08-10 08:40:38 +0200

    :wrench:

--- reanimated-2/src/MaskedView/Phone.tsx
+++ reanimated-2/src/MaskedView/Phone.tsx
@@ -4,19 +4,30 @@ import MaskedView from "@react-native-community/masked-view";
 
 import { PhoneModel, SIZE } from "./Phones";
 import Animated, { useAnimatedStyle, Easing } from "react-native-reanimated";
-import { useTiming, mix, Vector } from "../components/AnimatedHelpers";
+import {
+  useTiming,
+  mix,
+  Vector,
+  cartesian2Canvas,
+} from "../components/AnimatedHelpers";
 
 interface PhoneProps {
   phone: PhoneModel;
 }
 
 const r = SIZE / 2;
-const MAX_SCALE = SIZE / Math.hypot(r, r);
-const R = MAX_SCALE * r;
-const scale = (pos: Vector<number>) => {
+const center = { x: r, y: r };
+const scale = (tr: Vector<number>) => {
   "worklet";
-  const R2 = R + Math.hypot(pos.x, pos.y);
-  return R2 / r;
+  const pos = cartesian2Canvas(
+    {
+      x: Math.abs(tr.x),
+      y: -Math.abs(tr.y),
+    },
+    center
+  );
+  const radius = Math.hypot(pos.x, pos.y);
+  return radius / r;
 };
 
 const Phone = ({ phone }: PhoneProps) => {
@@ -25,7 +36,6 @@ const Phone = ({ phone }: PhoneProps) => {
     duration: 600,
     easing: Easing.inOut(Easing.ease),
   });
-  console.log(SIZE / Math.hypot(SIZE / 2, SIZE / 2));
   const style = useAnimatedStyle(() => ({
     transform: [
       { translateX: phone.translate.x },
