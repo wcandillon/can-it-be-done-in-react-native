074ef8ce820bbedd678f8d8bd98c671f47deada6
William Candillon <wcandillon@gmail.com>
2020-08-16 16:35:26 +0200

    :wrench:

--- the-10-min/src/Picker/AnimationHelpers.tsx
+++ the-10-min/src/Picker/AnimationHelpers.tsx
@@ -6,24 +6,16 @@ import Animated, {
   cond,
   eq,
   set,
-  decay,
-  debug,
   startClock,
-  lessThan,
-  sub,
-  modulo,
-  neq,
   and,
   not,
-  abs,
   clockRunning,
   timing,
-  multiply,
   Easing,
-  divide,
+  stopClock,
 } from "react-native-reanimated";
 import { State } from "react-native-gesture-handler";
-import { min, snapPoint } from "react-native-redash";
+import { snapPoint } from "react-native-redash";
 
 import { ITEM_HEIGHT } from "./Constants";
 
@@ -31,15 +23,15 @@ interface WithDecayParams {
   value: Animated.Adaptable<number>;
   velocity: Animated.Adaptable<number>;
   state: Animated.Node<State>;
-  offset?: Animated.Value<number>;
+  offset: Animated.Value<number>;
   snapPoints: number[];
 }
 
 export const withDecay = (params: WithDecayParams) => {
   const { value, velocity, state: gestureState, offset, snapPoints } = {
-    offset: new Value(0),
     ...params,
   };
+  const init = new Value(0);
   const clock = new Clock();
   const state = {
     finished: new Value(0),
@@ -53,7 +45,7 @@ export const withDecay = (params: WithDecayParams) => {
     easing: Easing.bezier(0.22, 1, 0.36, 1),
   };
   return block([
-    startClock(clock),
+    cond(not(init), [set(state.position, offset), set(init, 1)]),
     cond(eq(gestureState, State.BEGAN), set(offset, state.position)),
     cond(eq(gestureState, State.ACTIVE), [
       set(state.position, add(offset, value)),
@@ -63,7 +55,9 @@ export const withDecay = (params: WithDecayParams) => {
       set(config.toValue, snapPoint(state.position, velocity, snapPoints)),
     ]),
     cond(and(not(state.finished), eq(gestureState, State.END)), [
+      cond(not(clockRunning(clock)), [startClock(clock)]),
       timing(clock, state, config),
+      cond(state.finished, stopClock(clock)),
     ]),
     state.position,
   ]);
