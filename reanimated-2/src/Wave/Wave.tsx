9ba244f11fb774ae01fcffd61a90a79ca3a5c21d
William Candillon <wcandillon@gmail.com>
2020-08-30 15:14:03 +0200

    :wrench:

--- reanimated-2/src/Wave/Wave.tsx
+++ reanimated-2/src/Wave/Wave.tsx
@@ -1,33 +1,80 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Dimensions } from "react-native";
 import Svg, { Path, Circle, Line } from "react-native-svg";
-import Animated from "react-native-reanimated";
+import Animated, {
+  repeat,
+  useDerivedValue,
+  withTiming,
+  useAnimatedProps,
+  useSharedValue,
+  Easing,
+} from "react-native-reanimated";
 import { StyleGuide } from "../components";
+import { mix } from "../components/AnimatedHelpers";
 
 const { width } = Dimensions.get("window");
 const AnimatedPath = Animated.createAnimatedComponent(Path);
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 const Wave = () => {
-  const from = { x: 0, y: 0.3 };
-  const c1 = { x: 0, y: 0.5 };
-  const c2 = { x: 1, y: 0.5 };
-  const to = { x: 1, y: 0.7 };
-  const d = `M ${from.x} ${from.y} C ${c1.x} ${c1.y} ${c2.x} ${c2.y} ${to.x} ${to.y} L 1 1 L 0 1 Z`;
+  const progress = useSharedValue(0);
+  const wave = useDerivedValue(() => ({
+    from: {
+      x: mix(progress.value, -0.1, -1),
+      y: mix(progress.value, 0.2, 0.5),
+    },
+    c1: { x: mix(progress.value, 0, 0.5), y: mix(progress.value, 0.7, 1) },
+    c2: { x: mix(progress.value, 1, 0.5), y: mix(progress.value, 0.3, 0) },
+    to: { x: mix(progress.value, 1.1, 2), y: mix(progress.value, 0.8, 0.5) },
+  }));
+  const path = useAnimatedProps(() => {
+    const { from, c1, c2, to } = wave.value;
+    return {
+      d: `M ${from.x} ${from.y} C ${c1.x} ${c1.y} ${c2.x} ${c2.y} ${to.x} ${to.y} L 1 1 L 0 1 Z`,
+    };
+  });
+  const c1 = useAnimatedProps(() => {
+    const { x, y } = wave.value.c1;
+    return {
+      cx: x,
+      cy: y,
+    };
+  });
+  const c2 = useAnimatedProps(() => {
+    const { x, y } = wave.value.c2;
+    return {
+      cx: x,
+      cy: y,
+    };
+  });
+  const from = useAnimatedProps(() => {
+    const { x, y } = wave.value.from;
+    return {
+      cx: x,
+      cy: y,
+    };
+  });
+  const to = useAnimatedProps(() => {
+    const { x, y } = wave.value.to;
+    return {
+      cx: x,
+      cy: y,
+    };
+  });
+  useEffect(() => {
+    progress.value = repeat(
+      withTiming(1, { duration: 1000, easing: Easing.inOut(Easing.ease) }),
+      -1,
+      true
+    );
+  }, [progress]);
   return (
     <Svg width={width} height={width} viewBox="0 0 1 1">
-      <AnimatedPath d={d} fill={StyleGuide.palette.primary} />
-      <Circle r={0.05} fill="red" cy={c1.y} cx={c1.x} />
-      <Circle r={0.05} fill="red" cy={c2.y} cx={c2.x} />
-      <Line
-        x1={c1.x}
-        y1={c1.y}
-        x2={c2.x}
-        y2={c2.y}
-        stroke="red"
-        strokeWidth={0.01}
-      />
-      <Circle r={0.05} fill="blue" cy={from.y} cx={from.x} />
-      <Circle r={0.05} fill="blue" cy={to.y} cx={to.x} />
+      <AnimatedPath animatedProps={path} fill={StyleGuide.palette.primary} />
+      <AnimatedCircle r={0.05} fill="red" animatedProps={from} />
+      <AnimatedCircle r={0.05} fill="red" animatedProps={to} />
+      <AnimatedCircle r={0.05} fill="blue" animatedProps={c1} />
+      <AnimatedCircle r={0.05} fill="blue" animatedProps={c2} />
     </Svg>
   );
 };
