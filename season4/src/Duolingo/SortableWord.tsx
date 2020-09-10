591a780698387059d5dece3e4e9c3086664d2756
William Candillon <wcandillon@gmail.com>
2020-09-10 11:28:04 +0200

    :wrench:

--- season4/src/Duolingo/SortableWord.tsx
+++ season4/src/Duolingo/SortableWord.tsx
@@ -7,6 +7,8 @@ import Animated, {
 export interface Offset {
   width: number;
   height: number;
+  x: number;
+  y: number;
 }
 
 interface SortableWordProps {
@@ -23,38 +25,17 @@ const SortableWord = ({
   containerWidth,
 }: SortableWordProps) => {
   const style = useAnimatedStyle(() => {
-    const { width, height } = offsets.value[index];
+    const { width, height, x, y } = offsets.value[index];
     if (width === 0) {
       return {};
     }
-    const { total, lastBreak } = offsets.value.slice(0, index + 1).reduce(
-      (acc, offset, i) => {
-        if (acc.done) {
-          return acc;
-        }
-        acc.total += offset.width;
-        if (acc.total > containerWidth) {
-          acc.done = true;
-          acc.lastBreak = i;
-          return acc;
-        }
-        return acc;
-      },
-      { done: false, total: 0, lastBreak: 0 }
-    );
-
-    const vIndex = Math.floor(total / containerWidth);
-    const translateY = height * vIndex;
-    const translateX = offsets.value
-      .slice(lastBreak, index)
-      .reduce((acc, offset) => acc + offset.width, 0);
     return {
       position: "absolute",
       top: 0,
       left: 0,
       width,
       height,
-      transform: [{ translateX }, { translateY }],
+      transform: [{ translateX: x }, { translateY: y }],
     };
   });
   return (
@@ -65,7 +46,28 @@ const SortableWord = ({
           layout: { width, height },
         },
       }) => {
-        offsets.value[index] = { width, height };
+        const { total, lastBreak } = offsets.value.slice(0, index + 1).reduce(
+          (acc, offset, i) => {
+            if (acc.done) {
+              return acc;
+            }
+            acc.total += offset.width;
+            if (acc.total > containerWidth) {
+              acc.done = true;
+              acc.lastBreak = i;
+              return acc;
+            }
+            return acc;
+          },
+          { done: false, total: 0, lastBreak: 0 }
+        );
+
+        const vIndex = Math.floor(total / containerWidth);
+        const y = height * vIndex;
+        const x = offsets.value
+          .slice(lastBreak, index)
+          .reduce((acc, offset) => acc + offset.width, 0);
+        offsets.value[index] = { width, height, x, y };
         offsets.value = [...offsets.value];
       }}
     >
