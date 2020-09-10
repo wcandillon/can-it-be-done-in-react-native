362b954885e172f13bb3f1f16a1e6eacf2a640c0
William Candillon <wcandillon@gmail.com>
2020-09-10 22:25:07 +0200

    :wrench:

--- season4/src/Duolingo/SortableWord.tsx
+++ season4/src/Duolingo/SortableWord.tsx
@@ -3,6 +3,8 @@ import Animated, {
   useAnimatedStyle,
   useAnimatedGestureHandler,
   withSpring,
+  useSharedValue,
+  useDerivedValue,
 } from "react-native-reanimated";
 import { PanGestureHandler } from "react-native-gesture-handler";
 import { useVector } from "react-native-redash";
@@ -16,38 +18,62 @@ interface SortableWordProps {
 }
 
 const SortableWord = ({ offsets, index, children }: SortableWordProps) => {
+  const gestureActive = useSharedValue(false);
   const offset = offsets[index];
+  const height = offset.height.current;
+  const width = offset.width.current;
   const translation = useVector(offset.x.value, offset.y.value);
   const panOffset = useVector();
   const onGestureEvent = useAnimatedGestureHandler({
     onStart: () => {
+      gestureActive.value = true;
       panOffset.x.value = offset.x.value;
       panOffset.y.value = offset.y.value;
     },
     onActive: (event) => {
       translation.x.value = offset.x.value + event.translationX;
       translation.y.value = offset.x.value + event.translationY;
+      const offsetY = Math.floor(translation.y.value / height) * height;
+      offsets.forEach((o, i) => {
+        if (
+          o.y.value === offsetY &&
+          o.x.value >= translation.x.value &&
+          translation.x.value <= o.x.value + o.width.current &&
+          i !== index
+        ) {
+          return;
+          const tmpX = o.x.value;
+          const tmpY = o.y.value;
+          const tmpWidth = o.width.current;
+          const tmpHeight = o.height.current;
+          o.y.value = offset.y.value;
+          o.x.value = offset.x.value;
+          o.width.current = offset.width.current;
+          o.height.current = offset.height.current;
+          offset.x.value = tmpX;
+          offset.y.value = tmpY;
+          offset.width.current = tmpWidth;
+          offset.height.current = tmpHeight;
+        }
+      });
     },
     onEnd: ({ velocityX, velocityY }) => {
+      gestureActive.value = false;
       translation.x.value = withSpring(offset.x.value, {
-        stiffness: 100,
-        mass: 1,
-        damping: 10,
-        overshootClamping: false,
-        restSpeedThreshold: 0.001,
-        restDisplacementThreshold: 0.001,
         velocity: velocityX,
       });
       translation.y.value = withSpring(offset.y.value, {
-        stiffness: 100,
-        mass: 1,
-        damping: 10,
-        overshootClamping: false,
-        restSpeedThreshold: 0.001,
-        restDisplacementThreshold: 0.001,
         velocity: velocityY,
       });
     },
+  });
+  const translateX = useDerivedValue(() => translation.x.value);
+  const translateY = useDerivedValue(() => {
+    if (gestureActive.value) {
+      return translation.y.value;
+    } else {
+      return withSpring(offset.y.value);
+    }
   });
   const style = useAnimatedStyle(() => {
     const { width, height } = offsets[index];
@@ -57,9 +83,10 @@ const SortableWord = ({ offsets, index, children }: SortableWordProps) => {
       left: 0,
       width: width.current,
       height: height.current,
+      zIndex: gestureActive.value ? 100 : 0,
       transform: [
-        { translateX: translation.x.value },
-        { translateY: translation.y.value },
+        { translateX: translateX.value },
+        { translateY: translateY.value },
       ],
     };
   });
