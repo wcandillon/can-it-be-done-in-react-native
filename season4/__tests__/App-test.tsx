1a0c7b5f07db88e51b00d5aed099d57ff0f20457
William Candillon <wcandillon@gmail.com>
2020-09-19 09:59:10 +0200

    :wrench:

--- season4/__tests__/App-test.tsx
+++ season4/__tests__/App-test.tsx
@@ -4,11 +4,10 @@ import "react-native-gesture-handler/jestSetup";
 
 const reorder = (input: { order: number }[], from: number, to: number) => {
   input.forEach((p) => {
-    if (p.order > to && p.order < from) {
-      p.order -= 1;
-    }
     if (p.order === from) {
-      p.order = to - 1;
+      p.order = to;
+    } else if (p.order >= to && p.order < from) {
+      p.order += 1;
     }
   });
 };
@@ -18,20 +17,22 @@ it("1", () => {
     { order: 1, word: "Ihr" },
     { order: 2, word: "isst" },
     { order: 3, word: "einen" },
-    { order: 4, word: "Apfel" },
-    { order: 5, word: "," },
-    { order: 6, word: "weil" },
-    { order: 7, word: "er" },
+    { order: 4, word: "," },
+    { order: 5, word: "weil" },
+    { order: 6, word: "er" },
+    { order: 7, word: "Apfel" },
     { order: 8, word: "hungrig" },
     { order: 9, word: "ist" },
   ];
-  reorder(words, 4, 7);
+  reorder(words, 7, 4);
+  words.sort((a, b) => (a.order > b.order ? 1 : -1));
+  console.log(JSON.stringify(words, null, 2));
   expect(words[0].order).toBe(1);
   expect(words[1].order).toBe(2);
   expect(words[2].order).toBe(3);
-  expect(words[3].order).toBe(6);
-  expect(words[4].order).toBe(4);
-  expect(words[5].order).toBe(5);
+  expect(words[3].order).toBe(4);
+  expect(words[4].order).toBe(5);
+  expect(words[5].order).toBe(6);
   expect(words[6].order).toBe(7);
   expect(words[7].order).toBe(8);
   expect(words[8].order).toBe(9);
