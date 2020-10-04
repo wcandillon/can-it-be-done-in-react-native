d8d677562929206b228158f554d84ed20c28e231
William Candillon <wcandillon@gmail.com>
2020-10-04 10:15:29 +0200

    :wrench:

--- season4/src/Reflectly/Tabbar.tsx
+++ season4/src/Reflectly/Tabbar.tsx
@@ -33,6 +33,10 @@ const p12 = parse(
   "M100 173.5V275C100 288.807 111.193 300 125 300H175C188.807 300 200 288.807 200 275V173.5C200 159.693 190.81 148.5 175 148.5H125C111.19 148.5 100 159.693 100 173.5Z"
 );
 
+const p31 = parse(
+  "M74 148.901V270.299C74 286.813 90.7893 300.2 111.5 300.2H186.5C207.211 300.2 224 286.813 224 270.299V148.901C224 132.387 210.215 119 186.5 119H111.5C90.785 119 74 132.387 74 148.901Z"
+);
+
 const Tabbar = () => {
   const open = useSharedValue(0);
   useLayoutEffect(() => {
@@ -48,8 +52,34 @@ const Tabbar = () => {
       d: interpolatePath(clamp(open.value, 0, 0.1), [0, 0.1], [p11, p12]),
     };
   });
+  const k3 = useAnimatedProps(() => {
+    return {
+      d: interpolatePath(clamp(open.value, 0.1, 0.25), [0.1, 0.25], [p12, p31]),
+    };
+  });
   return (
     <View>
+      <View
+        style={{
+          position: "absolute",
+          top: -SIZE,
+          left: 0,
+          right: 0,
+          alignItems: "center",
+        }}
+        pointerEvents="none"
+      >
+        <Svg
+          width={SIZE * 3}
+          height={SIZE * 3}
+          style={{
+            top: -SIZE,
+          }}
+          viewBox="0 0 300 300"
+        >
+          <AnimatedPath animatedProps={k3} fill="#02CBD6" />
+        </Svg>
+      </View>
       <StaticTabbar />
       <View
         style={{
