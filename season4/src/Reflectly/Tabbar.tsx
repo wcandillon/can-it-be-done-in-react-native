8576b836ce594c5f07e089d318934f8ee8371a55
William Candillon <wcandillon@gmail.com>
2020-10-04 10:32:10 +0200

    :wrench:

--- season4/src/Reflectly/Tabbar.tsx
+++ season4/src/Reflectly/Tabbar.tsx
@@ -1,5 +1,6 @@
 import React, { useLayoutEffect } from "react";
 import { StyleSheet, View } from "react-native";
+import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import Animated, {
   Extrapolate,
   interpolate,
@@ -41,77 +42,87 @@ const p31 = parse(
   "M74 148.901V270.299C74 286.813 90.7893 300.2 111.5 300.2H186.5C207.211 300.2 224 286.813 224 270.299V148.901C224 132.387 210.215 119 186.5 119H111.5C90.785 119 74 132.387 74 148.901Z"
 );
 
+const p32 = parse(
+  "M0 50V150C0 177.614 7.57864 200 49 200H250.5C291.921 200 300 177.614 300 150V50C300 22.3858 297.93 0 250.5 0H49C7.57 0 0 22.3858 0 50Z"
+);
+
+const K1 = 0.1;
+const K2 = 0.25;
+
 const Tabbar = () => {
   const open = useSharedValue(0);
-  useLayoutEffect(() => {
-    open.value = repeat(withTiming(1, { duration: 2000 }), -1, true);
-  });
   const k2 = useAnimatedProps(() => {
     return {
-      d: interpolatePath(clamp(open.value, 0.1, 0.25), [0.1, 0.25], [p1, p2]),
+      d: interpolatePath(clamp(open.value, K1, K2), [K1, K2], [p1, p2]),
     };
   });
   const k1 = useAnimatedProps(() => {
     return {
-      d: interpolatePath(clamp(open.value, 0, 0.1), [0, 0.1], [p11, p12]),
+      d: interpolatePath(clamp(open.value, 0, K1), [0, K1], [p11, p12]),
     };
   });
   const k3 = useAnimatedProps(() => {
     return {
       d: interpolatePath(
-        clamp(open.value, 0, 0.25),
-        [0, 0.1, 0.25],
-        [p30, p12, p31]
+        clamp(open.value, 0, 1),
+        [0, K1, K2, 1],
+        [p30, p12, p31, p32]
       ),
     };
   });
   return (
-    <View>
-      <View
-        style={{
-          position: "absolute",
-          top: -SIZE,
-          left: 0,
-          right: 0,
-          alignItems: "center",
-        }}
-        pointerEvents="none"
-      >
-        <Svg
-          width={SIZE * 3}
-          height={SIZE * 3}
+    <TouchableWithoutFeedback
+      onPress={() => {
+        open.value = withTiming(open.value === 1 ? 0 : 1);
+      }}
+    >
+      <View>
+        <View
           style={{
+            position: "absolute",
             top: -SIZE,
+            left: 0,
+            right: 0,
+            alignItems: "center",
           }}
-          viewBox="0 0 300 300"
+          pointerEvents="none"
         >
-          <AnimatedPath animatedProps={k3} fill="#02CBD6" />
-        </Svg>
-      </View>
-      <StaticTabbar />
-      <View
-        style={{
-          position: "absolute",
-          top: -SIZE,
-          left: 0,
-          right: 0,
-          alignItems: "center",
-        }}
-        pointerEvents="none"
-      >
-        <Svg
-          width={SIZE * 3}
-          height={SIZE * 3}
+          <Svg
+            width={SIZE * 3}
+            height={SIZE * 3}
+            style={{
+              top: -SIZE,
+            }}
+            viewBox="0 0 300 300"
+          >
+            <AnimatedPath animatedProps={k3} fill="#02CBD6" />
+          </Svg>
+        </View>
+        <StaticTabbar />
+        <View
           style={{
+            position: "absolute",
             top: -SIZE,
+            left: 0,
+            right: 0,
+            alignItems: "center",
           }}
-          viewBox="0 0 300 300"
+          pointerEvents="none"
         >
-          <AnimatedPath animatedProps={k1} fill="#02CBD6" />
-          <AnimatedPath animatedProps={k2} fill="#02CBD6" />
-        </Svg>
+          <Svg
+            width={SIZE * 3}
+            height={SIZE * 3}
+            style={{
+              top: -SIZE,
+            }}
+            viewBox="0 0 300 300"
+          >
+            <AnimatedPath animatedProps={k1} fill="#02CBD6" />
+            <AnimatedPath animatedProps={k2} fill="#02CBD6" />
+          </Svg>
+        </View>
       </View>
-    </View>
+    </TouchableWithoutFeedback>
   );
 };
 
