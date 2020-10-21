68969e1850d332d235851d451134fc2f62650fdb
William Candillon <wcandillon@gmail.com>
2020-10-21 23:16:08 +0200

    :wrench:

--- reanimated-2/src/ZAnimations/ZAnimations.tsx
+++ reanimated-2/src/ZAnimations/ZAnimations.tsx
@@ -5,7 +5,13 @@ import Svg from "react-native-svg";
 
 import Camera from "./components/Camera";
 import ZPath from "./components/ZPath";
-import { createPath3, close3, addLine3, addCurve3 } from "./components/Path3";
+import {
+  createPath3,
+  close3,
+  addLine3,
+  addCurve3,
+  addArc3,
+} from "./components/Path3";
 
 const { width } = Dimensions.get("window");
 const styles = StyleSheet.create({
@@ -32,7 +38,11 @@ const ZAnimations = () => {
   const camera = useCamera();
   const x = 0.5;
   const y = 0.5;
-  const path = createPath3({ x: -x, y: -y, z: 0 });
+  const path = createPath3({ x: -0.6, y: -0.6, z: 0 });
+  addArc3(path, { x: 0.2, y: -0.6, z: 0 }, { x: 0.2, y: 0.2, z: 0 });
+  addArc3(path, { x: 0.2, y: 0.6, z: 0 }, { x: 0.6, y: 0.6, z: 0 });
+
+  /*
   addCurve3(path, {
     c1: { x: 0.2, y: -0.6, z: 0 },
     c2: { x: 0.2, y: 0.6, z: 0 },
@@ -43,6 +53,7 @@ const ZAnimations = () => {
     c2: { x: 1, y: -0.12, z: 0 },
     to: { x: 1.2, y: 0.6, z: 0 },
   });
+  */
   return (
     <View style={styles.container}>
       <View>
--- reanimated-2/src/ZAnimations/components/Path3.ts
+++ reanimated-2/src/ZAnimations/components/Path3.ts
@@ -1,3 +1,5 @@
+import { lerp } from "react-native-redash";
+
 import { Vector3 } from "./Vector";
 
 export interface Curve3 {
@@ -33,6 +35,41 @@ export const addCurve3 = (path: Path3, c: Curve3) => {
     c1: c.c1,
     c2: c.c2,
     to: c.to,
+  });
+};
+
+/**
+ * PathCommand.prototype.arc = function( ctx, elem, renderer ) {
+  var prev = this.previousPoint;
+  var corner = this.renderPoints[0];
+  var end = this.renderPoints[1];
+  var cp0 = this.controlPoints[0];
+  var cp1 = this.controlPoints[1];
+  cp0.set( prev ).lerp( corner, arcHandleLength );
+  cp1.set( end ).lerp( corner, arcHandleLength );
+  return renderer.bezier( ctx, elem, cp0, cp1, end );
+};*/
+
+const lerp3 = (a: Vector3, b: Vector3, c: number) => {
+  "worklet";
+  return {
+    x: (b.x - a.x) * c + a.x,
+    y: (b.y - a.y) * c + a.y,
+    z: (b.z - a.z) * c + a.z,
+  };
+};
+
+/**
+ * @summary Add an arc command to a path
+ */
+export const addArc3 = (path: Path3, corner: Vector3, to: Vector3) => {
+  "worklet";
+  const last = path.curves[path.curves.length - 1];
+  const from = last ? last.to : path.move;
+  path.curves.push({
+    c1: lerp3(from, corner, 9 / 16),
+    c2: lerp3(to, corner, 9 / 16),
+    to,
   });
 };
 
