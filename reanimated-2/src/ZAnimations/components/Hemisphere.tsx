87a91820391a3d113bfdd754719fa505de7125b4
William Candillon <wcandillon@gmail.com>
2020-10-25 14:43:02 +0100

    :wrench:

--- reanimated-2/src/ZAnimations/components/Hemisphere.tsx
+++ reanimated-2/src/ZAnimations/components/Hemisphere.tsx
@@ -2,37 +2,39 @@ import React from "react";
 import { processColor } from "react-native";
 import Animated, {
   useAnimatedProps,
+  useAnimatedStyle,
   useDerivedValue,
 } from "react-native-reanimated";
-import { processTransform3d, serialize } from "react-native-redash";
+import { avg, processTransform3d, serialize } from "react-native-redash";
 import { Path } from "react-native-svg";
 
 import Layer from "./Layer";
 import { addArc3, close3, createPath3 } from "./Path3";
 import { project } from "./Vector";
+import Vertex from "./Vertex";
 import { useZSvg } from "./ZSvg";
 
 const AnimatedPath = Animated.createAnimatedComponent(Path);
 
-interface ZConeProps {
+interface HemisphereProps {
   r: number;
   base: string;
   body: string;
 }
 
-const ZCone = ({ r, base: baseColor, body: bodyColor }: ZConeProps) => {
+const Hemisphere = ({
+  r,
+  base: baseColor,
+  body: bodyColor,
+}: HemisphereProps) => {
   const c1 = processColor(baseColor);
   const c2 = processColor(bodyColor);
   const { camera, canvas } = useZSvg();
-  const basePath = createPath3({ x: -r, y: 0, z: 0 });
-  addArc3(basePath, { x: -r, y: r, z: 0 }, { x: 0, y: r, z: 0 });
-  addArc3(basePath, { x: r, y: r, z: 0 }, { x: r, y: 0, z: 0 });
-  addArc3(basePath, { x: r, y: -r, z: 0 }, { x: 0, y: -r, z: 0 });
-  addArc3(basePath, { x: -r, y: -r, z: 0 }, { x: -r, y: 0, z: 0 });
-
-  const bodyPath = createPath3({ x: -r, y: 0, z: 0 });
-  addArc3(bodyPath, { x: -r, y: 0, z: -r }, { x: 0, y: 0, z: -r });
-  addArc3(bodyPath, { x: r, y: 0, z: -r }, { x: r, y: 0, z: 0 });
+  const path = createPath3({ x: -r, y: 0, z: 0 });
+  addArc3(path, { x: -r, y: r, z: 0 }, { x: 0, y: r, z: 0 });
+  addArc3(path, { x: r, y: r, z: 0 }, { x: r, y: 0, z: 0 });
+  addArc3(path, { x: r, y: -r, z: 0 }, { x: 0, y: -r, z: 0 });
+  addArc3(path, { x: -r, y: -r, z: 0 }, { x: -r, y: 0, z: 0 });
 
   const data = useDerivedValue(() => {
     const cameraTransform = processTransform3d([
@@ -52,52 +54,51 @@ const ZCone = ({ r, base: baseColor, body: bodyColor }: ZConeProps) => {
       y: ((r * canvas.x) / 2) * Math.sin(alpha + Math.PI / 2),
       z: 0,
     };
-    const basePathPrj = {
-      move: project(basePath.move, canvas, cameraTransform),
-      curves: basePath.curves.map((curve) => ({
+    const bPath = {
+      move: project(path.move, canvas, cameraTransform),
+      curves: path.curves.map((curve) => ({
         c1: project(curve.c1, canvas, cameraTransform),
         c2: project(curve.c2, canvas, cameraTransform),
         to: project(curve.to, canvas, cameraTransform),
       })),
-      close: basePath.close,
+      close: path.close,
     };
-
-    const bodyPathPrj = {
-      move: project(bodyPath.move, canvas, cameraTransform),
-      curves: bodyPath.curves.map((curve) => ({
-        c1: project(curve.c1, canvas, cameraTransform),
-        c2: project(curve.c2, canvas, cameraTransform),
-        to: project(curve.to, canvas, cameraTransform),
-      })),
-      close: bodyPath.close,
-    };
+    const body = createPath3(p1);
+    addArc3(body, { x: p1.x, y: apex.y, z: 0 }, apex);
+    addArc3(body, { x: p2.x, y: apex.y, z: 0 }, p2);
+    close3(body);
 
     return {
-      zIndex: apex.z,
-      base: serialize(basePathPrj),
-      body: serialize(bodyPathPrj),
+      base: serialize(bPath),
+      body: serialize(body),
+      points: [apex, p1, p2],
     };
   });
 
-  const base = useAnimatedProps(() => ({
+  const face = useAnimatedProps(() => ({
     d: data.value.base,
-    fill: data.value.zIndex < 0 ? c1 : c2,
+    fill: data.value.points[0].z < 0 ? c1 : c2,
   }));
 
   const body = useAnimatedProps(() => ({
     d: data.value.body,
   }));
 
+  const bodyZIndex = useAnimatedStyle(() => ({
+    zIndex: avg(data.value.points.map(({ z }) => z)),
+  }));
+
   return (
     <>
       <Layer zIndexStyle={{ zIndex: 0 }}>
-        <AnimatedPath animatedProps={base} />
+        <AnimatedPath animatedProps={face} />
       </Layer>
-      <Layer zIndexStyle={{ zIndex: 0 }}>
+
+      <Layer zIndexStyle={bodyZIndex}>
         <AnimatedPath animatedProps={body} fill={bodyColor} />
       </Layer>
     </>
   );
 };
 
-export default ZCone;
+export default Hemisphere;
