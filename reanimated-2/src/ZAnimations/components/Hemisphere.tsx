db953750c059e39af8b9de749845f57ec7366eae
William Candillon <wcandillon@gmail.com>
2020-10-25 15:39:14 +0100

    :wrench:

--- reanimated-2/src/ZAnimations/components/Hemisphere.tsx
+++ reanimated-2/src/ZAnimations/components/Hemisphere.tsx
@@ -11,7 +11,7 @@ import { Path } from "react-native-svg";
 import Layer from "./Layer";
 import { addArc3, close3, createPath3 } from "./Path3";
 import { project } from "./Vector";
-import Vertex from "./Vertex";
+import ZPoints from "./ZPoints";
 import { useZSvg } from "./ZSvg";
 
 const AnimatedPath = Animated.createAnimatedComponent(Path);
@@ -27,8 +27,8 @@ const Hemisphere = ({
   base: baseColor,
   body: bodyColor,
 }: HemisphereProps) => {
-  const c1 = processColor(baseColor);
-  const c2 = processColor(bodyColor);
+  const co1 = processColor(baseColor);
+  const co2 = processColor(bodyColor);
   const { camera, canvas } = useZSvg();
   const path = createPath3({ x: -r, y: 0, z: 0 });
   addArc3(path, { x: -r, y: r, z: 0 }, { x: 0, y: r, z: 0 });
@@ -54,6 +54,25 @@ const Hemisphere = ({
       y: ((r * canvas.x) / 2) * Math.sin(alpha + Math.PI / 2),
       z: 0,
     };
+    const a = Math.PI + Math.atan2(apex.y, apex.x) - Math.PI / 4;
+    const b = Math.PI + Math.atan2(apex.y, apex.x) + Math.PI / 4;
+    const c = Math.PI + Math.atan2(apex.y, apex.x);
+    const d = Math.sign(r) * Math.sqrt(r ** 2 + r ** 2);
+    const c1 = {
+      x: ((d * canvas.x) / 2) * Math.cos(a),
+      y: ((d * canvas.x) / 2) * Math.sin(a),
+      z: 0,
+    };
+    const c2 = {
+      x: ((d * canvas.x) / 2) * Math.cos(b),
+      y: ((d * canvas.x) / 2) * Math.sin(b),
+      z: 0,
+    };
+    const c3 = {
+      x: ((d * canvas.x) / 2) * Math.cos(c),
+      y: ((d * canvas.x) / 2) * Math.sin(c),
+      z: 0,
+    };
     const bPath = {
       move: project(path.move, canvas, cameraTransform),
       curves: path.curves.map((curve) => ({
@@ -64,20 +83,21 @@ const Hemisphere = ({
       close: path.close,
     };
     const body = createPath3(p1);
-    addArc3(body, { x: p1.x, y: apex.y, z: 0 }, apex);
-    addArc3(body, { x: p2.x, y: apex.y, z: 0 }, p2);
+    addArc3(body, c1, c3);
+    addArc3(body, c2, p2);
     close3(body);
 
     return {
       base: serialize(bPath),
       body: serialize(body),
       points: [apex, p1, p2],
+      c: [c1, c2, c3],
     };
   });
 
   const face = useAnimatedProps(() => ({
     d: data.value.base,
-    fill: data.value.points[0].z < 0 ? c1 : c2,
+    fill: data.value.points[0].z < 0 ? co1 : co2,
   }));
 
   const body = useAnimatedProps(() => ({
@@ -88,6 +108,7 @@ const Hemisphere = ({
     zIndex: avg(data.value.points.map(({ z }) => z)),
   }));
 
+  const points = useDerivedValue(() => data.value.c);
   return (
     <>
       <Layer zIndexStyle={{ zIndex: 0 }}>
@@ -97,6 +118,7 @@ const Hemisphere = ({
       <Layer zIndexStyle={bodyZIndex}>
         <AnimatedPath animatedProps={body} fill={bodyColor} />
       </Layer>
+      <ZPoints points={points} fill="red" />
     </>
   );
 };
