81266ca7080c33d8df0a131b9d05f8d5d31fc79e
William Candillon <wcandillon@gmail.com>
2020-10-25 20:52:05 +0100

    :wrench:

--- reanimated-2/src/ZAnimations/components/ZCone.tsx
+++ reanimated-2/src/ZAnimations/components/ZCone.tsx
@@ -5,15 +5,17 @@ import Animated, {
   useDerivedValue,
 } from "react-native-reanimated";
 import { serialize } from "react-native-redash";
-import { Path } from "react-native-svg";
+import { Circle, Path } from "react-native-svg";
 
 import Layer from "./Layer";
 import { addArc3, createPath3 } from "./Path3";
 import { project } from "./Vector";
 import Vertex from "./Vertex";
+import Points from "./ZPoints";
 import { useZSvg } from "./ZSvg";
 
 const AnimatedPath = Animated.createAnimatedComponent(Path);
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
 
 interface ZConeProps {
   r: number;
@@ -34,17 +36,7 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
 
   const data = useDerivedValue(() => {
     const apex = project({ x: 0, y: 0, z: -length }, canvas, camera.value);
-    const alpha = Math.PI + Math.atan2(apex.y, apex.x);
-    const p1 = {
-      x: ((r * canvas.x) / 2) * Math.cos(alpha - Math.PI / 2),
-      y: ((r * canvas.x) / 2) * Math.sin(alpha - Math.PI / 2),
-      z: 0,
-    };
-    const p2 = {
-      x: ((r * canvas.x) / 2) * Math.cos(alpha + Math.PI / 2),
-      y: ((r * canvas.x) / 2) * Math.sin(alpha + Math.PI / 2),
-      z: 0,
-    };
+
     const bPath = {
       move: project(path.move, canvas, camera.value),
       curves: path.curves.map((curve) => ({
@@ -54,33 +46,53 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
       })),
       close: path.close,
     };
+    const alpha = Math.atan2(apex.y, apex.x);
+    const rs = (r * canvas.x) / 2;
+    const p1 = {
+      x: rs * Math.cos(alpha - Math.PI / 2),
+      y: rs * Math.sin(alpha - Math.PI / 2),
+      z: 0,
+    };
+    const p2 = {
+      x: rs * Math.cos(alpha + Math.PI / 2),
+      y: rs * Math.sin(alpha + Math.PI / 2),
+      z: 0,
+    };
     return {
       body: serialize(bPath),
-      base: [apex, p1, p2],
-      points: [
-        p1,
-        p2,
-        bPath.curves[0].to,
-        bPath.curves[1].to,
-        bPath.curves[2].to,
-        bPath.curves[3].to,
-      ],
+      apex: apex,
+      points: [p1, p2, apex],
     };
   });
 
   const face = useAnimatedProps(() => ({
     d: data.value.body,
-    fill: data.value.base[0].z < 0 ? c1 : c2,
+    fill: data.value.apex.z < 0 ? c1 : c2,
   }));
 
-  const points = useDerivedValue(() => data.value.base);
+  const points = useDerivedValue(() => data.value.points);
 
+  const animatedProps = useAnimatedProps(() => ({
+    cx: data.value.apex.x,
+    cy: data.value.apex.y,
+  }));
   return (
     <>
       <Layer zIndexStyle={{ zIndex: 0 }}>
         <AnimatedPath animatedProps={face} />
       </Layer>
       <Vertex points={points} fill={bodyColor} />
+      <Layer zIndexStyle={{ zIndex: 10 }}>
+        <AnimatedCircle animatedProps={animatedProps} r={5} fill="blue" />
+        <Circle
+          r={(r * canvas.x) / 2}
+          cx={0}
+          cy={0}
+          stroke="blue"
+          strokeWidth={1}
+        />
+      </Layer>
+      <Points points={points} fill="green" />
     </>
   );
 };
