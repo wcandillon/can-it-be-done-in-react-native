7537ab4cf4d68eb663138f2af3530eca4c4f4cb4
William Candillon <wcandillon@gmail.com>
2020-10-28 23:31:22 +0100

    :wrench:

--- reanimated-2/src/ZAnimations/components/ZCone.tsx
+++ reanimated-2/src/ZAnimations/components/ZCone.tsx
@@ -8,7 +8,7 @@ import Animated, {
   useDerivedValue,
 } from "react-native-reanimated";
 import { processTransform3d, serialize } from "react-native-redash";
-import { Circle, Path } from "react-native-svg";
+import { Circle, Ellipse, Line, Path } from "react-native-svg";
 
 import Layer from "./Layer";
 import { addArc3, createPath3 } from "./Path3";
@@ -19,6 +19,8 @@ import { useZSvg } from "./ZSvg";
 
 const AnimatedPath = Animated.createAnimatedComponent(Path);
 const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+const AnimatedLine = Animated.createAnimatedComponent(Line);
+const AnimatedEllipse = Animated.createAnimatedComponent(Ellipse);
 
 interface ZConeProps {
   r: number;
@@ -31,79 +33,73 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
   const c1 = processColor(baseColor);
   const c2 = processColor(bodyColor);
   const { camera, canvas } = useZSvg();
-  const path = createPath3({ x: -r, y: 0, z: 0 });
-  addArc3(path, { x: -r, y: r, z: 0 }, { x: 0, y: r, z: 0 });
-  addArc3(path, { x: r, y: r, z: 0 }, { x: r, y: 0, z: 0 });
-  addArc3(path, { x: r, y: -r, z: 0 }, { x: 0, y: -r, z: 0 });
-  addArc3(path, { x: -r, y: -r, z: 0 }, { x: -r, y: 0, z: 0 });
-
   const data = useDerivedValue(() => {
     const m = processTransform3d([
       { rotateX: camera.y.value },
-      // { rotateY: camera.x.value },
+      { rotateY: camera.x.value },
     ]);
     const apex = project({ x: 0, y: 0, z: -length }, canvas, m);
-    const rotation = Math.atan2(apex.y, apex.x) - Math.PI / 2;
-
-    const bPath = {
-      move: project(path.move, canvas, m),
-      curves: path.curves.map((curve) => ({
-        c1: project(curve.c1, canvas, m),
-        c2: project(curve.c2, canvas, m),
-        to: project(curve.to, canvas, m),
-      })),
-      close: path.close,
-    };
-    const rs = (r * canvas.x) / 2;
-    const b1 = bPath.curves[0].to;
-    const b = Math.sqrt(b1.x ** 2 + b1.y ** 2);
-    const a = rs;
-    // https://www.desmos.com/calculator/ocnckn71um
+    const rz = Math.atan2(apex.y, apex.x) - Math.PI / 2;
     const y0 = Math.sqrt(apex.x ** 2 + apex.y ** 2);
+    const a = (r * canvas.x) / 2;
+    const L = (length * canvas.x) / 2;
+    const b = interpolate(y0, [0, L], [a, 0]); //;
     const y = b ** 2 / y0;
-    if (y0 < rs) {
+    const x = a * Math.sqrt(1 - y ** 2 / b ** 2);
+    if (y0 < a) {
       return {
-        body: serialize(bPath),
-        apex: apex,
+        b,
+        rz,
+        apex,
+        a,
         points: [{ x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, apex],
       };
     }
-    const x = a * Math.sqrt(1 - y ** 2 / b ** 2);
-    const p1 = {
-      x: x,
-      y: Math.sign(apex.y) * y,
-      z: 0,
-    };
-    const p2 = {
-      x: -x,
-      y: Math.sign(apex.y) * y,
-      z: 0,
-    };
+    const p1 = rotateZ(
+      {
+        x: x,
+        y: y,
+        z: 0,
+      },
+      rz
+    );
+    const p2 = rotateZ(
+      {
+        x: -x,
+        y: y,
+        z: 0,
+      },
+      rz
+    );
     return {
-      body: serialize(bPath),
-      apex: apex,
+      b,
+      rz,
+      apex,
+      a,
       points: [p1, p2, apex],
     };
   });
-
-  const face = useAnimatedProps(() => ({
-    d: data.value.body,
-    fill: data.value.apex.z < 0 ? c1 : c2,
-    //stroke: data.value.apex.z < 0 ? c1 : c2,
-    //strokeWidth: 4,
-    //fillOpacity: 0.8,
+  const ellipseStyle = useAnimatedStyle(() => ({
+    transform: [{ rotateZ: data.value.rz }],
+    zIndex: 0,
   }));
-
+  const ellipse = useAnimatedProps(() => ({
+    rx: data.value.a,
+    ry: data.value.b,
+    fill: data.value.apex.z < 0 ? c1 : c2,
+  }));
   const points = useDerivedValue(() => data.value.points);
-
   return (
     <>
-      <Layer zIndexStyle={{ zIndex: 0 }}>
-        <AnimatedPath animatedProps={face} />
+      <Layer zIndexStyle={ellipseStyle}>
+        <AnimatedEllipse animatedProps={ellipse} />
       </Layer>
       <Vertex points={points} fill={bodyColor} />
     </>
   );
 };
-
+/*
+      <Vertex points={points} fill={bodyColor} />
+      <Points points={debug} fill="blue" />
+      */
 export default ZCone;
