7fa0d01e6a3e1d4c0fb49eabb280ffecccf9741f
William Candillon <wcandillon@gmail.com>
2020-10-29 10:20:40 +0100

    :wrench:

--- reanimated-2/src/ZAnimations/components/ZCone.tsx
+++ reanimated-2/src/ZAnimations/components/ZCone.tsx
@@ -7,7 +7,12 @@ import Animated, {
   useAnimatedStyle,
   useDerivedValue,
 } from "react-native-reanimated";
-import { processTransform3d, serialize, toDeg } from "react-native-redash";
+import {
+  identityMatrix4,
+  processTransform3d,
+  serialize,
+  toDeg,
+} from "react-native-redash";
 import { Circle, Ellipse, Line, Path } from "react-native-svg";
 
 import Layer from "./Layer";
@@ -41,9 +46,12 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
 
   const data = useDerivedValue(() => {
     const m = processTransform3d([
-      //{ rotateX: camera.y.value },
+      { rotateX: camera.y.value },
       { rotateY: camera.x.value },
     ]);
+    const apex3d = project({ x: 0, y: 0, z: -length }, canvas, m);
+    const apex = { x: 0, y: Math.sqrt(apex3d.x ** 2 + apex3d.y ** 2), z: 0 };
+    const rz = -Math.PI / 2 + Math.atan2(apex3d.y, apex3d.x);
 
     const bPath = {
       move: project(path.move, canvas, m),
@@ -55,35 +63,55 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
       close: path.close,
     };
 
-    const apex = project({ x: 0, y: 0, z: -length }, canvas, m);
-    const n = project({ x: 0, y: length, z: 0 }, canvas, m);
-    const scale = Math.sqrt(n.x ** 2 + n.y ** 2 + n.y ** 2);
-    const nDist = Math.sqrt(n.x ** 2 + n.y ** 2);
+    //const n = project({ x: 0, y: length, z: 0 }, canvas, m);
     //const eccenAngle = Math.acos(nDist / scale);
-    const e = Math.abs(Math.sin(Math.atan2(n.y, n.z)));
-
-    console.log(e);
-    const rz = Math.atan2(apex.y, apex.x) - Math.PI / 2;
-
+    // const e = Math.abs(Math.sin(Math.atan2(n.y, n.z)));
+    // console.log(e);
     const y0 = Math.sqrt(apex.x ** 2 + apex.y ** 2);
-
     const a = (r * canvas.x) / 2;
-    const L = (length * canvas.x) / 2;
-    const b = a * e; //;
+    // const L = (length * canvas.x) / 2;
+    const p = createPath3({ x: -r, y: 0, z: 0 });
+    addArc3(p, { x: -r, y: r, z: 0 }, { x: 0, y: r, z: 0 });
+    const pr2 = {
+      move: rotateZ(project(p.move, canvas, m), -rz),
+      curves: p.curves.map((curve) => ({
+        c1: rotateZ(project(curve.c1, canvas, m), -rz),
+        c2: rotateZ(project(curve.c2, canvas, m), -rz),
+        to: rotateZ(project(curve.to, canvas, m), -rz),
+      })),
+      close: p.close,
+    };
+    const pr = {
+      move: project(p.move, canvas, m),
+      curves: p.curves.map((curve) => ({
+        c1: project(curve.c1, canvas, m),
+        c2: project(curve.c2, canvas, m),
+        to: project(curve.to, canvas, m),
+      })),
+      close: p.close,
+    };
+    const n = project({ x: 0, y: r, z: 0 }, canvas, m);
+    const s = project({ x: 0, y: r, z: 0 }, canvas, identityMatrix4);
+    const b = Math.sqrt(n.x ** 2 + n.y ** 2); //;
+    const b2 = Math.sqrt(s.x ** 2 + s.y ** 2); //;
+
+    console.log(JSON.stringify({ a, b, b2 }, null, 2));
     const y = b ** 2 / y0;
     const x = a * Math.sqrt(1 - y ** 2 / b ** 2);
     if (y0 < b) {
       console.log("Invisible");
       return {
         d: serialize(bPath),
-        apex,
-        points: [{ x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, apex],
+        apex: apex3d,
+        points: [{ x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, apex3d],
+        n,
+        pr,
       };
     }
     const p1 = rotateZ(
       {
         x: x,
-        y: y,
+        y: Math.sign(y0) * y,
         z: 0,
       },
       rz
@@ -91,15 +119,17 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
     const p2 = rotateZ(
       {
         x: -x,
-        y: y,
+        y: Math.sign(y0) * y,
         z: 0,
       },
       rz
     );
     return {
       d: serialize(bPath),
-      apex,
-      points: [p1, p2, apex],
+      apex: apex3d,
+      points: [p1, p2, apex3d],
+      pr,
+      n,
     };
   });
   const points = useDerivedValue(() => data.value.points);
@@ -110,17 +140,30 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
     //strokeWidth: 4,
     //fillOpacity: 0.5,
   }));
+  const animatedProps = useAnimatedProps(() => ({
+    cx: data.value.n.x,
+    cy: data.value.n.y,
+  }));
+  const pr = useAnimatedProps(() => ({
+    d: serialize(data.value.pr),
+    fill: "red",
+    //stroke: data.value.apex.z < 0 ? c1 : c2,
+    //strokeWidth: 4,
+    //fillOpacity: 0.5,
+  }));
   return (
     <>
       <Layer zIndexStyle={{ zIndex: 0 }}>
         <AnimatedPath animatedProps={face} />
       </Layer>
       <Vertex points={points} fill={bodyColor} />
+      <Layer zIndexStyle={{ zIndex: 0 }}>
+        <AnimatedCircle r={5} fill="blue" animatedProps={animatedProps} />
+      </Layer>
+      <Layer zIndexStyle={{ zIndex: 0 }}>
+        <AnimatedPath animatedProps={pr} />
+      </Layer>
     </>
   );
 };
-/*
-      <Vertex points={points} fill={bodyColor} />
-      <Points points={debug} fill="blue" />
-      */
 export default ZCone;
