f7372ce1ccfb3df1614c24d4f47e451baeaa4d8a
William Candillon <wcandillon@gmail.com>
2020-10-29 11:22:41 +0100

    :wrench:

--- reanimated-2/src/ZAnimations/components/ZCone.tsx
+++ reanimated-2/src/ZAnimations/components/ZCone.tsx
@@ -34,6 +34,10 @@ interface ZConeProps {
   body: string;
 }
 
+// https://www.desmos.com/calculator/ocnckn71um
+// https://en.wikipedia.org/wiki/Conjugate_diameters
+// https://en.wikipedia.org/wiki/Ellipse#Theorem_of_Apollonios_on_conjugate_diameters
+
 const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
   const c1 = processColor(baseColor);
   const c2 = processColor(bodyColor);
@@ -52,7 +56,6 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
     const apex3d = project({ x: 0, y: 0, z: -length }, canvas, m);
     const apex = { x: 0, y: Math.sqrt(apex3d.x ** 2 + apex3d.y ** 2), z: 0 };
     const rz = -Math.PI / 2 + Math.atan2(apex3d.y, apex3d.x);
-
     const bPath = {
       move: project(path.move, canvas, m),
       curves: path.curves.map((curve) => ({
@@ -68,58 +71,15 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
     // const e = Math.abs(Math.sin(Math.atan2(n.y, n.z)));
     // console.log(e);
     const y0 = Math.sqrt(apex.x ** 2 + apex.y ** 2);
-    const a = (r * canvas.x) / 2;
     // const L = (length * canvas.x) / 2;
-    const pr1 = {
-      move: project(path.move, canvas, m),
-      curves: [
-        {
-          c1: project(path.curves[0].c1, canvas, m),
-          c2: project(path.curves[0].c2, canvas, m),
-          to: project(path.curves[0].to, canvas, m),
-        },
-      ],
-      close: false,
-    };
-    const pr2 = {
-      move: project(path.curves[0].to, canvas, m),
-      curves: [
-        {
-          c1: project(path.curves[1].c1, canvas, m),
-          c2: project(path.curves[1].c2, canvas, m),
-          to: project(path.curves[1].to, canvas, m),
-        },
-      ],
-      close: false,
-    };
-    const pr3 = {
-      move: project(path.curves[1].to, canvas, m),
-      curves: [
-        {
-          c1: project(path.curves[2].c1, canvas, m),
-          c2: project(path.curves[2].c2, canvas, m),
-          to: project(path.curves[2].to, canvas, m),
-        },
-      ],
-      close: false,
-    };
-    const pr4 = {
-      move: project(path.curves[2].to, canvas, m),
-      curves: [
-        {
-          c1: project(path.curves[3].c1, canvas, m),
-          c2: project(path.curves[3].c2, canvas, m),
-          to: project(path.curves[3].to, canvas, m),
-        },
-      ],
-      close: false,
-    };
-    const n = project({ x: 0, y: r, z: 0 }, canvas, m);
-    const s = project({ x: 0, y: r, z: 0 }, canvas, identityMatrix4);
-    const b = Math.sqrt(n.x ** 2 + n.y ** 2); //;
-    const b2 = Math.sqrt(s.x ** 2 + s.y ** 2); //;
 
-    console.log(JSON.stringify({ a, b, b2 }, null, 2));
+    const c1v = project({ x: 0, y: r, z: 0 }, canvas, m);
+    const c1 = Math.sqrt(c1v.x ** 2 + c1v.y ** 2);
+    const c2v = project({ x: -r, y: 0, z: 0 }, canvas, m);
+    const c2 = Math.sqrt(c2v.x ** 2 + c2v.y ** 2);
+    const a = (r * canvas.x) / 2;
+    const b = Math.sqrt(c1 ** 2 + c2 ** 2 - a ** 2);
+
     const y = b ** 2 / y0;
     const x = a * Math.sqrt(1 - y ** 2 / b ** 2);
     if (y0 < b) {
@@ -128,11 +88,6 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
         d: serialize(bPath),
         apex: apex3d,
         points: [{ x: 0, y: 0, z: 0 }, { x: 0, y: 0, z: 0 }, apex3d],
-        n,
-        pr1,
-        pr2,
-        pr3,
-        pr4,
       };
     }
     const p1 = rotateZ(
@@ -155,11 +110,6 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
       d: serialize(bPath),
       apex: apex3d,
       points: [p1, p2, apex3d],
-      n,
-      pr1,
-      pr2,
-      pr3,
-      pr4,
     };
   });
   const points = useDerivedValue(() => data.value.points);
@@ -170,47 +120,13 @@ const ZCone = ({ r, length, base: baseColor, body: bodyColor }: ZConeProps) => {
     //strokeWidth: 4,
     //fillOpacity: 0.5,
   }));
-  const animatedProps = useAnimatedProps(() => ({
-    cx: data.value.n.x,
-    cy: data.value.n.y,
-  }));
-  const pr1 = useAnimatedProps(() => ({
-    d: serialize(data.value.pr1),
-    fill: "red",
-  }));
-  const pr2 = useAnimatedProps(() => ({
-    d: serialize(data.value.pr2),
-    fill: "green",
-  }));
-  const pr3 = useAnimatedProps(() => ({
-    d: serialize(data.value.pr3),
-    fill: "blue",
-  }));
-  const pr4 = useAnimatedProps(() => ({
-    d: serialize(data.value.pr4),
-    fill: "yellow",
-  }));
+
   return (
     <>
       <Layer zIndexStyle={{ zIndex: 0 }}>
         <AnimatedPath animatedProps={face} />
       </Layer>
       <Vertex points={points} fill={bodyColor} />
-      <Layer zIndexStyle={{ zIndex: 0 }}>
-        <AnimatedCircle r={5} fill="blue" animatedProps={animatedProps} />
-      </Layer>
-      <Layer zIndexStyle={{ zIndex: 0 }}>
-        <AnimatedPath animatedProps={pr1} />
-      </Layer>
-      <Layer zIndexStyle={{ zIndex: 0 }}>
-        <AnimatedPath animatedProps={pr2} />
-      </Layer>
-      <Layer zIndexStyle={{ zIndex: 0 }}>
-        <AnimatedPath animatedProps={pr3} />
-      </Layer>
-      <Layer zIndexStyle={{ zIndex: 0 }}>
-        <AnimatedPath animatedProps={pr4} />
-      </Layer>
     </>
   );
 };
