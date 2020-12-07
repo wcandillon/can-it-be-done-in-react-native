3aa678561acb0def253648825956045c67d952cc
William Candillon <wcandillon@gmail.com>
2020-12-07 12:45:04 +0100

    :wrench:

--- reanimated-2/src/ColorSelection/Background.tsx
+++ reanimated-2/src/ColorSelection/Background.tsx
@@ -1,15 +1,16 @@
-import React, { useEffect, useRef, useState } from "react";
+import React, { useEffect, useMemo, useRef, useState } from "react";
 import { Dimensions, StyleSheet, View } from "react-native";
 // @ts-ignore
 import { Surface } from "gl-react-expo";
 // @ts-ignore
 import { GLSL, Node, Shaders } from "gl-react";
+// @ts-ignore
 import { Blur } from "gl-react-blur";
 import Color from "color";
 
 const { width, height } = Dimensions.get("window");
 const SIZE = height;
-const delta = (height - width) / 2;
+const DELTA = (height - width) / 2;
 
 const vec3 = (color: string): [number, number, number] => {
   const co = Color(color);
@@ -24,12 +25,11 @@ uniform vec3 backgroundColorStart;
 uniform vec3 foregroundColorStart;
 uniform vec2 position;
 uniform float progress;
-uniform float radius;
 varying vec2 uv;
 
 void main() {
   float mag = distance(uv, position);
-  gl_FragColor = mag > (radius * progress) ? vec4(backgroundColorStart, 1.0) : vec4(foregroundColorStart, 1.0);
+  gl_FragColor = mag > progress ? vec4(backgroundColorStart, 1.0) : vec4(foregroundColorStart, 1.0);
 }
 `,
   },
@@ -52,42 +52,43 @@ interface BackgroundProps {
 }
 
 const Background = ({ colorSelection, position }: BackgroundProps) => {
-  const node = useRef(null);
-  const [progress, setProgress] = useState(0);
+  const node = useRef<Node>(null);
+  const progress = useRef(0);
   useEffect(() => {
-    setProgress(0);
+    progress.current = 0;
+    requestAnimationFrame(animate);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [colorSelection]);
+  const uniforms = {
+    backgroundColorStart: vec3(colorSelection.previous.start),
+    foregroundColorStart: vec3(colorSelection.current.start),
+    position: [(DELTA + position.x) / SIZE, 1 - position.y / SIZE],
+    progress: 0,
+  };
   const animate = () => {
-    if (progress !== 1) {
-      setProgress((p) => p + 0.05);
+    if (progress.current < 1) {
+      progress.current += 0.05;
+      node.current?.setDrawProps({
+        uniforms: {
+          ...uniforms,
+          progress: progress.current,
+        },
+      });
+      requestAnimationFrame(animate);
     }
   };
-  if (progress < 1) {
-    requestAnimationFrame(animate);
-  }
-  console.log([(delta + position.x) / height, position.y / height]);
   return (
     <Surface
       style={{
         position: "absolute",
         top: 0,
-        left: -delta,
+        left: -DELTA,
         height,
         width: height,
       }}
     >
       <Blur factor={20} passes={6}>
-        <Node
-          ref={node}
-          shader={shaders.background}
-          uniforms={{
-            backgroundColorStart: vec3(colorSelection.previous.start),
-            foregroundColorStart: vec3(colorSelection.current.start),
-            position: [(delta + position.x) / height, 1 - position.y / height],
-            radius: 1,
-            progress,
-          }}
-        />
+        <Node ref={node} shader={shaders.background} uniforms={uniforms} />
       </Blur>
     </Surface>
   );
