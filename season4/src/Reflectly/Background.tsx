fa4f749d07766d61f742f4c58768d6abca50d819
William Candillon <wcandillon@gmail.com>
2020-12-08 17:37:22 +0100

    :wrench:

---
+++ season4/src/Reflectly/Background.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect } from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import Animated, {
+  Easing,
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
+
+const RADIUS = 45;
+const { width, height } = Dimensions.get("window");
+
+interface BackgroundProps {
+  colorSelection: {
+    position: { x: number; y: number };
+    current: { start: string; end: string };
+    previous: { start: string; end: string };
+  };
+}
+
+const Background = ({ colorSelection }: BackgroundProps) => {
+  const progress = useSharedValue(0);
+  useEffect(() => {
+    progress.value = 0;
+    progress.value = withTiming(1, {
+      duration: 650,
+      easing: Easing.inOut(Easing.ease),
+    });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [colorSelection]);
+  const MAX_RADIUS =
+    (Math.SQRT2 *
+      Math.max(
+        width + colorSelection.position.x,
+        height + colorSelection.position.y
+      )) /
+    2;
+  const style = useAnimatedStyle(() => {
+    return {
+      top: -RADIUS + colorSelection.position.y,
+      left: -RADIUS + colorSelection.position.x,
+      borderRadius: RADIUS,
+      width: RADIUS * 2,
+      height: RADIUS * 2,
+      backgroundColor: colorSelection.current.start,
+      transform: [{ scale: progress.value * (MAX_RADIUS / RADIUS) }],
+    };
+  });
+  return (
+    <View
+      style={{
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: colorSelection.previous.start,
+      }}
+    >
+      <Animated.View style={style} />
+    </View>
+  );
+};
+
+export default Background;
--- season4/src/Reflectly/ColorSelection.tsx
+++ season4/src/Reflectly/ColorSelection.tsx
@@ -1,5 +1,18 @@
-import React from "react";
-import { View } from "react-native";
+import React, { useState } from "react";
+import { StyleSheet, View } from "react-native";
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { snapPoint } from "react-native-redash";
+
+import Color, { COLOR_WIDTH } from "./Color";
+import BackgroundGL from "./BackgroundGL";
 
 const colors = [
   {
@@ -33,9 +46,67 @@ const colors = [
     end: "#FD9F9C",
   },
 ];
+const snapPoints = colors.map((_, i) => -i * COLOR_WIDTH);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  placeholder: {
+    width: COLOR_WIDTH,
+  },
+});
 
 const ColorSelection = () => {
-  return <View />;
+  const [colorSelection, setColorSelection] = useState({
+    previous: colors[0],
+    current: colors[0],
+    position: { x: 0, y: 0 },
+  });
+  const translateX = useSharedValue(0);
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    { x: number }
+  >({
+    onStart: (_, ctx) => {
+      ctx.x = translateX.value;
+    },
+    onActive: ({ translationX }, { x }) => {
+      translateX.value = x + translationX;
+    },
+    onEnd: ({ velocityX }) => {
+      const dest = snapPoint(translateX.value, velocityX, snapPoints);
+      translateX.value = withSpring(dest);
+    },
+  });
+  return (
+    <PanGestureHandler onGestureEvent={onGestureEvent}>
+      <Animated.View style={styles.container}>
+        <BackgroundGL colorSelection={colorSelection} />
+        <View style={styles.placeholder} />
+        {colors.map((color, index) => {
+          return (
+            <Color
+              color={color}
+              key={index}
+              index={index}
+              translateX={translateX}
+              onPress={(position) => {
+                translateX.value = withSpring(-index * COLOR_WIDTH);
+                setColorSelection({
+                  position,
+                  previous: colorSelection.current,
+                  current: color,
+                });
+              }}
+            />
+          );
+        })}
+      </Animated.View>
+    </PanGestureHandler>
+  );
 };
 
 export default ColorSelection;
