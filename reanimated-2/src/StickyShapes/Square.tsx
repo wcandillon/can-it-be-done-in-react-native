7f6b3a1d8d3b28209e448d594a22be4257c6f226
William Candillon <wcandillon@gmail.com>
2020-12-16 05:57:29 +0100

    :wrench:

--- reanimated-2/src/StickyShapes/Square.tsx
+++ reanimated-2/src/StickyShapes/Square.tsx
@@ -1,168 +1,49 @@
 import React from "react";
-import { Dimensions, StyleSheet } from "react-native";
-import {
-  PanGestureHandler,
-  PanGestureHandlerGestureEvent,
-} from "react-native-gesture-handler";
-import Animated, {
-  add,
-  Extrapolate,
-  interpolate,
-  useAnimatedGestureHandler,
-  useAnimatedProps,
-  useDerivedValue,
-  useSharedValue,
-  withSpring,
-} from "react-native-reanimated";
+import { StyleSheet, View } from "react-native";
+import Animated, { useAnimatedProps } from "react-native-reanimated";
 import {
   addCurve,
   addLine,
   createPath,
+  mix,
   serialize,
-  snapPoint,
-  useVector,
-  Vector,
 } from "react-native-redash";
 import Svg, { Path } from "react-native-svg";
 
+export const SIZE = 150;
+const H_FACTOR = 0.3;
+const V_FACTOR = 2.5;
+export const MAX_HEIGHT = SIZE * V_FACTOR;
 const AnimatedPath = Animated.createAnimatedComponent(Path);
-const { height } = Dimensions.get("window");
-const SIZE = 150;
-enum Mode {
-  TOP,
-  BOTTOM,
-  FREE,
+
+interface SquareProps {
+  progress: Animated.SharedValue<number>;
 }
-const exhaustiveCheck = (value: never) => {
-  throw new Error(value + " was not handled");
-};
 
-const assignVector = (
-  a: Vector<Animated.SharedValue<number>>,
-  b: Vector<number>,
-  withTransition: boolean
-) => {
-  "worklet";
-  a.x.value = withTransition ? withSpring(b.x) : b.x;
-  a.y.value = withTransition ? withSpring(b.y) : b.y;
-  return { x: a.x.value, y: a.y.value };
-};
-
-const Square = () => {
-  const mode = useSharedValue(Mode.TOP);
-  const translateY = useSharedValue(0);
-  const isGestureActive = useSharedValue(false);
-  const p1 = useVector();
-  const p2 = useVector();
-  const p3 = useVector();
-  const p4 = useVector();
-  const y = useDerivedValue(() =>
-    translateY.value < 0 ? height + translateY.value - SIZE : translateY.value
-  );
+const Square = ({ progress }: SquareProps) => {
   const animatedProps = useAnimatedProps(() => {
-    const delta = interpolate(
-      translateY.value,
-      [-height / 4, 0, height / 4],
-      [SIZE / 2 - 25, 0, SIZE / 2 - 25],
-      Extrapolate.CLAMP
-    );
-    const points = (() => {
-      switch (mode.value) {
-        case Mode.TOP:
-          return {
-            c1: { x: 0, y: 0 },
-            c2: { x: SIZE, y: 0 },
-            c3: { x: SIZE - delta, y: SIZE + translateY.value },
-            c4: { x: delta, y: SIZE + translateY.value },
-          };
-        case Mode.BOTTOM:
-          return {
-            c1: { x: delta, y: height - SIZE + translateY.value },
-            c2: { x: SIZE - delta, y: height - SIZE + translateY.value },
-            c3: { x: SIZE, y: height },
-            c4: { x: 0, y: height },
-          };
-        case Mode.FREE:
-          return {
-            c1: { x: 0, y: y.value },
-            c2: { x: SIZE, y: y.value },
-            c3: { x: SIZE, y: y.value + SIZE },
-            c4: { x: 0, y: y.value + SIZE },
-          };
-        default:
-          return exhaustiveCheck(mode);
-      }
-    })();
-    const withTransition = !(isGestureActive.value && mode.value === Mode.FREE);
-    const c1 = assignVector(p1, points.c1, withTransition);
-    const c2 = assignVector(p2, points.c2, withTransition);
-    const c3 = assignVector(p3, points.c3, withTransition);
-    const c4 = assignVector(p4, points.c4, withTransition);
-    const path = createPath(c1);
-    if (mode.value === Mode.TOP) {
-      addLine(path, c2);
-      addCurve(path, {
-        c1: c2,
-        c2: { x: c3.x, y: c2.y },
-        to: c3,
-      });
-      addLine(path, c4);
-      addCurve(path, {
-        c1: c4,
-        c2: { x: c4.x, y: c1.y },
-        to: c1,
-      });
-    } else {
-      addLine(path, c2);
-      addCurve(path, {
-        c1: c2,
-        c2: { x: c2.x, y: c3.y },
-        to: c3,
-      });
-      addLine(path, c4);
-      addCurve(path, {
-        c1: c4,
-        c2: { x: c1.x, y: c4.y },
-        to: c1,
-      });
-    }
+    const factor = {
+      x: mix(progress.value, 0, H_FACTOR),
+      y: mix(progress.value, 1, V_FACTOR),
+    };
+    const p1 = { x: 0, y: 0 };
+    const p2 = { x: SIZE, y: 0 };
+    const p3 = { x: SIZE * (1 - factor.x), y: SIZE * factor.y };
+    const p4 = { x: SIZE * factor.x, y: SIZE * factor.y };
+    const path = createPath(p1);
+    addLine(path, p2);
+    addCurve(path, { c1: { x: p2.x, y: 0 }, c2: { x: p3.x, y: 0 }, to: p3 });
+    addLine(path, p4);
+    addCurve(path, { c1: { x: p4.x, y: 0 }, c2: { x: p1.x, y: 0 }, to: p1 });
     return {
       d: serialize(path),
+      fill: "#45A6E5",
     };
   });
-  const onGestureEvent = useAnimatedGestureHandler<
-    PanGestureHandlerGestureEvent,
-    { y: number }
-  >({
-    onStart: (_, ctx) => {
-      ctx.y = translateY.value;
-      isGestureActive.value = true;
-    },
-    onActive: ({ translationY }, ctx) => {
-      translateY.value = ctx.y + translationY;
-      if (Math.abs(translateY.value) > height / 4) {
-        mode.value = Mode.FREE;
-      }
-    },
-    onEnd: ({ velocityY }) => {
-      const dest = snapPoint(y.value, velocityY, [0, height]);
-      if (dest === 0) {
-        mode.value = Mode.TOP;
-      } else {
-        mode.value = Mode.BOTTOM;
-      }
-      translateY.value = 0;
-      isGestureActive.value = false;
-    },
-  });
   return (
-    <PanGestureHandler onGestureEvent={onGestureEvent}>
-      <Animated.View style={StyleSheet.absoluteFill}>
-        <Svg style={StyleSheet.absoluteFill}>
-          <AnimatedPath animatedProps={animatedProps} fill="#7EDAB9" />
-        </Svg>
-      </Animated.View>
-    </PanGestureHandler>
+    <Svg style={StyleSheet.absoluteFill}>
+      <AnimatedPath animatedProps={animatedProps} />
+    </Svg>
   );
 };
 
--- reanimated-2/src/StickyShapes/StickyShapes.tsx
+++ reanimated-2/src/StickyShapes/StickyShapes.tsx
@@ -1,23 +1,48 @@
 import React from "react";
 import { View, StyleSheet, Dimensions } from "react-native";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedGestureHandler,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
 
-import Square from "./Square";
+import Square, { SIZE, MAX_HEIGHT } from "./Square";
 
-const { width } = Dimensions.get("window");
+const { width, height } = Dimensions.get("window");
 const styles = StyleSheet.create({
   container: {
     position: "absolute",
-    left: (width - 150) / 2,
+    left: (width - SIZE) / 2,
     top: 0,
     bottom: 0,
-    width: 150,
+    width: SIZE,
   },
 });
 
 const StickyShapes = () => {
+  const translateY = useSharedValue(0);
+  const progress = useDerivedValue(() =>
+    interpolate(translateY.value, [0, MAX_HEIGHT], [0, 1], Extrapolate.CLAMP)
+  );
+  const onGestureEvent = useAnimatedGestureHandler({
+    onActive: ({ translationY }) => {
+      translateY.value = translationY;
+    },
+    onEnd: ({ velocityY: velocity }) => {
+      translateY.value = withSpring(0, { velocity, overshootClamping: true });
+    },
+  });
   return (
     <View style={styles.container}>
-      <Square />
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View style={StyleSheet.absoluteFill}>
+          <Square progress={progress} />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
