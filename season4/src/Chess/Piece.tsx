27445d02a0e882522aa6ea1c1fbaccaae384af51
William Candillon <wcandillon@gmail.com>
2021-01-22 14:04:20 +0100

    :wrench:

--- season4/src/Chess/Piece.tsx
+++ season4/src/Chess/Piece.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Dimensions, StyleSheet, Image } from 'react-native';
-import Animated, { useAnimatedStyle, useSharedValue } from 'react-native-reanimated';
-import { Vector } from 'react-native-redash';
+import { PanGestureHandler } from 'react-native-gesture-handler';
+import Animated, { useAnimatedGestureHandler, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import { Vector} from 'react-native-redash';
+import {PanGestureHandlerGestureEvent} from 'react-native-gesture-handler';
 
 const {width} = Dimensions.get("window");
 const SIZE = width / 8;
@@ -34,6 +36,20 @@ interface PieceProps {
 const Piece = ({ id, startPosition }: PieceProps) => {
   const translateX = useSharedValue(startPosition.x * SIZE);
   const translateY = useSharedValue(startPosition.y * SIZE);
+  const onGestureEvent = useAnimatedGestureHandler<PanGestureHandlerGestureEvent, { x: number, y: number }>({
+    onStart: (_, ctx) => {
+      ctx.x = translateX.value;
+      ctx.y = translateY.value;
+    },
+    onActive: ({ translationX, translationY}, { x, y }) => {
+      translateX.value = x + translationX;
+      translateY.value = y + translationY;
+    },
+    onEnd: (_, { x, y }) => {
+      translateX.value = withTiming(x);
+      translateY.value = withTiming(y);
+    }
+  });
   const style = useAnimatedStyle(() => ({
     position: "absolute",
     transform: [
@@ -42,9 +58,11 @@ const Piece = ({ id, startPosition }: PieceProps) => {
     ]
   }));
   return (
+    <PanGestureHandler onGestureEvent={onGestureEvent}>
       <Animated.View style={style}>
         <Image source={PIECES[id]} style={styles.piece} />
       </Animated.View>
+    </PanGestureHandler>
   );
 }
 
