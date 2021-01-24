3b407a573bbb9efbf171c9b16ef0ced6ded0facd
William Candillon <wcandillon@gmail.com>
2021-01-24 15:23:56 +0100

    :wrench:

--- season4/src/Chess/Board.tsx
+++ season4/src/Chess/Board.tsx
@@ -1,11 +1,7 @@
-import React, { useCallback, useState } from "react";
+import React from "react";
 import { View, StyleSheet, Dimensions } from "react-native";
-import { Chess } from "chess.js";
-
-import { useConst } from "../components/AnimatedHelpers";
 
 import Background from "./Background";
-import Piece from "./Piece";
 
 const { width } = Dimensions.get("window");
 
@@ -17,37 +13,9 @@ const styles = StyleSheet.create({
 });
 
 const Board = () => {
-  const chess = useConst(() => new Chess());
-  const [state, setState] = useState({
-    player: "w",
-    board: chess.board(),
-  });
-  const onTurn = useCallback(() => {
-    setState({
-      player: state.player === "w" ? "b" : "w",
-      board: chess.board(),
-    });
-  }, [chess, state.player]);
   return (
     <View style={styles.container}>
       <Background />
-      {state.board.map((row, y) =>
-        row.map((piece, x) => {
-          if (piece !== null) {
-            return (
-              <Piece
-                key={`${x}-${y}`}
-                id={`${piece.color}${piece.type}` as const}
-                startPosition={{ x, y }}
-                chess={chess}
-                onTurn={onTurn}
-                enabled={state.player === piece.color}
-              />
-            );
-          }
-          return null;
-        })
-      )}
     </View>
   );
 };
--- season4/src/Chess/Piece.tsx
+++ season4/src/Chess/Piece.tsx
@@ -11,7 +11,7 @@ import Animated, {
 } from "react-native-reanimated";
 import { Vector } from "react-native-redash";
 
-import { toTranslation, SIZE, toPosition } from "./Notation";
+import { SIZE, toPosition, toTranslation } from "./Notation";
 
 const styles = StyleSheet.create({
   piece: {
@@ -40,101 +40,10 @@ export const PIECES: Pieces = {
 
 interface PieceProps {
   id: Piece;
-  startPosition: Vector;
-  chess: Chess;
-  onTurn: () => void;
-  enabled: boolean;
 }
 
-const Piece = ({ id, startPosition, chess, onTurn, enabled }: PieceProps) => {
-  const isGestureActive = useSharedValue(false);
-  const offsetX = useSharedValue(0);
-  const offsetY = useSharedValue(0);
-  const translateX = useSharedValue(startPosition.x * SIZE);
-  const translateY = useSharedValue(startPosition.y * SIZE);
-  const movePiece = useCallback(
-    (to: Position) => {
-      const moves = chess.moves({ verbose: true });
-      const from = toPosition({ x: offsetX.value, y: offsetY.value });
-      const move = moves.find((m) => m.from === from && m.to === to);
-      const { x, y } = toTranslation(move ? move.to : from);
-      translateX.value = withTiming(
-        x,
-        {},
-        () => (offsetX.value = translateX.value)
-      );
-      translateY.value = withTiming(y, {}, () => {
-        offsetY.value = translateY.value;
-        isGestureActive.value = false;
-      });
-      if (move) {
-        chess.move({ from, to });
-        onTurn();
-      }
-    },
-    [chess, isGestureActive, offsetX, offsetY, onTurn, translateX, translateY]
-  );
-  const onGestureEvent = useAnimatedGestureHandler({
-    onStart: () => {
-      offsetX.value = translateX.value;
-      offsetY.value = translateY.value;
-      isGestureActive.value = true;
-    },
-    onActive: ({ translationX, translationY }) => {
-      translateX.value = offsetX.value + translationX;
-      translateY.value = offsetY.value + translationY;
-    },
-    onEnd: () => {
-      runOnJS(movePiece)(
-        toPosition({ x: translateX.value, y: translateY.value })
-      );
-    },
-  });
-  const style = useAnimatedStyle(() => ({
-    position: "absolute",
-    zIndex: isGestureActive.value ? 100 : 10,
-    transform: [
-      { translateX: translateX.value },
-      { translateY: translateY.value },
-    ],
-  }));
-  const original = useAnimatedStyle(() => {
-    return {
-      position: "absolute",
-      width: SIZE,
-      height: SIZE,
-      zIndex: 0,
-      backgroundColor: isGestureActive.value
-        ? "rgba(255, 255, 0, 0.5)"
-        : "transparent",
-      transform: [{ translateX: offsetX.value }, { translateY: offsetY.value }],
-    };
-  });
-  const underlay = useAnimatedStyle(() => {
-    const position = toPosition({ x: translateX.value, y: translateY.value });
-    const translation = toTranslation(position);
-    return {
-      position: "absolute",
-      width: SIZE,
-      height: SIZE,
-      zIndex: 0,
-      backgroundColor: isGestureActive.value
-        ? "rgba(255, 255, 0, 0.5)"
-        : "transparent",
-      transform: [{ translateX: translation.x }, { translateY: translation.y }],
-    };
-  });
-  return (
-    <>
-      <Animated.View style={original} />
-      <Animated.View style={underlay} />
-      <PanGestureHandler onGestureEvent={onGestureEvent} enabled={enabled}>
-        <Animated.View style={style}>
-          <Image source={PIECES[id]} style={styles.piece} />
-        </Animated.View>
-      </PanGestureHandler>
-    </>
-  );
+const Piece = ({ id }: PieceProps) => {
+  return <Image source={PIECES[id]} style={styles.piece} />;
 };
 
 export default Piece;
