2da3d86931dc9c660299aa24a46148941dcaed1d
William Candillon <wcandillon@gmail.com>
2021-02-08 13:40:51 +0100

    :wrench:

--- reanimated-2/src/PizzaChallenge/Config.tsx
+++ reanimated-2/src/PizzaChallenge/Config.tsx
@@ -4,5 +4,5 @@ const {width} = Dimensions.get("window");
 export const PIZZA_SIZE = width - 64;
 export const BREAD_PADDING = PIZZA_SIZE * 0.03;
 export const INGREDIENT_SCALE = 0.15;
-export const MIN_RADIUS = PIZZA_SIZE/2 * 0.3;
-export const MAX_RADIUS = PIZZA_SIZE/2 * 0.6;
+export const MIN_RADIUS = PIZZA_SIZE/2 * 0.35;
+export const MAX_RADIUS = PIZZA_SIZE/2 * 0.55;
--- reanimated-2/src/PizzaChallenge/components/Ingredient.tsx
+++ reanimated-2/src/PizzaChallenge/components/Ingredient.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { View, Image } from 'react-native';
-import Animated from 'react-native-reanimated';
+import Animated, { useAnimatedStyle } from 'react-native-reanimated';
 import { mix, polar2Canvas } from 'react-native-redash';
 import { INGREDIENT_SCALE, MIN_RADIUS, MAX_RADIUS, PIZZA_SIZE } from '../Config';
 
@@ -15,24 +15,31 @@ const Ingredient = ({ asset, progress, index }: IngredientProps) => {
   const width = dimension.width * INGREDIENT_SCALE;
   const height = dimension.height * INGREDIENT_SCALE;
   const radius = mix(Math.round(Math.random()), MIN_RADIUS, MAX_RADIUS);
-  console.log({index});
   const theta = index * 2 * Math.PI/11;
   const {x, y} = polar2Canvas({ radius, theta }, { x: PIZZA_SIZE/2 - width/2, y: PIZZA_SIZE/2-height/2 });
+  const side1 = (Math.round(Math.random()) === 0 ? -1 : 1);
+  const side2 = (Math.round(Math.random()) === 0 ? -1 : 1);
+  const style = useAnimatedStyle(() => ({
+    opacity: mix(progress.value * 2, 0, 1),
+    transform: [
+      { translateX: x },
+      { translateY: y },
+      { translateX: mix(progress.value, side1 * PIZZA_SIZE/2, 0) },
+      { translateY: mix(progress.value, side2 * PIZZA_SIZE/2, 0) },
+      { scale: mix(progress.value, 3, 1)}
+    ]
+  }))
   return (
-    <Image
-      source={asset}
-      style={{
-        position: "absolute",
-        top: 0,
-        left: 0,
-        width,
-        height,
-        transform: [
-          { translateX: x },
-          { translateY: y }
-        ]
-      }}
-    />
+      <Animated.Image
+        source={asset}
+        style={[{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width,
+          height,
+        }, style]}
+      />
   );
 }
 
