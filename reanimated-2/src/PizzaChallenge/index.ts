5128877223feda14136aaa5c174e5ffc0addce04
William Candillon <wcandillon@gmail.com>
2021-02-17 14:20:57 +0100

    Delete index.ts

--- reanimated-2/src/PizzaChallenge/index.ts
+++
@@ -1,5 +0,0 @@
-import { assets as pizzaAsset } from "./Config";
-export { default } from "./PizzaChallenge";
-export const assets = Object.values(pizzaAsset).map((asset) =>
-  Array.isArray(asset) ? asset.flat() : asset
-);
