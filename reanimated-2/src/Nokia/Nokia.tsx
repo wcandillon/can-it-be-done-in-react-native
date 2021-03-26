8eff0d795aa2f156b195b29e109209bc9b3528f5
William Candillon <wcandillon@gmail.com>
2021-03-26 14:49:11 +0100

    :wrench:

--- reanimated-2/src/Nokia/Nokia.tsx
+++ reanimated-2/src/Nokia/Nokia.tsx
@@ -18,11 +18,11 @@ const Nokia = () => {
   return (
     <View style={styles.phone}>
       <View style={styles.screen}>
-        {new Array(HEIGHT * WIDTH).fill(0).map((_, i) => {
-          const x = i % WIDTH;
-          const y = Math.floor(i / WIDTH);
-          return <Pixel key={i} x={x} y={y} />;
-        })}
+        {new Array(HEIGHT).fill(0).map((_0, y) =>
+          new Array(WIDTH).fill(0).map((_1, x) => {
+            return <Pixel key={`${x}-${y}`} x={x} y={y} />;
+          })
+        )}
       </View>
     </View>
   );
--- reanimated-2/src/Nokia/Pixel.tsx
+++ reanimated-2/src/Nokia/Pixel.tsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { Dimensions, StyleSheet, View } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import Animated, { useAnimatedStyle } from "react-native-reanimated";
 
 const { width } = Dimensions.get("window");
-export const WIDTH = 5; //160 48x84
-export const HEIGHT = 5; //116
+export const WIDTH = 50; //160 48x84
+export const HEIGHT = 50; //116
 const POINT = width / WIDTH;
 const styles = StyleSheet.create({
   pixel: {
@@ -13,9 +13,14 @@ const styles = StyleSheet.create({
   },
 });
 
-const shader = (x: number, y: number): boolean => {
+const circle = (x: number, y: number): boolean => {
   "worklet";
-  return x % 2 === 0 && y % 2 === 0;
+  const x1 = (WIDTH - 1) / 2;
+  const y1 = (HEIGHT - 1) / 2;
+  const x2 = x;
+  const y2 = y;
+  const dist = Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+  return dist <= (WIDTH - 1) / 2;
 };
 
 interface PixelProps {
@@ -24,9 +29,8 @@ interface PixelProps {
 }
 
 const Pixel = ({ x, y }: PixelProps) => {
-  console.log({ x, y });
   const style = useAnimatedStyle(() => {
-    const on = shader(x, y);
+    const on = circle(x, y);
     return {
       backgroundColor: on ? "#303031" : "#7D8C73",
     };
