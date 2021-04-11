d92da7912a2d0f3a49c0874cb610d2d63d369ecc
William Candillon <wcandillon@gmail.com>
2021-04-11 21:03:14 +0200

    :wrench:

--- season4/src/Darkroom/Picture.tsx
+++ season4/src/Darkroom/Picture.tsx
@@ -1,7 +1,7 @@
 import React, { useRef } from "react";
 import { Dimensions, StyleSheet } from "react-native";
 import { Surface } from "gl-react-expo";
-import { Node, Shaders, GLSL } from "gl-react";
+import { Node, Shaders, GLSL, Uniform } from "gl-react";
 
 const { width } = Dimensions.get("window");
 const styles = StyleSheet.create({
@@ -13,12 +13,29 @@ const styles = StyleSheet.create({
 
 const shaders = Shaders.create({
   picture: {
-    frag: GLSL`
-precision highp float;
+    vert: GLSL`
+attribute vec2 _p;
 varying vec2 uv;
-
+uniform float tR;
+uniform vec2 res;
+float r;
 void main() {
-  gl_FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+  r = res.x / res.y;
+  gl_Position = vec4(_p,0.0,1.0);
+  uv = .5+.5*_p*vec2(max(r/tR,1.),max(tR/r,1.));
+}`,
+    frag: GLSL`
+precision mediump float;
+
+// our texture
+uniform sampler2D source;
+  
+// the texCoords passed in from the vertex shader.
+varying vec2 uv;
+  
+void main() {
+    // Look up a color from the texture.
+    gl_FragColor = texture2D(source, uv);
 }
 `,
   },
@@ -32,7 +49,15 @@ const Picture = ({ source }: PictureProps) => {
   const node = useRef<Node>(null);
   return (
     <Surface style={styles.container}>
-      <Node ref={node} shader={shaders.picture!} />
+      <Node
+        ref={node}
+        shader={shaders.picture!}
+        uniforms={{
+          source,
+          tR: Uniform.textureSizeRatio(source),
+          res: Uniform.Resolution,
+        }}
+      />
     </Surface>
   );
 };
