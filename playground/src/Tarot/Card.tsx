f6bbf7dae55d180e3517c156dc12e35c6e08e37b
William Candillon <wcandillon@gmail.com>
2021-08-23 12:51:02 +0200

    :wrench:

--- playground/src/Tarot/Card.tsx
+++ playground/src/Tarot/Card.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 import { View, StyleSheet, Dimensions, Image } from "react-native";
+import type { PanGestureHandlerGestureEvent } from "react-native-gesture-handler";
+import { PanGestureHandler } from "react-native-gesture-handler";
+import Animated, {
+  useAnimatedGestureHandler,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from "react-native-reanimated";
+import { snapPoint } from "react-native-redash";
 
 const { width: wWidth } = Dimensions.get("window");
+
+const SNAP_POINTS = [-wWidth, 0, wWidth];
 const aspectRatio = 430.94 / 228.14;
 const CARD_WIDTH = wWidth - 128;
 const CARD_HEIGHT = CARD_WIDTH * aspectRatio;
@@ -16,14 +27,45 @@ interface CardProps {
 }
 
 const Card = ({ card: { source, width, height } }: CardProps) => {
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const onGestureEvent = useAnimatedGestureHandler<
+    PanGestureHandlerGestureEvent,
+    { x: number; y: number }
+  >({
+    onStart: (_, ctx) => {
+      ctx.x = translateX.value;
+      ctx.y = translateY.value;
+    },
+    onActive: ({ translationX, translationY }, ctx) => {
+      translateX.value = ctx.x + translationX;
+      translateY.value = ctx.y + translationY;
+    },
+    onEnd: ({ velocityX, velocityY }) => {
+      const dest = snapPoint(translateX.value, velocityX, SNAP_POINTS);
+      translateX.value = withSpring(dest, { velocity: velocityX });
+      translateY.value = withSpring(0, { velocity: velocityY });
+    },
+  });
+  const style = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: translateX.value },
+      { translateY: translateY.value },
+    ],
+  }));
   return (
     <View style={styles.container}>
-      <View style={styles.card}>
-        <Image
-          source={source}
-          style={{ width: IMAGE_WIDTH, height: (IMAGE_WIDTH * height) / width }}
-        />
-      </View>
+      <PanGestureHandler onGestureEvent={onGestureEvent}>
+        <Animated.View style={[styles.card, style]}>
+          <Image
+            source={source}
+            style={{
+              width: IMAGE_WIDTH,
+              height: (IMAGE_WIDTH * height) / width,
+            }}
+          />
+        </Animated.View>
+      </PanGestureHandler>
     </View>
   );
 };
