edd4b833cfe8d602bb4d745094c95b7bc49e277a
William Candillon <wcandillon@gmail.com>
2022-09-27 14:21:42 +0200

    :new:

---
+++ season5/App.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import type { Routes } from "./src/Routes";
+import { Examples } from "./src/Examples";
+import { YouTube } from "./src/YouTube";
+import { LoadAssets } from "./src/components/LoadAssets";
+
+const fonts = {};
+
+const assets: number[] = [];
+
+const Stack = createStackNavigator<Routes>();
+const AppNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen
+      name="Examples"
+      component={Examples}
+      options={{
+        title: "Can it be done in React Native?",
+      }}
+    />
+    <Stack.Screen
+      name="YouTube"
+      component={YouTube}
+      options={{
+        title: "📺 YouTube",
+      }}
+    />
+  </Stack.Navigator>
+);
+
+const App = () => {
+  return (
+    <LoadAssets assets={assets} fonts={fonts}>
+      <AppNavigator />
+    </LoadAssets>
+  );
+};
+
+// eslint-disable-next-line import/no-default-export
+export default App;
---
+++ season5/src/YouTube/YouTube.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { View } from "react-native";
+
+export const YouTube = () => {
+  return <View style={{ flex: 1, backgroundColor: "cyan" }} />;
+};
