46517b05dc5edfba51c6906ae8c942b76c059f2f
William Candillon <wcandillon@gmail.com>
2022-10-05 10:48:24 +0200

    :wrench:

--- season5/src/Breathe/Breathe.tsx
+++ season5/src/Breathe/Breathe.tsx
@@ -1,5 +1,6 @@
+import type { SkiaValue } from "@shopify/react-native-skia";
 import {
-  BlurMask,
+  mix,
   Canvas,
   Circle,
   Easing,
@@ -7,67 +8,84 @@ import {
   Group,
   polar2Canvas,
   vec,
-  Shadow,
-  useLoop,
   useComputedValue,
-  RuntimeShader,
-  Skia,
+  SweepGradient,
+  useValue,
+  useTouchHandler,
+  runTiming,
 } from "@shopify/react-native-skia";
 import React from "react";
 import { Dimensions } from "react-native";
 import { interpolate } from "react-native-reanimated";
-import { mix } from "react-native-redash";
 
 const toRad = (deg: number) => (deg * Math.PI) / 180;
-//const c1 = "#61bea2";
-//const c2 = "#529ca0";
-const c3 = "#42B9DB";
 
-const N = 100;
+const N = 600;
 const { width, height } = Dimensions.get("window");
 const center = vec(width / 2, height / 2);
-const easing = Easing.bezier(0.85, 0, 0.15, 1); //Easing.inOut(Easing.ease);
+const easing = Easing.bezier(0.12, 0, 0.1, 0); // https://easings.net/, https://cubic-bezier.com/#.12,0,.1,-0.01
 
-const source = Skia.RuntimeEffect.Make(`
-uniform shader image;
-
-half4 main(float2 xy) {
-  return image.eval(xy).rgba;
+interface CenterProps {
+  progress: SkiaValue<number>;
 }
-`)!;
+
+const Center = ({ progress }: CenterProps) => {
+  const r = useComputedValue(() => mix(progress.current, 50, 25), [progress]);
+  return (
+    <Group>
+      <Circle c={center} r={r}>
+        <SweepGradient
+          colors={["yellow", "cyan", "magenta", "yellow"]}
+          c={center}
+        />
+      </Circle>
+      <Circle c={center} r={40} />
+    </Group>
+  );
+};
+
+interface CellProps {
+  n: number;
+  progress: SkiaValue<number>;
+}
+
+const Cell = ({ progress, n }: CellProps) => {
+  const theta = n * toRad(137.5);
+  const c1 = 12;
+  const radius = c1 * Math.sqrt(n);
+  const { x, y } = polar2Canvas({ theta, radius }, center);
+  const r = interpolate(easing(n / N), [0, 1], [0, 8]);
+  const c = useComputedValue(() => {
+    const s = mix(1 - n / N, 0, 0.5);
+    const p = interpolate(progress.current, [s, s + 0.5], [0, 1], "clamp");
+    return vec(mix(p, center.x, x), mix(p, center.y, y));
+  }, [progress]);
+  return <Circle c={c} r={r} />;
+};
 
 export const Breathe = () => {
-  const progress = useLoop({ duration: 3000 });
-  const transform = useComputedValue(
-    () => [{ rotate: mix(progress.current, -Math.PI, 0) }],
-    [progress]
-  );
+  const progress = useValue(0);
+  const onTouch = useTouchHandler({
+    onEnd: () => {
+      runTiming(progress, progress.current === 1 ? 0 : 1, {
+        duration: 1000,
+        //   easing: Easing.linear,
+      });
+    },
+  });
   return (
-    <Canvas style={{ width, height }} debug>
+    <Canvas style={{ width, height }} onTouch={onTouch}>
       <Fill color="black" />
-      <Group origin={center} transform={transform} blendMode="screen">
-        {new Array(N).fill(0).map((_, n) => {
-          const theta = n * toRad(137.5);
-          const c1 = interpolate(n, [0, N], [10, 30]);
-          const radius = c1 * Math.sqrt(n);
-          const { x, y } = polar2Canvas({ theta, radius }, center);
-          const r = interpolate(easing(n / N), [0, 1], [5, 45]);
-          const cl = c3;
-          // eslint-disable-next-line react-hooks/rules-of-hooks
-          const c = useComputedValue(() => {
-            const cx = mix(progress.current, center.x, x);
-            const cy = mix(progress.current, center.y, y);
-            return vec(cx, cy);
-          }, [progress]);
-          return (
-            <Group key={n}>
-              <Circle c={c} r={r} color={cl}>
-                <RuntimeShader source={source} />
-              </Circle>
-            </Group>
-          );
-        })}
+      <Group>
+        <SweepGradient
+          colors={["yellow", "cyan", "magenta", "yellow"]}
+          c={center}
+        />
+        {new Array(N).fill(0).map((_, n) => (
+          <Cell key={n} n={n} progress={progress} />
+        ))}
       </Group>
+      <Center progress={progress} />
     </Canvas>
   );
 };
