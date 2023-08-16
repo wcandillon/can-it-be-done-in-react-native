dd1ef7a3babf8c9c0e9cb57775fc91a405083ea7
William Candillon <wcandillon@gmail.com>
2023-08-16 10:12:59 +0200

    :wrench:

--- season5/src/Instagram/stickers/index.ts
+++ season5/src/Instagram/stickers/index.ts
@@ -4,11 +4,15 @@ import {
   QuestionsSticker,
   QuestionsStickerDimensions,
 } from "./QuestionsSticker";
+import { SkiaSticker, SkiaStickerDimensions } from "./SkiaSticker";
 import { SupportSticker, SupportStickerDimensions } from "./SupportSticker";
+import { TimeSticker, TimeStickerDimensions } from "./TimeSticker";
 
 export const stickers = [
   { Sticker: HelloSticker, dimensions: HelloStickerDimensions },
   { Sticker: LocationSticker, dimensions: LocationStickerDimensions },
   { Sticker: QuestionsSticker, dimensions: QuestionsStickerDimensions },
   { Sticker: SupportSticker, dimensions: SupportStickerDimensions },
+  { Sticker: TimeSticker, dimensions: TimeStickerDimensions },
+  { Sticker: SkiaSticker, dimensions: SkiaStickerDimensions },
 ];
