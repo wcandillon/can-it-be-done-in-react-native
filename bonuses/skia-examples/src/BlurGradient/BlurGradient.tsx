ca5d021db14cf27611ef977ead09a61f8578e1cc
William Candillon <wcandillon@gmail.com>
2023-08-26 16:19:51 +0200

    :wrench:

--- bonuses/skia-examples/src/BlurGradient/BlurGradient.tsx
+++ bonuses/skia-examples/src/BlurGradient/BlurGradient.tsx
@@ -8,6 +8,8 @@ import {
   ImageShader,
   Fill,
   Shader,
+  LinearGradient,
+  vec,
 } from "@shopify/react-native-skia";
 import React from "react";
 import { Dimensions, StyleSheet, View } from "react-native";
@@ -21,6 +23,7 @@ import { frag } from "../components/ShaderLib";
 const { width, height } = Dimensions.get("window");
 const source = frag`
 uniform shader image;
+uniform shader mask;
 uniform float2 resolution;
 
 half4 main(float2 xy) {
@@ -28,8 +31,8 @@ half4 main(float2 xy) {
   const float Pi = 6.28318530718; // Pi*2
   const float Directions = 16.0; // BLUR DIRECTIONS (Default 16.0 - More is better but slower)
   const float Quality = 3.0; // BLUR QUALITY (Default 4.0 - More is better but slower)
-  const float Size = 10.0; // BLUR SIZE (Radius)
-  vec2 Radius = Size/resolution.xy;
+  const float Size = 30; // BLUR SIZE (Radius)
+  vec2 Radius = mix(0, Size, mask.eval(xy).a)/resolution.xy;
 
   // Normalized pixel coordinates (from 0 to 1)
   vec2 uv = xy/resolution;
@@ -38,14 +41,14 @@ half4 main(float2 xy) {
   // Blur calculations
   for( float d=0.0; d<Pi; d+=Pi/Directions)
   {
-      for(float i=1.0/Quality; i<=1.0; i+=1.0/Quality)
+      for(float i=1.0/Quality; i<=1.001; i+=1.0/Quality)
       {
         float2 val = uv+vec2(cos(d),sin(d))*Radius*i;
         Color += image.eval(val * resolution);		
       }
   }
   // Output to screen
-  Color /= Quality * Directions - 15.0;
+  Color /= Quality * Directions + 1.0;
   return Color;
 }
 `;
@@ -82,6 +85,11 @@ export const BlurGradient = () => {
               width={width}
               height={height}
               fit="cover"
+            />
+            <LinearGradient
+              start={vec(0, height / 2)}
+              end={vec(0, height)}
+              colors={["transparent", "black"]}
             />
           </Shader>
         </Fill>
