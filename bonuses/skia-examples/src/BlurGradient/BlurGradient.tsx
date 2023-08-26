981882a9cddd99c3d6ac893f97183c01bf0aebd7
William Candillon <wcandillon@gmail.com>
2023-08-26 11:49:18 +0200

    :wrench:

--- bonuses/skia-examples/src/BlurGradient/BlurGradient.tsx
+++ bonuses/skia-examples/src/BlurGradient/BlurGradient.tsx
@@ -1,6 +1,47 @@
+import {
+  Canvas,
+  useImage,
+  Image,
+  Group,
+  Skia,
+} from "@shopify/react-native-skia";
 import React from "react";
-import { View } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
+import Animated, {
+  useAnimatedScrollHandler,
+  useSharedValue,
+} from "react-native-reanimated";
+
+const { width, height } = Dimensions.get("window");
 
 export const BlurGradient = () => {
-  return <View style={{ flex: 1, backgroundColor: "cyan" }} />;
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const image = useImage(require("./zurich.jpg"));
+  const matrix = useSharedValue(Skia.Matrix());
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: ({ contentOffset: { y } }) => {
+      matrix.value = Skia.Matrix();
+      matrix.value.translate(0, -y);
+    },
+  });
+
+  return (
+    <View style={{ flex: 1 }}>
+      <Canvas style={{ flex: 1 }}>
+        <Group matrix={matrix}>
+          <Image
+            image={image}
+            x={0}
+            y={0}
+            width={width}
+            height={height}
+            fit="cover"
+          />
+        </Group>
+      </Canvas>
+      <View style={StyleSheet.absoluteFill}>
+        <Animated.ScrollView scrollEventThrottle={16} onScroll={onScroll} />
+      </View>
+    </View>
+  );
 };
