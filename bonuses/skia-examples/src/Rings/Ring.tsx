c3d0700518204982b3941118d466be34904f5ac0
William Candillon <wcandillon@gmail.com>
2023-09-20 07:18:28 +0200

    :wrench:

--- bonuses/skia-examples/src/Rings/Ring.tsx
+++ bonuses/skia-examples/src/Rings/Ring.tsx
@@ -20,6 +20,22 @@ import {
 
 import { frag } from "../components";
 
+const source = frag`
+uniform shader image;
+
+
+vec2 rotate(in vec2 coord, in float angle, vec2 origin) {
+  vec2 coord1 = coord - origin;
+  vec2 rotated = coord1 * mat2( cos(angle), -sin(angle),
+                       sin(angle),  cos(angle));
+  return rotated + origin;
+ }
+
+vec4 main(vec2 xy) {
+  return image.eval(pos);
+}
+`;
+
 const fromCircle = (center: Vector, r: number) => {
   "worklet";
   return Skia.XYWHRect(center.x - r, center.y - r, r * 2, r * 2);
