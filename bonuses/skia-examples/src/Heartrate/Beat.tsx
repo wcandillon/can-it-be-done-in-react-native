3e201b36d6e4c3f57703542d22e49bb9067e8832
William Candillon <wcandillon@gmail.com>
2024-02-04 21:42:43 +0100

    :wrench:

--- bonuses/skia-examples/src/Heartrate/Beat.tsx
+++ bonuses/skia-examples/src/Heartrate/Beat.tsx
@@ -7,13 +7,10 @@ import {
   mix,
   rect,
 } from "@shopify/react-native-skia";
-import React, { useEffect } from "react";
+import React from "react";
 import { Dimensions } from "react-native";
-import {
-  useDerivedValue,
-  useSharedValue,
-  withTiming,
-} from "react-native-reanimated";
+import type { SharedValue } from "react-native-reanimated";
+import { Easing, useDerivedValue } from "react-native-reanimated";
 
 const path = Skia.Path.MakeFromSVGString(
   "M 8 15 C -7.3 4.9 3.3 -3 7.8 1.1 C 7.9 1.2 7.9 1.3 8 1.3 A 3.1 3.1 0 0 1 8.2 1.1 C 12.7 -3 23.3 4.9 8 15 Z"
@@ -23,18 +20,18 @@ const c = { x: width / 2, y: height / 2 };
 const src = path.computeTightBounds();
 const pad = 64;
 const dst = rect(pad, pad, width - 2 * pad, height - pad * 2);
+interface BeatProps {
+  progress: SharedValue<number>;
+}
 
-export const Breat = () => {
-  const progress = useSharedValue(0);
-  useEffect(() => {
-    progress.value = withTiming(1, { duration: 3000 });
-  }, [progress]);
+export const Beat = ({ progress }: BeatProps) => {
   const transform = useDerivedValue(() => [
-    { scale: mix(progress.value, 1, 8) },
+    { scale: mix(progress.value, 1, 3) },
   ]);
-  const blur = useDerivedValue(() => mix(progress.value, 1, 4));
+  const blur = useDerivedValue(() => mix(progress.value, 0.5, 2));
+  const opacity = useDerivedValue(() => 1 - Math.pow(progress.value, 3));
   return (
-    <Group transform={transform} origin={c}>
+    <Group transform={transform} origin={c} opacity={opacity}>
       <Group transform={fitbox("contain", src, dst)}>
         <Path color="#D52327" path={path} style="stroke" strokeWidth={1}>
           <Blur blur={blur} />
--- bonuses/skia-examples/src/Heartrate/Heartrate.tsx
+++ bonuses/skia-examples/src/Heartrate/Heartrate.tsx
@@ -8,11 +8,22 @@ import {
   fitbox,
   rect,
   center,
+  mix,
+  useValue,
 } from "@shopify/react-native-skia";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Dimensions } from "react-native";
+import type { SharedValue } from "react-native-reanimated";
+import {
+  Easing,
+  makeMutable,
+  useDerivedValue,
+  useSharedValue,
+  withTiming,
+} from "react-native-reanimated";
 
-import { Breat } from "./Beat";
+import { Beat } from "./Beat";
+import { useLoop } from "./Animations";
 
 const path = Skia.Path.MakeFromSVGString(
   "M 8 15 C -7.3 4.9 3.3 -3 7.8 1.1 C 7.9 1.2 7.9 1.3 8 1.3 A 3.1 3.1 0 0 1 8.2 1.1 C 12.7 -3 23.3 4.9 8 15 Z"
@@ -24,24 +35,47 @@ const pad = 64;
 const dst1 = rect(pad, pad, width - 2 * pad, height - pad * 2);
 
 const bpm = 50;
-const duration = 3000;
+const duration = (60 * 1000) / bpm;
 
 export const Heartrate = () => {
+  const count = useRef(0);
+  const val1 = useSharedValue(1);
+  const val2 = useSharedValue(1);
+  const val3 = useSharedValue(1);
+  const progress = useLoop({ duration: duration / 2 });
+  useEffect(() => {
+    const it = setInterval(() => {
+      const val = [val1, val2, val3][count.current];
+      val.value = 0;
+      val.value = withTiming(1, { duration: 3000, easing: Easing.linear });
+      count.current = (count.current + 1) % 3;
+    }, duration);
+    return () => {
+      clearInterval(it);
+    };
+  }, [val1, val2, val3]);
+  const transform = useDerivedValue(() => [
+    { scale: mix(progress.value, 1.1, 1) },
+  ]);
   return (
     <Canvas style={{ flex: 1 }}>
       <Fill color="black" />
-      <Breat />
-      <Group transform={fitbox("contain", src, dst1)}>
-        <Path color="#D52327" path={path} />
-        <Group>
-          <Path
-            color="black"
-            path={path}
-            transform={[{ scale: 0.9 }]}
-            origin={center(src)}
-          >
-            <Blur blur={4} />
-          </Path>
+      <Beat progress={val1} />
+      <Beat progress={val2} />
+      <Beat progress={val3} />
+      <Group transform={transform} origin={c}>
+        <Group transform={fitbox("contain", src, dst1)}>
+          <Path color="#D52327" path={path} />
+          <Group>
+            <Path
+              color="black"
+              path={path}
+              transform={[{ scale: 0.9 }]}
+              origin={center(src)}
+            >
+              <Blur blur={4} />
+            </Path>
+          </Group>
         </Group>
       </Group>
     </Canvas>
